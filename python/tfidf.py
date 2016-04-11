ce315d3b5a8b977a5df9730dee86efb5d3458bdd
Doh Young Mo <modohyoung@gmail.com>
2016-04-12 02:50:26 +0900

    Stemming is implemented.

--- python/tfidf.py
+++ python/tfidf.py
@@ -1,18 +1,31 @@
 import nltk
 import string
+import os
 
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 from collections import Counter
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+stemmer = PorterStemmer()
+stop_words = [s.encode('utf-8') for s in stopwords.words('english')]
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
 
 def get_tokens(filename):
     r = open(filename,'r')
-    text = r.read().lower().translate(None, string.punctuation)
+    text = r.read().lower().translate(None,string.punctuation)
     tokens = nltk.word_tokenize(text)
-    return tokens
+    filtered_tokens = [w.decode('utf-8') for w in tokens if not w in stop_words]
+    stems = stem_tokens(filtered_tokens, stemmer)
+    return stems
 
 tokens = get_tokens("parsed_ipg160105.txt")
-# tokens = get_tokens("test.txt")
-stop_words = [s.encode('utf-8') for s in stopwords.words('english')]
-filtered = [w for w in tokens if not w in stop_words]
-count = Counter(filtered)
+count = Counter(tokens)
+
 print count.most_common(100)
