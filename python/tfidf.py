97256086cd07fd2d3313d98fa636b6100fe80fd6
Doh Young Mo <modohyoung@gmail.com>
2016-04-13 19:15:54 +0900

    Stemming library in nltk has memory leakage. Summation of dictionaries is really slow.

--- python/tfidf.py
+++ python/tfidf.py
@@ -4,50 +4,77 @@ import os
 import re
 
 from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+# from nltk.stem.porter import PorterStemmer
 from collections import Counter
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-stemmer = PorterStemmer()
+#stemmer = PorterStemmer()
 stop_words = [s.encode('utf-8') for s in stopwords.words('english')]
 
 docs = []
 tfmap = Counter()
 
-def stem_tokens(tokens, stemmer):
-    stemmed = []
-    for item in tokens:
-        stemmed.append(stemmer.stem(item))
-    return stemmed
+#def stem_tokens(tokens, stemmer):
+#    stemmed = []
+#    for item in tokens:
+#        stemmed.append(stemmer.stem(item))
+#    return stemmed
 
-def get_tokens(doc):
-    tokens = nltk.word_tokenize(doc)
-    filtered_tokens = [w.decode('utf-8') for w in tokens if not w in stop_words]
-    stems = stem_tokens(filtered_tokens, stemmer)
-    return stems
+def get_tokens(text):
+    tokens = nltk.word_tokenize(text)
+    filtered_tokens = [w for w in tokens if not w in stop_words]
+    # stems = stem_tokens(filtered_tokens, stemmer)
+    return filtered_tokens
 
+#filename = "smalltest.txt"
 filename = "parsed_ipg160105.txt"
 
 r = open(filename,'r')
-text = r.read().lower().split("==============")
-length = len(text)
 
-for i in range(length):
-    if (i==length):
-        break;
-    if (i%3) == 0:
-        context = text[i].translate(None,string.punctuation)
-    if (i%3) == 1:
-        nth = text[i].split(':')[0]
-        docid = text[i].split(':')[1]
+splitter14 = "="*14
+splitter38 = "="*38
+
+text = r.read().lower().replace(splitter38,"DOCUMENTSPLITTER").replace(splitter14,"DOCUMENTSPLITTER").replace(":","\n DOCOLLONS \n").translate(None,string.punctuation)
+
+print "Finished Reading!"
+
+tokens = get_tokens(text)
+
+context = []
+check0 = True
+check1 = False
+check2 = False
+
+print "Finished tokenizing!"
+
+for word in tokens:
+    if ("DOCUMENTSPLITTER" in word and check0):
+        nth = word.replace("DOCUMENTSPLITTER","")
+        check1 = True
+        check0 = False
+        continue
+    if check1:
+        check1 = False
+        check2 = True
+        continue
+    if check2:
+        docid = word.replace("DOCUMENTSPLITTER","")
+        check0 = True
+        check2 = False
         docs.append([nth,docid,context])
+        context = []
+        continue
 
-for doc in docs[0:100]:
-    tokens = get_tokens(doc[2])
+    if (not word == 'DOCOLLONS'):
+        context.append(word)
+
+for doc in docs:
+    tokens = doc[2]
     count = Counter(tokens)
     print doc[0] + ":" + doc[1]
-    tfmap = tfmap + count
+    tfmap = tfmap + count # Really Slow Process!!
+
 
 print tfmap
 
