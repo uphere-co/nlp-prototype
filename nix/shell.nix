6001aac5572c71bd3129a45a2f8a43ec6d7d9d11
Ian-Woo Kim <ianwookim@gmail.com>
2016-04-29 00:02:52 +0000

    much faster vector operation using Data.Vector.Storable and direct memcpy

--- nix/shell.nix
+++ nix/shell.nix
@@ -4,6 +4,7 @@ with pkgs;
 
 let hsenv = haskellPackages.ghcWithPackages (p: with p; [
               xml-conduit split unordered-containers data-binary-ieee754
+	      spool
             ]);
 in stdenv.mkDerivation {
      name = "ghc-shell";
