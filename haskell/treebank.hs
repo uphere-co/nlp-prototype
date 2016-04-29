3e91d9043c2937433b9bd1618177d310fe92f86b
Ian-Woo Kim <ianwookim@gmail.com>
2016-04-29 19:02:51 +0000

    move syntax tree parser modules to NLP.SyntaxTree.*

--- haskell/treebank.hs
+++ haskell/treebank.hs
@@ -8,22 +8,20 @@ import           Data.Text                   (Text(..))
 import qualified Data.Text            as T
 import qualified Data.Text.IO         as TIO
 --
-import           Binarize
-import           Parser
-import           Printer
-import           Types
-
-
+import           NLP.SyntaxTree.Binarize
+import           NLP.SyntaxTree.Parser
+import           NLP.SyntaxTree.Printer
+import           NLP.SyntaxTree.Types
 
 main :: IO ()
 main = do
-  txt <- TIO.readFile "parsed.txt"
+  txt <- TIO.readFile "testparsed.txt" -- "parsed.txt"
   let p' = penntree <* A.skipSpace 
   let r = A.parseOnly (A.many1 p') txt
   case r of
     Left err -> print err
     Right lst -> do
-      F.forM_ (take 1 lst) $ \tr -> do
+      F.forM_ (take 100 lst) $ \tr -> do
         print tr 
         putStrLn "-----"
         TIO.putStrLn (treeprinter 0 tr)
