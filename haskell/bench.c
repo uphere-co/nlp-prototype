00f842caaf22f87933cb1a083afe1f7d62ab4b00
Ian-Woo Kim <ianwookim@gmail.com>
2016-04-30 15:40:52 +0000

    C matrix square and trace benchmark. Accelerate outperforms others!

--- haskell/bench.c
+++ haskell/bench.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #define N 100000000
+#define M 1000
 
 void load_file( float dat[] )
 {
@@ -16,9 +17,9 @@ void load_file( float dat[] )
   fclose(file);
 }
 
-int main (int argc, char** argv)
-{
 
+void test_vector_dot( ) 
+{
   static float dat[N];
   load_file( dat );
 
@@ -34,11 +35,39 @@ int main (int argc, char** argv)
     sumtemp = sum+y;
     c = (sumtemp - sum) - y;
     sum = sumtemp;
-    // sum += e*e;
   }
-  //sum = 0;
-  //for( i = 0; i < 10000000 ; i++ ) {
-  //  sum += e[i]*e[i];
-  //}
-  printf("%e",sum);
+  printf("sum = %e\n",sum);
+}
+
+void test_matrix_square_trace()
+{
+  static float dat[N];
+  static float mat[M][M];
+
+  int i,j,k ;
+  float sum; 
+  load_file( dat );
+
+  for (i = 0 ; i < M ; i++) {
+    for(j = 0 ; j < M ; j++) {
+      float s;
+      s =0;
+      for(k = 0; k < M ; k++) {
+	s += dat[i*M+k]*dat[k*M+j];
+      }
+      mat[i][j] = s;
+    }
+  }
+  sum = 0 ;
+  for (i =0 ; i < M ; i++ ) {
+    sum += mat[i][i];
+  }
+  printf("trace = %e\n", sum ); 
+}
+
+int main (int argc, char** argv)
+{
+  //test_vector_dot();
+  test_matrix_square_trace();
+  return 0;
 }
