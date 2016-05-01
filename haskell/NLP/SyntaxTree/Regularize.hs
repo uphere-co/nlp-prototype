496e854dd3233d1606ae6bf13700d143044c089a
Ian-Woo Kim <ianwookim@gmail.com>
2016-05-01 21:05:41 +0000

    a little more regularization. some arbitrary replacement.

--- haskell/NLP/SyntaxTree/Regularize.hs
+++ haskell/NLP/SyntaxTree/Regularize.hs
@@ -5,7 +5,7 @@ module NLP.SyntaxTree.Regularize where
 import           Data.Char      (isDigit)
 import qualified Data.List as L
 import           Data.Text      (Text)
-import qualified Data.Text as T (drop, head, null, splitOn, take, toLower)
+import qualified Data.Text as T (drop, filter, head, null, reverse, splitOn, take, toLower)
 --
 import NLP.SyntaxTree.Type
 --
@@ -16,14 +16,20 @@ isPunctuation x = x `elem` [ ".", "?", "!", ","
                            , "\"", "'", "`"
                            , "''" , "\"\"" , "``"
                            , "\"\" \"\"", "`` ``" , "'' ''"
+                           , "'''"
                            ]
 
 isxRB :: Text -> Bool
 isxRB x = x' == "lrb" || x' == "rrb" where x' = T.toLower x
 
-removeDotFromMrMrs "mr." = "mr"
-removeDotFromMrMrs "mrs." = "mrs"
-removeDotFromMrMrs x = x 
+
+removeApostrophe txt
+  | txt == "'s" = "his"  -- this is very tempororay.
+  | txt == "'ll" = "will"
+  | txt == "'m" = "am"
+  | otherwise = txt
+             
+removeTrailingPunctuation = T.reverse . T.filter (not . (`elem` ['.',',',' '] )  ) . T.reverse 
 
 removeDollarAnnot :: Text -> Text
 removeDollarAnnot txt = if (T.take 2 txt == "$ ") then T.drop 2 txt else txt
@@ -35,15 +41,9 @@ expandComposite :: Text -> BinTree Text
 expandComposite txt = let lst = T.splitOn "-" txt
                       in (L.foldr1 BinNode . map BinLeaf) lst 
 
-{- 
-data NullableBinTree a = Null
-                       | NBTNode (NullableBinTree a) (NullableBinTree a)
-                       | NBTLeaf a
--}
 
 isRemovable :: Text -> Bool
 isRemovable x = T.null x || isPunctuation x || isxRB x
--- isRemovable (BinNode x y) = isRemovable x && isRemovable y
 
 nullify :: BinTree Text -> BNTree Bool (Bool,Text)
 nullify (BinLeaf x) = if isRemovable x then BNTLeaf (True,x) else BNTLeaf (False,x) 
@@ -52,15 +52,13 @@ nullify (BinNode x y) =
         y' = nullify y
     in if (isNulled x' && isNulled y') then BNTNode True x' y' else BNTNode False x' y'
 
--- nullifyOrRegularize (BinNode Null Null) = Null
-
 isNulled :: BNTree Bool (Bool,Text) -> Bool
 isNulled (BNTLeaf (b,x)) = b
 isNulled (BNTNode b _ _) = b
 
 regularize :: BinTree Text -> BinTree Text
-regularize tr = trace (show tr') $ (fmap regtext . remove . nullify . regcomp . fmap replaceNumber) tr
-  where tr' = (nullify . regcomp) tr
+regularize = fmap regtext . remove . nullify . regcomp . fmap replaceNumber
+  where 
         remove (BNTNode False x y)
           | isNulled x = remove y
           | isNulled y = remove x
@@ -71,18 +69,5 @@ regularize tr = trace (show tr') $ (fmap regtext . remove . nullify . regcomp . 
         regcomp (BinLeaf x) = expandComposite x
         regcomp (BinNode x y) = BinNode (regcomp x) (regcomp y)
         
-        regtext = removeDotFromMrMrs . removeDollarAnnot . T.toLower
-
-
-
-{- 
-(BinLeaf x) =  x
-regularize (BinNode y z)
-  | isRemovable y = regularize z
-  | isRemovable z = regularize y
-  | otherwise     = BinNode (regularize y) (regularize z)
--}
--- regularize (BinNode y z)
---     = if isRemovable z then regularize y else BinNode (regularize y) (regularize z)
--- regularize (BinNode y z) = BinNode (regularize y) (regularize z)
+        regtext = {- removeDotFromMrMrs .-} removeApostrophe . removeTrailingPunctuation .  removeDollarAnnot . T.toLower
 
--- haskell/treebank.hs
+++ haskell/treebank.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -7,6 +8,7 @@ import qualified Data.Foldable        as F
 import qualified Data.HashMap.Strict  as HM
 import           Data.Monoid
 import qualified Data.List            as L
+import qualified Data.Set             as Set
 import           Data.Text                   (Text)
 import qualified Data.Text            as T
 import qualified Data.Text.IO         as TIO
@@ -22,36 +24,21 @@ main :: IO ()
 main = do
   txt <- TIO.readFile "LDC2003T05_parsed1.pos" -- "parsed.txt"
   (_,wvm) <- createWordVectorMap "vectors100statmt.bin" -- "vectors100t8.bin"
+  let v_unknown = HM.lookup "unknown" (wvmap wvm)
   let p' = penntree <* A.skipSpace 
   let r = A.parseOnly (A.many1 p') txt
   case r of
     Left err -> print err
     Right lst -> do
-      (rf :: [[Text]] -> [[Text]]) <- (\f -> foldM f id (take 10 lst)) $ \acc tr -> do
-        let btr = (regularize . binarizeR) tr
-        let btr' = flip fmap btr (\w -> (w,HM.lookup w (wvmap wvm)))
-        let (a,b) = (\(a,b) -> (map fst a, map fst b)) . L.partition f . F.toList $ btr'
-                  where f (w,Just _) = True
-                        f (w,Nothing) = False
-        -- TIO.putStrLn (treeprinter 0 tr)
-        TIO.putStrLn (btreePrint [] id btr)
-        print (a,b)
-        return (acc . ( b :) )
-      print (rf [])
-        {- print tr 
-        putStrLn "-----"
-        TIO.putStrLn (treeprinter 0 tr)
-        putStrLn "-----"
-        TIO.putStrLn $ btreePrint [] id btr
-        putStrLn "-----" -}
-        {- 
-        TIO.putStrLn $ btreePrint [] f btr'
-          where f (w,(Just r)) = w <> " : " <> (T.pack . show . V.sum . snd) r
-                f (w,Nothing)  = w <> " : no such word" -}
-{-              
-        putStrLn "-----"
-        let bntr = convert btr
-        TIO.putStrLn  $ bntPrint [] id id bntr
-        putStrLn "=====" 
-        
--}
+      result <- (\f -> foldM f (0,0,Set.empty) (take 100 lst)) $ \acc tr -> do
+        let btr   = (regularize . binarizeR) tr
+        let btr'  = fmap (\w -> (w,HM.lookup w (wvmap wvm))) btr
+            btr'' = fmap (\case (w,Nothing) -> ("unknown",v_unknown) ; x -> x) btr'
+            unknowns = map fst . filter (\case (w,Nothing) -> True; _ -> False) . F.toList $ btr'
+            (passed,failed,unknownset) = acc
+            (passed',failed')
+              | null unknowns = (passed+1,failed)
+              | otherwise     = (passed,failed+1)
+        when ((not.null) unknowns) $ TIO.putStrLn (btreePrint [] id btr)
+        return (passed',failed',L.foldl' (\s x -> Set.insert x s) unknownset unknowns)
+      print result
