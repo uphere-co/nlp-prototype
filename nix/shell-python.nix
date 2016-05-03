2039fd03b19f16b901f43c9f88ac06857c5ce9fc
Ian-Woo Kim <ianwookim@gmail.com>
2016-05-03 16:04:17 +0000

    nix expression now works well with sqlite3 python.

--- nix/shell-python.nix
+++ nix/shell-python.nix
@@ -6,12 +6,25 @@ let toolz = callPackage ./default-python.nix {
               pkgs=pkgs;
               buildPythonPackage = pkgs.python27Packages.buildPythonPackage;
             };
-in (pkgs.python27.buildEnv.override rec {
-      extraLibs =  with pkgs.python27Packages; [ ipython feedparser numpy scipy pip
-      		   sqlite3 scikitlearn dateutil flask tornado toolz.Flask-Limiter toolz.nltk ];
-      shellHook = ''
-          export PYTHONPATH=/nix/store/rc445bsxb245k2xb86y2w6qi1w6k8rpg-python-sqlite3-2.7.11/lib/python2.7/site-packages
-      '';}).env
+in
+stdenv.mkDerivation {
+  name = "python-env";
+  buildInputs = with python27Packages;
+                 [ ipython feedparser numpy scipy pip
+                   sqlite3 scikitlearn dateutil flask
+                   tornado toolz.Flask-Limiter toolz.nltk  ];
+  shellHook = ''
+  '';
+}
+
+
+#(pkgs.python27.buildEnv.override rec {
+#      extraLibs =  with pkgs.python27Packages; [ ipython feedparser numpy scipy pip
+#      		   sqlite3 scikitlearn dateutil flask tornado toolz.Flask-Limiter toolz.nltk ];
+      #shellHook = ''
+      #    export PYTHONPATH=/nix/store/rc445bsxb245k2xb86y2w6qi1w6k8rpg-python-sqlite3-2.7.11/lib/python2.7/site-packages
+      #'';
+#      }).env
 
 
 
