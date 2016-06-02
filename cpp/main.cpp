4566c14a7925256fd8c4a7ce99053d951a8ae6a4
Doh Young Mo <modohyoung@gmail.com>
2016-06-03 01:18:33 +0900

    Stopwords are implemented by using vector<string> and iterator

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -24,11 +24,14 @@ void remove_punctuations(string& str){
 }
 
 void remove_stopwords(vector<string>& tokens){
-  string stopwords[119] = {"a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"}; // 119 + 5
-  for(int i=0;i<119;i++){
-    if(std::find(tokens.begin(), tokens.end(), stopwords[i]) != tokens.end())
-      tokens.erase(std::remove(tokens.begin(), tokens.end(), stopwords[i]), tokens.end());
+  vector<string> stopwords {"a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"}; // 119 stopwords.
+
+  for(vector<string>::const_iterator iter = stopwords.begin(); iter != stopwords.end(); ++iter) {
+    if(std::find(tokens.begin(), tokens.end(), (*iter)) != tokens.end())
+      tokens.erase(std::remove(tokens.begin(), tokens.end(), (*iter)), tokens.end());
+  
   }
+
 }
 
 
