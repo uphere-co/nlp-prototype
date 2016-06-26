b45117d94d95f28d71f3e570be9da25922502816
Ian-Woo Kim <ianwookim@gmail.com>
2016-06-26 02:39:31 +0000

    Hashable instance for Exp

--- paraphrase/autoencode/trie/trie.hs
+++ paraphrase/autoencode/trie/trie.hs
@@ -1,10 +1,12 @@
 -- {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
 import Control.Lens (over, _1)
+import Data.Bits (xor)
 import Data.Function (fix)
--- import Data.Hashable
+import Data.Hashable
 import Data.MemoTrie
 -- import GHC.Generics (Generic)
 
@@ -12,6 +14,7 @@ type Symbol = String
 
 data Exp = Zero
          | One
+         | Val Int
          | Var Symbol
          | Fun1 Symbol Exp
          | Fun2 Symbol Exp Exp
@@ -24,14 +27,19 @@ instance HasTrie Exp where
                               (Symbol :->: b)
                               ((Symbol,Exp) :->: b)
                               ((Symbol,Exp,Exp) :->: b)
+  trie :: (Exp -> b) -> (Exp :->: b)
   trie f = ExpTrie (trie (\() -> f Zero)) (trie (\() -> f One)) (trie (f . Var))
                    (trie (\(s,e)-> f (Fun1 s e))) (trie (\(s,e1,e2)-> f (Fun2 s e1 e2)))
+           
+  untrie :: (Exp :->: b) -> Exp -> b
   untrie (ExpTrie z o v f1 f2) e = case e of
                                      Zero         -> untrie z ()
                                      One          -> untrie o ()
                                      Var s        -> untrie v s
                                      Fun1 s e     -> untrie f1 (s,e)
                                      Fun2 s e1 e2 -> untrie f2 (s,e1,e2)
+                                     
+  enumerate :: (Exp :->: b) -> [(Exp,b)]
   enumerate (ExpTrie z o v f1 f2) = enum' (\()->Zero) z
                                     `weave`
                                     enum' (\()->One) o
@@ -50,6 +58,22 @@ weave :: [a] -> [a] -> [a]
 as `weave` [] = as
 (a:as) `weave` bs = a : (bs `weave` as)
                   
+instance Hashable Exp where
+  hashWithSalt :: Int -> Exp -> Int
+  hashWithSalt s Zero             = s `combine` 0
+  hashWithSalt s One              = s `combine` 1
+  hashWithSalt s (Val n)          = s `combine` 2 `combine` n
+  hashWithSalt s (Var str)        = s `combine` 3 `hashWithSalt` str
+  hashWithSalt s (Fun1 str e)     = s `combine` 4 `hashWithSalt` str `hashWithSalt` e
+  hashWithSalt s (Fun2 str e1 e2) = s `combine` 5 `hashWithSalt` str `hashWithSalt` e1 `hashWithSalt` e2
+
+combine :: Int -> Int -> Int
+combine h1 h2 = (h1 * 16777619) `xor` h2
+  
+  -- hash :: Exp -> Int
+  -- hash 
+
+
 add = Fun2 "+"
 mul = Fun2 "*"
 
@@ -82,6 +106,8 @@ exp1 = square (x `add` y)
 exp2 = power 10 (x `add` y)
 
 
+
+
 fib' :: (Int :->: Int) -> Int -> Int
 fib' t 0 = 0 
 fib' t 1 = 1
@@ -90,10 +116,14 @@ fib' t n = untrie t (n-1) + untrie t (n-2)
 main = do
     mapM_ (putStrLn . prettyPrint) [ exp1, exp2 ]
 
+    mapM_ (print . hash) [exp1, exp2]
+
     -- let t = trie fib
     --    fib = fib' t 
     -- let fib = fib' (trie fib)
+    
     let fib = fix (fib' . trie)
+    
     
     print (fib 50)
     
