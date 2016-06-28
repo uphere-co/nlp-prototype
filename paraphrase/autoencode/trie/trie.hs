9ed90946b0b155f01b785473d831eed100da66f9
Ian-Woo Kim <ianwookim@gmail.com>
2016-06-28 19:17:19 +0000

    simplify when applying and differentiating + and *.

--- paraphrase/autoencode/trie/trie.hs
+++ paraphrase/autoencode/trie/trie.hs
@@ -13,6 +13,7 @@ import           Data.HashMap.Strict       (HashMap)
 import qualified Data.HashMap.Strict as HM
 import           Data.HashSet              (HashSet)
 import qualified Data.HashSet        as HS
+import           Data.Maybe                (fromJust)
 import           Data.MemoTrie
 import           Data.Monoid
 import           Text.Printf
@@ -37,13 +38,34 @@ diff s (ExpMap e m) =
     Val n -> zero
     Var s' -> if s == s' then one else zero
     Fun1 f h1 -> let Just e1 = HM.lookup h1 m
-                 in ExpMap (Fun1 (suffix' f) h1) m `mul` diff s e1
+                 in ExpMap (Fun1 (suffix' f) h1) m `mul'` diff s e1
     Fun2 f h1 h2 -> let Just e1 = HM.lookup h1 m
                         Just e2 = HM.lookup h2 m
-                    in (ExpMap (Fun2 (suffix_1 f) h1 h2) m `mul` diff s e1) `add`
-                       (ExpMap (Fun2 (suffix_2 f) h1 h2) m `mul` diff s e2) 
+                    in (simplify2 m f Pos1 h1 h2 `mul'` diff s e1) `add'`
+                       (simplify2 m f Pos2 h1 h2 `mul'` diff s e2) 
 
+data Pos = Pos1 | Pos2 
 
+justLookup h m = fromJust (HM.lookup h m)
+
+simplify2 m f pos h1 h2
+  | f == "+" = one
+  | f == "*" = case pos of
+                 Pos1 -> justLookup h2 m
+                 Pos2 -> justLookup h1 m
+  | otherwise = case pos of
+                  Pos1 -> ExpMap (Fun2 (suffix_1 f) h1 h2) m
+                  Pos2 -> ExpMap (Fun2 (suffix_2 f) h1 h2) m
+
+add' e1              (ExpMap Zero _) = e1
+add' (ExpMap Zero _) e2              = e2
+add' e1              e2              = e1 `add` e2
+
+mul' e1              (ExpMap Zero _) = zero
+mul' (ExpMap Zero _) e2              = zero
+mul' e1              (ExpMap One  _) = e1
+mul' (ExpMap One  _) e2              = e2
+mul' e1              e2              = e1 `mul` e2 
 
 exp1 :: (?expHash :: Exp :->: Hash) => ExpMap
 exp1 = square (x `add` y)
@@ -85,10 +107,11 @@ main' = do
 main = do
     let ?expHash = trie hash
     let -- (e,m) = exp2
-        e = diff "x" exp2
+        e = diff "x" exp1 -- exp2
         e2 = diff "x" (expfib 100)
     putStrLn . prettyPrint . exp2RExp $ e
-    printf "%x\n" . untrie ?expHash . expMapExp $ e2
+    digraph e
+    -- printf "%x\n" . untrie ?expHash . expMapExp $ e2
 
     -- putStrLn (prettyPrint (fst exp1))
     -- putStrLn (prettyPrint (fst x))
