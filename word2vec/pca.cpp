92f7f505fea70810e5b46b694b53a352f71c522a
Doh Young Mo <modohyoung@gmail.com>
2016-06-28 22:19:57 +0900

    PCA is now calculated by word vector. Utility functions are separated out.

--- word2vec/pca.cpp
+++ word2vec/pca.cpp
@@ -1,80 +1,134 @@
-#include "pca.h"
 #include <iostream>
 #include <random>
 
 #include <boost/tuple/tuple.hpp>
 
+#include "pca.h"
+#include "utils.h"
+
 #include "gnuplot-iostream.h"
 
-using namespace std;
+std::string word_vector_file;
 
-int main() {
+// main function arguments
 
-  const int num_variables = 100; // Dimensions of word vector
-  const int num_records = 300; // Number of vocabularies
+void printHelp() {
+  std::cout << "c++ pca implementation \n\n";
+  std::cout << "Options:\n";
+  std::cout << "Parameters for drawing:\n";
+  std::cout << "\t-word-vector <file>\n";
+  std::cout << "\t\tUse word vector stored in <file>\n";
+  std::cout << "\nExamples:\n";
+  std::cout << "./pca -word-vector data.txt";
   
+}
+
+int ArgPos(char *str, int argc, char **argv) {
+  int i;
+  std::string s_str;
+  std::string s_argv[argc];
+  
+  for(i = 1; i < argc; i++) s_argv[i] = argv[i];
+  s_str = str;
+  
+  for(i = 1; i < argc; i++) if(s_str == s_argv[i]) {
+      if(i == argc - 1) {
+	std::cout << "Argument missing for " << s_str << std::endl;;
+	exit(1);
+      }
+      return i;
+  }
+  return -1;
+}
+
+void ArgPass(int argc, char **argv) {
+  int i;
+  if ((i = ArgPos((char *)"-word-vector", argc, argv)) > 0) word_vector_file = argv[i + 1];
+}
+
+// main function arguments
+
+int main(int argc, char **argv) {
+
+  std::string line;
+  std::vector<std::string> word;
+
+  if(argc == 1) {
+    printHelp();
+    return 0;
+  }
+
+  ArgPass(argc, argv);
+
+  std::ifstream inFile;
+  inFile.open(word_vector_file, std::ifstream::in);
+  if(inFile.fail()) {
+    std::cout << "Word vector file not found!\n";
+    exit(1);
+  }
+
+  getline(inFile, line);
+  split(line, word);
+  int num_records = atoi(word[0].c_str());
+  int num_variables = atoi(word[1].c_str());
+
   stats::pca pca(num_variables);
   pca.set_do_bootstrap(true, 100);
 
-  for (int i=0; i<num_records; ++i) {
-    vector<double> record(num_variables);
-    for (auto value=record.begin(); value!=record.end(); ++value)
-      *value = rand()%20 - 10;
-
+  for(int i = 0; i < num_records; ++i) {
+    word.clear();
+    std::getline(inFile,line);
+    split(line, word);
+    std::vector<double> record(num_variables);
+    for(int j = 0; j < num_variables; j++) record[j] = atof(word[j+1].c_str());
+    
     pca.add_record(record);
   }
-
-  cout<<"Solving ..."<<endl;
   pca.solve();
 
-  cout<<"Energy = "<<pca.get_energy()<<" ("<<
-    stats::utils::get_sigma(pca.get_energy_boot())<<")"<<endl;
+  std::cout<<"Energy = "<<pca.get_energy()<<" ("<<
+    stats::utils::get_sigma(pca.get_energy_boot())<<")"<<std::endl;;
 
   const auto eigenvalues = pca.get_eigenvalues();
-  cout<<"First three eigenvalues = "<<eigenvalues[0]<<", "
+  std::cout<<"First three eigenvalues = "<<eigenvalues[0]<<", "
       <<eigenvalues[1]<<", "
-      <<eigenvalues[2]<<endl;
+	   <<eigenvalues[2]<<std::endl;;
 
-  cout<<"Orthogonal Check = "<<pca.check_eigenvectors_orthogonal()<<endl;
-  cout<<"Projection Check = "<<pca.check_projection_accurate()<<endl;
+  std::cout<<"Orthogonal Check = "<<pca.check_eigenvectors_orthogonal()<<std::endl;;
+  std::cout<<"Projection Check = "<<pca.check_projection_accurate()<<std::endl;;
 
   pca.save("pca_results");
 
-  const vector<double> principal_1 = pca.get_principal(0);
-  const vector<double> principal_2 = pca.get_principal(1);
+  const std::vector<double> principal_1 = pca.get_principal(0);
+  const std::vector<double> principal_2 = pca.get_principal(1);
 
   for(int i = 0 ; i < principal_1.size(); i++)
-    cout << principal_1[i] << "  ";
-  cout << endl;
+    std::cout << principal_1[i] << "  ";
+  std::cout << std::endl;;
 
   for(int i = 0 ; i < principal_2.size(); i++)
-    cout << principal_2[i] << "  ";
-  cout << endl;
+    std::cout << principal_2[i] << "  ";
+  std::cout << std::endl;
   
   // Very simple use case of gnuplot
   Gnuplot gp;
 
-  vector<pair<double, double> > xy_pts_A;
-  vector<pair<double, double> > xy_pts_B;
+  std::vector<std::pair<double, double> > xy_pts_A;
+  std::vector<std::pair<double, double> > xy_pts_B;
 
   for(double x = -2; x <2; x += 0.01) {
     double y = x*x*x;
-    xy_pts_A.push_back(make_pair(x, y));
+    xy_pts_A.push_back(std::make_pair(x, y));
   }
 
   for(double alpha = 0; alpha < 1; alpha += 1.0/24.0) {
     double theta = alpha*2.0*3.14159;
-    xy_pts_B.push_back(make_pair(cos(theta), sin(theta)));
+    xy_pts_B.push_back(std::make_pair(cos(theta), sin(theta)));
   }
 
   gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
   gp << "plot" << gp.file1d(xy_pts_A) << "with lines title 'cubic',"
-     << gp.file1d(xy_pts_B) << "with points title 'circle'" << endl;
-  
-  
-  //
-  
-
+     << gp.file1d(xy_pts_B) << "with points title 'circle'" << std::endl;
   
   return 0;
 }
--- word2vec/word2vec.cpp
+++ word2vec/word2vec.cpp
@@ -1,13 +1,9 @@
-#include <algorithm>
 #include <array>
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
-#include <iterator>
 #include <fstream>
-#include <string>
-#include <vector>
 
 #include <boost/tuple/tuple.hpp>
 
@@ -15,6 +11,8 @@
 #include <string.h>
 
 #include "gnuplot-iostream.h"
+
+#include "utils.h"
 
 #define EXP_TABLE_SIZE 1000
 #define MAX_EXP 6
@@ -68,61 +66,6 @@ std::array<int, table_size> table;
 
 std::vector<double> mine;
 
-// Begin of Tokenizer
-
-void makeLower(std::string& str) {
-  std::transform(str.begin(), str.end(), str.begin(), ::tolower);
-}
-
-
-void remove_punctuations(std::string& str) {
-  std::vector<char> punctuations {'.',',','?',':',';','!'}; // 6 punctuations
-    for(std::vector<char>::const_iterator it = punctuations.begin(); it != punctuations.end(); ++it) {
-      str.erase(std::remove(str.begin(),str.end(), (*it)), str.end());
-   }
-}
-
-
-void remove_stopwords(std::vector<std::string>& tokens) {
-  std::vector<std::string> stopwords {"a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"}; // 119 stopwords.
-
-  for(std::vector<std::string>::const_iterator it = stopwords.begin(); it != stopwords.end(); ++it) {
-    if(std::find(tokens.begin(), tokens.end(), (*it)) != tokens.end())
-      tokens.erase(std::remove(tokens.begin(), tokens.end(), (*it)), tokens.end());  
-  }
-}
-
-
-void Tokenize(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters = " ") {
-  std::string::size_type lastPos = str.find_first_not_of(delimiters, 0);
-  std::string::size_type pos= str.find_first_of(delimiters, lastPos);
-
-  while (std::string::npos != pos || std::string::npos != lastPos) {
-    std::string a = str.substr(lastPos, pos-lastPos);
-    //makeLower(a); // Converting into lowercase characters.
-    //remove_punctuations(a);
-    if(std::find(tokens.begin(), tokens.end(), a) == tokens.end()) // find() returns tokens.end if tarket is not found. 
-      tokens.push_back(a);
-    lastPos = str.find_first_not_of(delimiters, pos);
-    pos = str.find_first_of(delimiters, lastPos);
-  }
-  remove_stopwords(tokens);
-}
-
-void split(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters = " ") {
-  std::string::size_type lastPos = str.find_first_not_of(delimiters, 0);
-  std::string::size_type pos= str.find_first_of(delimiters, lastPos);
-
-  while (std::string::npos != pos || std::string::npos != lastPos) {
-    std::string a = str.substr(lastPos, pos-lastPos);
-    if(std::find(tokens.begin(), tokens.end(), a) == tokens.end()) // find() returns tokens.end if tarket is not found. 
-      tokens.push_back(a);
-    lastPos = str.find_first_not_of(delimiters, pos);
-    pos = str.find_first_of(delimiters, lastPos);
-  }
-}
-
-// End of Tokenizer
 
 
 
@@ -390,7 +333,6 @@ void SaveVocab() {
 
 void ReadVocab() {
   long long a, i = 0;
-  char cline[MAX_STRING];
   std::string line;
   std::vector<std::string> word;
   std::ifstream inFile;
@@ -403,12 +345,10 @@ void ReadVocab() {
   vocab_size = 0;
   while(1) {
     word.clear();
-    inFile.getline(cline,MAX_SENTENCE_LENGTH);
+    std::getline(inFile,line);
     if(inFile.eof()) break;
-    line = cline;
     split(line, word);
     a = AddWordToVocab(word[0]);
-    if(word[0] == "post") std::cout << "hi!";
     std::cout << word[0] << " ";
     std::cout << word[1] << "\n";
     vocab[a].cn = atof(word[1].c_str());
@@ -624,7 +564,7 @@ void TrainModel() {
   TrainModelThread();
   outFile.open(output_file, std::ofstream::out | std::ofstream::binary);
   if(classes == 0) {
-    outFile << vocab_size << "    " << layer1_size << std::endl;
+    outFile << vocab_size << " " << layer1_size << std::endl;
     for(a = 0; a < vocab_size; a++) {
       outFile << vocab[a].word << " ";
       if(binary) for(b = 0; b < layer1_size; b++) outFile << syn0[a * layer1_size + b] << " ";
