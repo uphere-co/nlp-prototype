c79e2ce3a04009ba2cf8c5a39a491928cfe7032a
Doh Young Mo <modohyoung@gmail.com>
2016-06-30 05:47:45 +0900

    gnuplot-iostream is removed.

--- word2vec/pca.cpp
+++ word2vec/pca.cpp
@@ -6,8 +6,6 @@
 #include "pca.h"
 #include "utils.h"
 
-#include "gnuplot-iostream.h"
-
 std::string word_vector_file;
 
 // main function arguments
@@ -106,82 +104,31 @@ int main(int argc, char **argv) {
   std::cout<<"Orthogonal Check = "<<pca.check_eigenvectors_orthogonal()<<std::endl;;
   std::cout<<"Projection Check = "<<pca.check_projection_accurate()<<std::endl;;
   
-
-
   //pca.save("pca_results");
 
   std::vector<double> principal_1 = pca.get_principal(0);
   std::vector<double> principal_2 = pca.get_principal(1);
 
-  double prin1[principal_1.size()];
-  double prin2[principal_2.size()];
-
-  
-  for(int i = 0 ; i < principal_1.size(); i++) {
-    prin1[i] = principal_1[i];
-    prin2[i] = principal_2[i];
-  }
-
-  /* 
-  for(int i = 0 ; i < principal_2.size(); i++)
-    std::cout << principal_2[i] << "  ";
-  std::cout << std::endl;
-  */  
-  
   // Very simple use case of gnuplot
-  Gnuplot gp; // this does something really really strange
   
-  //std::vector< std::pair< std::string, std::pair<double, double> > > xy_pts_A;
   std::vector< boost::tuple<std::string, double, double> > xy_pts_A;
  
   double x, y;
 
-  std::vector<double> tempx;
   for(int i = 0; i < num_records; i++) {
     x = 0;
-    for(int j = 0; j < num_variables; j++) {
-      x += prin1[j] * data[i][j];
-    }
-    tempx.push_back(x);
-  }
-
-  std::vector<double> tempy;
-  for(int i = 0; i < num_records; i++) {
     y = 0;
     for(int j = 0; j < num_variables; j++) {
-      y += prin2[j] * data[i][j];
+      x += principal_1[j] * data[i][j];
+      y += principal_2[j] * data[i][j];
+      std::cout << y << " ";
     }
-    tempy.push_back(y);
-  }
-
-  for(int i = 0; i < num_records; i++) {
-    xy_pts_A.push_back(boost::make_tuple(label[i],tempx[i],tempy[i]));
+    xy_pts_A.push_back(boost::make_tuple(label[i],x,y));
   }
 
 
   
-  /*
-  for(double x = -2; x <2; x += 0.01) {
-    double y = x*x*x;
-    xy_pts_A.push_back(std::make_pair(x, y));
-    }*/
-  /*
-  for(double alpha = 0; alpha < 1; alpha += 1.0/24.0) {
-    double theta = alpha*2.0*3.14159;
-    xy_pts_B.push_back(std::make_pair(cos(theta), sin(theta)));
-  }
-  */
-  
-  //gp << "set xrange [-100:100]\nset yrange [-100:100]\n";
-
-
-
-  gp << "plot" << gp.file1d(xy_pts_A, "temp.dat") << " using 2:3:1 with labels offset 1 title 'word vectors'" << std::endl;
-  gp.send1d(xy_pts_A);
-
-  
-  //gp << "plot" << gp.file1d(xy_pts_A, "temp.dat") << "using 2:3, " << gp.file1d(xy_pts_A, "temp2.dat") << " 2:3:1 with labels offset 1 title 'word vectors'," << std::endl;
-    //<< gp.file1d(xy_pts_B) << "with points title 'circle'" << std::endl;
+  //  gp << "plot" << gp.file1d(xy_pts_A, "temp.dat") << " using 2:3:1 with labels offset 1 title 'word vectors'" << std::endl;
   
   return 0;
 }
