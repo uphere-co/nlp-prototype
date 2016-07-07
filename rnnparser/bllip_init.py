ff2084d7b1a7706d70da3d07eb5ea7e24f92f027
Jihun Kim <jihun@uphere.co>
2016-07-07 14:47:59 +0000

    Initialization script for BLLIP parser.

---
+++ rnnparser/bllip_init.py
@@ -0,0 +1,6 @@
+from bllipparser import ModelFetcher
+ModelFetcher.list_models()
+ModelFetcher.download_and_install_model("WSJ+Gigaword-v2")
+ModelFetcher.download_and_install_model("WSJ-PTB3")
+ModelFetcher.download_and_install_model("SANCL2012-Uniform")
+
