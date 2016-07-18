953003dc8dfa32899c8094037d65899e002c5b52
Jihun Kim <jihun@uphere.co>
2016-07-18 21:45:16 +0000

    Clean-up tests.

--- rnnparser/RecursiveNN/tests_npRNN/test_merge_info.py
+++ rnnparser/RecursiveNN/tests_npRNN/test_merge_info.py
@@ -11,86 +11,40 @@ def test_toymodel():
         return np.concatenate([word1,word2])
 
     #Define data layout
-    words=np.empty((15,5))
-    wpairs=np.empty((24,10))
-    words[:8]=np.tile([2**i for i in range(8)],(5,1)).T
-    wpairs[:7]= np.concatenate([words[:7],words[1:8]], axis=1)
     #Start from 8 words.
-    idxs_word = range(8)
-    idxs_wpair = range(7)
-    it_word=8
-    it_wpair=7
-    words_current =words[idxs_word]
-    wpairs_current=wpairs[idxs_wpair]
-    loc=3
-    h=phrase(wpairs_current[loc])
-    it_word=update_current_words(words, idxs_word, loc, h, it_word)
-    it_wpair=update_current_word_pairs(words,idxs_word, wpairs,idxs_wpair, loc,h, it_wpair)
-    assert it_word == 9
-    assert it_wpair== 9
-    assert idxs_word  == [0,1,2,8,5,6,7]
-    assert idxs_wpair == [0,1,7,8,5,6]
-    words_current =words[idxs_word]
-    wpairs_current=wpairs[idxs_wpair]
-    assert np.all(np.concatenate([words_current[:-1],words_current[1:]], axis=1)== wpairs_current)
+    n_words=8
+    dim_word=5
+    words=np.empty((n_words*2-1,dim_word))
+    wpairs=np.empty((n_words*3,dim_word*2))
+    words[:n_words]=np.tile([2**i for i in range(n_words)],(dim_word,1)).T
+    wpairs[:n_words-1]= np.concatenate([words[:n_words-1],words[1:n_words]], axis=1)
+    idxs_word = range(n_words)
+    idxs_wpair = range(n_words-1)
+    it_word=n_words
+    it_wpair=n_words-1
 
-    loc=1
-    h=phrase(wpairs_current[loc])
-    it_word=update_current_words(words, idxs_word, loc, h, it_word)
-    it_wpair=update_current_word_pairs(words,idxs_word, wpairs,idxs_wpair, loc,h, it_wpair)
-    assert it_word == 10
-    assert it_wpair== 11
-    assert idxs_word  == [0,9,8,5,6,7]
-    assert idxs_wpair == [9,10,8,5,6]
-    words_current =words[idxs_word]
-    wpairs_current=wpairs[idxs_wpair]
-    assert np.all(np.concatenate([words_current[:-1],words_current[1:]], axis=1)== wpairs_current)
+    merge_history=[3,1,1,0,2,1]
 
-    loc=1
-    h=phrase(wpairs_current[loc])
-    it_word=update_current_words(words, idxs_word, loc, h, it_word)
-    it_wpair=update_current_word_pairs(words,idxs_word, wpairs,idxs_wpair, loc,h, it_wpair)
-    assert it_word == 11
-    assert it_wpair== 13
-    assert idxs_word  == [0,10,5,6,7]
-    assert idxs_wpair == [11,12,5,6]
-    words_current =words[idxs_word]
-    wpairs_current=wpairs[idxs_wpair]
-    assert np.all(np.concatenate([words_current[:-1],words_current[1:]], axis=1)== wpairs_current)
+    expected_it_word  = [9,10,11,12,13,14]
+    expected_it_wpair = [9,11,13,14,15,16]
+    expected_idxs_word = [[0,1,2,8,5,6,7], [0,9,8,5,6,7], [0,10,5,6,7],\
+                          [11,5,6,7], [11,5,12],[11,13]]
+    expected_idxs_wpair = [[0,1,7,8,5,6], [9,10,8,5,6],\
+                           [11,12,5,6], [13,5,6], [13,14], [15]]
 
-    loc=0
-    h=phrase(wpairs_current[loc])
-    it_word=update_current_words(words, idxs_word, loc, h, it_word)
-    it_wpair=update_current_word_pairs(words,idxs_word, wpairs,idxs_wpair, loc,h, it_wpair)
-    assert it_word == 12
-    assert it_wpair== 14
-    assert idxs_word  == [11,5,6,7]
-    assert idxs_wpair == [13,5,6]
-    words_current =words[idxs_word]
-    wpairs_current=wpairs[idxs_wpair]
-    assert np.all(np.concatenate([words_current[:-1],words_current[1:]], axis=1)== wpairs_current)
+    for epoch in range(6):
+        loc=merge_history[epoch]
+        words_current =words[idxs_word]
+        wpairs_current=wpairs[idxs_wpair]
+        h=phrase(wpairs_current[loc])
+        it_word=update_current_words(words, idxs_word, loc, h, it_word)
+        it_wpair=update_current_word_pairs(words,idxs_word, wpairs,idxs_wpair, loc,h, it_wpair)
+        assert it_word == expected_it_word[epoch]
+        assert it_wpair== expected_it_wpair[epoch]
+        assert idxs_word  == expected_idxs_word[epoch]
+        assert idxs_wpair == expected_idxs_wpair[epoch]
+        words_current =words[idxs_word]
+        wpairs_current=wpairs[idxs_wpair]
+        assert np.all(np.concatenate([words_current[:-1],words_current[1:]], axis=1)== wpairs_current)
 
-    loc=2
-    h=phrase(wpairs_current[loc])
-    it_word=update_current_words(words, idxs_word, loc, h, it_word)
-    it_wpair=update_current_word_pairs(words,idxs_word, wpairs,idxs_wpair, loc,h, it_wpair)
-    assert it_word == 13
-    assert it_wpair== 15
-    assert idxs_word  == [11,5,12]
-    assert idxs_wpair == [13,14]
-    words_current =words[idxs_word]
-    wpairs_current=wpairs[idxs_wpair]
-    assert np.all(np.concatenate([words_current[:-1],words_current[1:]], axis=1)== wpairs_current)
-
-    loc=1
-    h=phrase(wpairs_current[loc])
-    it_word=update_current_words(words, idxs_word, loc, h, it_word)
-    it_wpair=update_current_word_pairs(words,idxs_word, wpairs,idxs_wpair, loc,h, it_wpair)
-    assert it_word == 14
-    assert it_wpair== 16
-    assert idxs_word  == [11,13]
-    assert idxs_wpair == [15]
-    words_current =words[idxs_word]
-    wpairs_current=wpairs[idxs_wpair]
-    assert np.all(np.concatenate([words_current[:-1],words_current[1:]], axis=1)== wpairs_current)
     assert np.all(wpairs_current == np.array([[31]*5+[224]*5]))
