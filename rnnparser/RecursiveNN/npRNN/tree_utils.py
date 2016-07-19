e88d36e43e276b4670a7974619d153514dabbd54
Jihun Kim <jihun@uphere.co>
2016-07-19 07:17:33 +0000

    NodeTree now provides merge direction info.

--- rnnparser/RecursiveNN/npRNN/tree_utils.py
+++ rnnparser/RecursiveNN/npRNN/tree_utils.py
@@ -1,24 +1,27 @@
-class Vec(object):
+class Node(object):
     @classmethod
     def merge(cls,wordL, wordR, epoch):
-        vec = Vec('(%s %s)'%(wordL.name,wordR.name))
-        vec.left = wordL
-        vec.right = wordR
-        vec.depth = max(wordL.depth, wordR.depth)+1
-        vec.epoch = epoch
-        return vec
+        node = cls('(%s %s)'%(wordL.name,wordR.name))
+        node.left = wordL
+        node.left.parent=node
+        node.right = wordR
+        node.right.parent=node
+        node.depth = max(wordL.depth, wordR.depth)+1
+        node.epoch = epoch
+        return node
     def __init__(self, word):
         self.depth=0
         self.name=word
         self.left=None
         self.right=None
+        self.parent=None
         self.epoch=None
     def __unicode__(self):
         return self.name
     def __repr__(self):
         return self.__unicode__()
     def isPhrase(self):
-        return vec.epoch is not None
+        return self.epoch is not None
     def iter(self):
         if self.depth==0:
             return []
@@ -30,34 +33,52 @@ class Vec(object):
         if self.right is not None:
             yield self.right.iter2()
 
-class VecTree(object):
+class NodeTree(object):
     @classmethod
-    def random_merge(cls,words):
+    def random_merge(cls,word_nodes):
         words=list(words)
         merge_history=[]
         epoch=0
+        new_words=[]
         while len(words)>1:
             idx_beg=np.random.choice(range(len(words)-1))
             merge_history.append(idx_beg)
-            idx_end=idx_beg+2
             wordL,wordR=words[idx_beg],words[idx_beg+1]
-            words[idx_beg:idx_end]= [Vec.merge(wordL,wordR, epoch)]
+            new_word = Node.merge(wordL,wordR, epoch)
+            words[idx_beg:idx_beg+2]= [new_word]
+            new_words.append(new_word)
             epoch+=1
-        return words[0], merge_history
+        return new_words, merge_history
     @classmethod
     def directed_merge(cls,words, merge_history):
         merge_history=list(merge_history)
         words=list(words)
         epoch=0
+        new_words=[]
         for idx_beg in merge_history:
-            idx_end=idx_beg+2
             wordL,wordR=words[idx_beg],words[idx_beg+1]
-            words[idx_beg:idx_end]= [Vec.merge(wordL,wordR, epoch)]
+            new_word = Node.merge(wordL,wordR, epoch)
+            words[idx_beg:idx_beg+2]= [new_word]
+            new_words.append(new_word)
             epoch+=1
-        return words[0]
+        return new_words, merge_history
+    @classmethod
+    def get_merge_direction(cls,composites):
+        left_merged=[[] for _ in composites]
+        for i,node in enumerate(composites):
+            left_merged[i].append(True)
+            parent=node.parent
+            while parent is not None:
+                if parent.left is node:
+                    left_merged[i].append(True)
+                else:
+                    left_merged[i].append(False)
+                parent,node=parent.parent,parent
+        return left_merged
     def __init__(self, words, history=None):
         if history is None:
-            self.phrase, self.history = VecTree.random_merge(words)
+            new_words, merge_history = NodeTree.random_merge(words)
         else:
-            self.history=history
-            self.phrase = VecTree.directed_merge(words, self.history)
+            new_words, merge_history = NodeTree.directed_merge(words, history)
+        self.history=merge_history
+        self.phrase=new_words[-1]
--- rnnparser/RecursiveNN/tests_npRNN/test_tree_utils.py
+++ rnnparser/RecursiveNN/tests_npRNN/test_tree_utils.py
@@ -1,22 +1,35 @@
 import pytest
 
-from npRNN.tree_utils import Vec, VecTree
+from npRNN.tree_utils import Node, NodeTree
 def test_merge_results():
     #sentence='I know a name of the cat on a hat'
     sentence='a name of the cat on a hat'
-    words=[Vec(word) for word in sentence.split()]
+    words=[Node(word) for word in sentence.split()]
 
-    tree=VecTree(words, [0, 5, 3, 1, 2, 0, 0])
+    tree=NodeTree(words, [0, 5, 3, 1, 2, 0, 0])
     assert tree.phrase.name =='(((a name) (of the)) ((cat on) (a hat)))'
     assert tree.phrase.depth==3
     assert tree.history == [0, 5, 3, 1, 2, 0, 0]
 
-    tree=VecTree(words, [0, 5, 0, 0, 1, 1, 0])
+    tree=NodeTree(words, [0, 5, 0, 0, 1, 1, 0])
     assert tree.phrase.name =='((((a name) of) the) ((cat on) (a hat)))'
     assert tree.phrase.depth==4
     assert tree.history == [0, 5, 0, 0, 1, 1, 0]
 
-    tree=VecTree(words, [2,0,3,2,2,0,0])
+    tree=NodeTree(words, [2,0,3,2,2,0,0])
     assert tree.phrase.name =='(((a name) (of the)) ((cat (on a)) hat))'
     assert tree.phrase.depth==4
     assert tree.history == [2,0,3,2,2,0,0]
+
+def test_merge_dicrection():
+    sentence='a name of the cat on a hat'
+    words=[Node(word) for word in sentence.split()]
+    merge_history=[3,1,1,0,2,1,0]
+    composites, _ =NodeTree.directed_merge(words,merge_history)
+    left_merged=NodeTree.get_merge_direction(composites)
+
+    expected_left_merged = [[True, False, False, True],[True, True, False, True],\
+        [True, False, True],[True, True],[True, False, False],[True, False],[True]]
+    assert left_merged == expected_left_merged
+    depths = [x.depth for x in composites]
+    assert depths==[1, 1, 2, 3, 1, 2, 4]
