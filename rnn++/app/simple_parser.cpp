73ccf41f6fa122c61ab93dab72b071268e05d002
Jihun Kim <jihun@uphere.co>
2016-07-25 05:36:58 +0000

    Cleanup code.

--- rnn++/app/simple_parser.cpp
+++ rnn++/app/simple_parser.cpp
@@ -5,20 +5,20 @@
 #include"parser/voca.h"
 
 using namespace rnn::io;
-using namespace rnn::parser;
+using namespace rnn::parser::wordrep;
 
 int main(){
    H5name file_name{"data.h5"};
    H5name voca_name{"1b.model.voca"}, w2vmodel_name{"1b.model"};
-   H5file file{file_name};
    //voca_max_word_len can be read using `h5dump -H` command.
    //It can be directly read from a H5File,
    //but it needs knowledge of low level details of HDF5.
    int voca_max_word_len = 74;
+
+   H5file file{file_name};
    Voca voca{file.getRawData<char>(voca_name), voca_max_word_len};
    for(int i=0; i<voca.size(); ++i){
-      std::string word = voca.getWord(i);
-      std::cout << word <<std::endl;
+      std::cout << voca.getWord(i) <<std::endl;
    }
 
    return 0;
--- rnn++/parser/voca.h
+++ rnn++/parser/voca.h
@@ -6,19 +6,24 @@
 
 namespace rnn{
 namespace parser{
-struct Voca{
+namespace wordrep{
+
+class Voca{
+public:
    Voca(std::vector<char> raw_data, int max_word_len)
    : val{raw_data}, max_word_len{max_word_len}, voca_size{raw_data.size()/max_word_len}{}
-   std::string getWord(int idx){
+   std::string const getWord(int idx){
       std::string word{val.data()+idx*max_word_len, 74};
       return std::string{word.c_str()};
    }
-   int size() {return voca_size;}
+   int size() const {return voca_size;}
+private:
    std::vector<char> val;
    int max_word_len;
    size_t voca_size;
 };
 
+}//namespace rnn::wordrep
 }//namespace rnn::parser
 }//namespace rnn
 
