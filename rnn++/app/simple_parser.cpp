4bce226366bb94ae20b333e91fd703873e5a8920
Jihun Kim <jihun@uphere.co>
2016-07-25 06:08:10 +0000

    Words in vocabulary are indexed in VocaIndex.

--- rnn++/app/simple_parser.cpp
+++ rnn++/app/simple_parser.cpp
@@ -17,7 +17,11 @@ int main(){
 
    H5file file{file_name};
    Voca voca{file.getRawData<char>(voca_name), voca_max_word_len};
-   for(int i=0; i<voca.size(); ++i){
+   auto word2idx = voca.indexing();
+   for(size_t i=0; i<voca.size(); ++i){
+      assert(word2idx.getIndex(voca.getWord(i)) == i);
+   }
+   for(size_t i=0; i<voca.size(); ++i){
       std::cout << voca.getWord(i) <<std::endl;
    }
 
--- rnn++/parser/voca.h
+++ rnn++/parser/voca.h
@@ -3,20 +3,38 @@
 
 #include<string>
 #include<vector>
+#include<map>
 
 namespace rnn{
 namespace parser{
 namespace wordrep{
 
+using word2idx_type = std::map<std::string, size_t>;
+
+class VocaIndex{
+public:
+   VocaIndex(word2idx_type const &word2idxs) : val{word2idxs}{}
+   size_t getIndex(std::string word) const {return val.find(word)->second;}
+private:
+   std::map<std::string, size_t> val;
+};
+
 class Voca{
 public:
    Voca(std::vector<char> raw_data, int max_word_len)
    : val{raw_data}, max_word_len{max_word_len}, voca_size{raw_data.size()/max_word_len}{}
-   std::string const getWord(int idx){
+   std::string getWord(int idx) const {
       std::string word{val.data()+idx*max_word_len, 74};
       return std::string{word.c_str()};
    }
-   int size() const {return voca_size;}
+   VocaIndex indexing() const{
+      word2idx_type word_to_idx;
+      for(size_t i=0; i<this->size(); ++i){
+         word_to_idx[this->getWord(i)]=i;
+      }
+      return VocaIndex{word_to_idx};
+   }
+   size_t size() const {return voca_size;}
 private:
    std::vector<char> val;
    int max_word_len;
