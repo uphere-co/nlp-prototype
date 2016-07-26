24e2c1b9b574fb7a470f13b11aa3111d5c18afbc
Jihun Kim <jihun@uphere.co>
2016-07-26 07:35:16 +0000

    Change indentation.

--- rnn++/app/simple_parser.cpp
+++ rnn++/app/simple_parser.cpp
@@ -8,23 +8,23 @@ using namespace rnn::io;
 using namespace rnn::parser::wordrep;
 
 int main(){
-   H5name file_name{"data.h5"};
-   H5name voca_name{"1b.model.voca"}, w2vmodel_name{"1b.model"};
-   //voca_max_word_len can be read using `h5dump -H` command.
-   //It can be directly read from a H5File,
-   //but it needs knowledge of low level details of HDF5.
-   int voca_max_word_len = 74;
+    H5name file_name{"data.h5"};
+    H5name voca_name{"1b.model.voca"}, w2vmodel_name{"1b.model"};
+    //voca_max_word_len can be read using `h5dump -H` command.
+    //It can be directly read from a H5File,
+    //but it needs knowledge of low level details of HDF5.
+    int voca_max_word_len = 74;
 
-   H5file file{file_name};
-   Voca voca{file.getRawData<char>(voca_name), voca_max_word_len};
-   VocaIndex word2idx = voca.indexing();
-   for(size_t i=0; i<voca.size(); ++i){
+    H5file file{file_name};
+    Voca voca{file.getRawData<char>(voca_name), voca_max_word_len};
+    VocaIndex word2idx = voca.indexing();
+    for(size_t i=0; i<voca.size(); ++i){
       assert(word2idx.getIndex(voca.getWord(i)) == i);
-   }
-   for(size_t i=0; i<voca.size(); ++i){
+    }
+    for(size_t i=0; i<voca.size(); ++i){
       std::cout << voca.getWord(i) <<std::endl;
-   }
+    }
 
-   return 0;
+    return 0;
 }
 //
--- rnn++/parser/voca.h
+++ rnn++/parser/voca.h
@@ -13,47 +13,47 @@ namespace parser{
 namespace wordrep{
 
 struct Word{
-   Word(gsl::cstring_span<> word) : span{word}{}
-   bool operator<(const Word &word)  const { return this->span < word.span;}
-   gsl::cstring_span<> span;
+    Word(gsl::cstring_span<> word) : span{word}{}
+    bool operator<(const Word &word)  const { return this->span < word.span;}
+    gsl::cstring_span<> span;
 };
 std::ostream& operator<<(std::ostream& os, const Word& obj) {
-   os<<gsl::to_string(obj.span).c_str();
-   return os;
+    os<<gsl::to_string(obj.span).c_str();
+    return os;
 }
 
 using word2idx_type = std::map<Word, size_t>;
 class VocaIndex{
 public:
-   VocaIndex(word2idx_type const &word2idxs) : val{word2idxs}{}
-   size_t getIndex(Word word) const {return val.find(word)->second;}
+    VocaIndex(word2idx_type const &word2idxs) : val{word2idxs}{}
+    size_t getIndex(Word word) const {return val.find(word)->second;}
 private:
-   std::map<Word, size_t> val;
+    std::map<Word, size_t> val;
 };
 
 class Voca{
 public:
-   Voca(std::vector<char> raw_data, int max_word_len)
-   : _val{raw_data}, span{_val}, max_word_len{max_word_len},
-     voca_size{raw_data.size()/max_word_len}{}
-   Word getWord(int idx) const {
-      // std::string word{_val.data()+idx*max_word_len, 74};
-      gsl::cstring_span<> word = span.subspan(idx*max_word_len, max_word_len);
-      return Word{word};
-   }
-   VocaIndex indexing() const{
-      word2idx_type word_to_idx;
-      for(size_t i=0; i<this->size(); ++i){
-         word_to_idx[this->getWord(i)]=i;
-      }
-      return VocaIndex{word_to_idx};
-   }
-   size_t size() const {return voca_size;}
+    Voca(std::vector<char> raw_data, int max_word_len)
+    : _val{raw_data}, span{_val}, max_word_len{max_word_len},
+    voca_size{raw_data.size()/max_word_len}{}
+    Word getWord(int idx) const {
+        // std::string word{_val.data()+idx*max_word_len, 74};
+        gsl::cstring_span<> word = span.subspan(idx*max_word_len, max_word_len);
+        return Word{word};
+    }
+    VocaIndex indexing() const{
+        word2idx_type word_to_idx;
+        for(size_t i=0; i<this->size(); ++i){
+            word_to_idx[this->getWord(i)]=i;
+        }
+        return VocaIndex{word_to_idx};
+    }
+    size_t size() const {return voca_size;}
 private:
-   std::vector<char> _val;
-   gsl::cstring_span<> span;
-   int max_word_len;
-   size_t voca_size;
+    std::vector<char> _val;
+    gsl::cstring_span<> span;
+    int max_word_len;
+    size_t voca_size;
 };
 
 }//namespace rnn::wordrep
