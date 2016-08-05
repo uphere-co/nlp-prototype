74f8844707d95042abd18837f5869e8903e160d7
Jihun Kim <jihun@uphere.co>
2016-08-05 02:38:12 +0000

    Conversion between 1-D and 2-D memory view.

--- rnn++/utils/linear_algebra.h
+++ rnn++/utils/linear_algebra.h
@@ -4,48 +4,61 @@
 
 #include<gsl.h>
 
+#include"utils/math.h"
+
 namespace util{
 namespace math{
 
-template<typename T, std::size_t M>
-auto to_span(std::array<T,M> &val){
-    return gsl::span<T,M>{val};
-}
-template<typename T, std::size_t M>
-auto to_cspan(std::array<T,M> const &val){
-    return gsl::span<const T,M>{val};
-}
-template<typename T>
-auto to_span(std::vector<T> &val){
-    return gsl::span<T>{val};
-}
-template<typename T>
-auto to_cspan(std::vector<T> const &val){
-    return gsl::span<const T>{val};
-}
-
+//TODO: should support move from existing container?
 template<typename T, int64_t M>
 struct Vector{
-    Vector(gsl::span<const T, M> vals) : dim{M}, _val(M) {
+    Vector(gsl::span<T, M> const vals) : _val(M) {
         std::cerr<<M<<" Vector{gsl::span<M>}\n";
         std::copy_n(vals.cbegin(), dim, _val.begin());
     }
-    Vector() : _val(M) {}
-    const int64_t dim;
+    const int64_t dim=M;
     std::vector<T> _val;
     gsl::span<T, M> span=_val;
-    gsl::span<const T, M> cspan=_val;
 };
-
 template<int64_t M, int64_t N>
 struct Matrix{
     Matrix(std::array<const float_t, M*N> vals) : _val(M*N) {
+        std::cerr<<M<<" "<<N<<" Matrix{gsl::span<M,N>}\n";
         std::copy_n(vals.begin(), M*N, _val.begin());
     }
     Matrix() : _val(M*N) {}
     std::vector<float_t> _val;
     gsl::span<const float_t, M, N> span=_val;
 };
+
+namespace factory{
+
+//TODO: val can be const &
+template<typename T, std::size_t M>
+auto Vector(std::array<T,M> &val){
+    const auto span = gsl::as_span(val);
+    return util::math::Vector<T,M>{span};
+}
+
+// template<typename T>
+// auto Vector(std::vector<T> const &val){
+//     constexpr auto span = gsl::span<const T>{val};
+//     return util::math::Vector<T,span.size()>{span};
+// }
+
+// template<typename T>
+// auto Vector(std::vector<T> const &val){
+//     return gsl::span<const T>{val};
+// }
+//
+// template<typename T>
+// auto Vector(T vals){
+//     auto a = util::math::Vector{to_cspan(vals)};
+//     return a;
+// }
+//
+
+}//namespace util::math::factory
 
 }//namespace util::math
 }//namespace util
--- rnn++/utils/math.h
+++ rnn++/utils/math.h
@@ -6,11 +6,30 @@
 
 namespace util{
 namespace math{
-auto sum(gsl::span<const float> vec){
-    return std::accumulate(vec.cbegin(), vec.cend(), decltype(vec)::value_type{0});
+
+template<typename T, std::size_t M>
+auto to_span(std::array<T,M> &val){
+    return gsl::span<T,M>{val};
 }
-auto sum(gsl::span<const double> vec){
-    return std::accumulate(vec.cbegin(), vec.cend(), decltype(vec)::value_type{0});
+template<typename T>
+auto to_span(std::vector<T> &val){
+    return gsl::span<T>{val};
+}
+
+template<typename T>
+auto sum(gsl::span<T> const vec){
+    return std::accumulate(vec.cbegin(), vec.cend(), T{0});
+}
+template<typename T, int64_t M>
+auto sum(gsl::span<T,M> const vec){
+    std::cerr << "sum<T,M> "<< M << std::endl;
+    return std::accumulate(vec.cbegin(), vec.cend(), T{0});
+}
+template<typename T, int64_t M,int64_t N>
+auto sum(gsl::span<T,M,N> const mat){
+    std::cerr << "sum<T,M,N> "<< M << " " << N << std::endl;
+    auto flat = mat.subspan(0,M*N);
+    return sum(flat);
 }
 }//namespace util::math
 }//namespace util
