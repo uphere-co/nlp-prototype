ac67659ca161f394e015f4ba9e272d7936d92d04
Jihun Kim <jihun@uphere.co>
2016-08-07 06:14:02 +0000

    Messy, but working RNN parser forward path.

--- rnn++/app/simple_parser.cpp
+++ rnn++/app/simple_parser.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
-#include<cassert>
+#include <iostream>
+#include <cassert>
+#include <algorithm> //forward path
+#include <chrono> //profiling
 
-#include"utils/hdf5.h"
-#include"utils/math.h"
-#include"utils/linear_algebra.h"
-#include"parser/voca.h"
-#include"parser/wordvec.h"
-#include"parser/simple_model.h"
-#include"parser/config.h"
+#include "utils/hdf5.h"
+#include "utils/math.h"
+#include "utils/linear_algebra.h"
+#include "parser/voca.h"
+#include "parser/wordvec.h"
+#include "parser/simple_model.h"
+#include "parser/config.h"
 
 
 
@@ -48,11 +50,11 @@ void test_init_rnn(){
     auto param_raw = param_storage.getRawData<rnn_t::float_t>(rnn_param_name);
     // auto span = gsl::span<rnn_t::float_t>{param_raw};
     rnn_model::Param param = rnn_model::deserializeParam(param_raw);
-    std::cerr << sum(param.w_left.span)+sum(param.w_right.span) << std::endl;
-    std::cerr << sum(param.bias.span) << std::endl;
-    std::cerr << sum(param.u_score.span) << std::endl;
-    std::cerr << dotdot(param.u_score, param.w_left, param.bias) << std::endl;
-    std::cerr << dotdot(param.u_score, param.w_right, param.bias) << std::endl;
+    std::cerr << "Test:   3.248616=="<< sum(param.w_left.span)+sum(param.w_right.span) << std::endl;
+    std::cerr << "Test: -50.581345=="<< sum(param.bias.span) << std::endl;
+    std::cerr << "Test:  -0.190589=="<< sum(param.u_score.span) << std::endl;
+    std::cerr << "Test:   0.675059=="<<dotdot(param.u_score, param.w_left, param.bias) << std::endl;
+    std::cerr << "Test:   1.5551  =="<< dotdot(param.u_score, param.w_right, param.bias) << std::endl;
     // auto w_flat = gsl::as_span(w.data(), w.extent(0)*w.extent(1));
 }
 void test_read_voca(){
@@ -84,16 +86,93 @@ int main(){
         WordBlock voca_vecs{file.getRawData<rnn_t::float_t>(w2vmodel_name), word_dim};
         VocaIndexMap word2idx = voca.indexing();
 
+        auto t_start = std::chrono::high_resolution_clock::now();
+
         auto sentence = u8"A symbol of\tBritish pound is £ .";
         auto idxs = word2idx.getIndex(sentence);
         auto word_block = voca_vecs.getWordVec(idxs);
         std::cerr << sum(word_block.span) << std::endl;
 
-        using rnn_model::compute::merge_to_phrase;
-        auto phrase = merge_to_phrase(param.w_left, param.w_right, param.bias,
-                                      word_block.getWordVec(2),word_block.getWordVec(3));
-        std::cerr <<"58.3763 =="<<dot(word_block[2], word_block[3])<<std::endl;
-        std::cerr <<"0.98579389 == "<< dot(param.u_score.span, phrase.span) << std::endl;
+        auto t_load = std::chrono::high_resolution_clock::now();
+
+        {
+            auto t_start = std::chrono::high_resolution_clock::now();
+            std::vector<float> test(100000);
+            auto t_end = std::chrono::high_resolution_clock::now();
+            std::cerr << "Allocate vector of size "<<test.size()<<" : "<< std::chrono::duration<double, std::milli>(t_end-t_start).count() << std::endl;
+        }
+
+        auto merge_to_phrase=[&](auto const word_left, auto const word_right){
+            return rnn_model::compute::merge_to_phrase(
+                        param.w_left, param.w_right, param.bias,
+                        word_left,word_right);
+        };
+        // auto phrase = merge_to_phrase(word_block[2],word_block[3]);
+        // std::cerr <<"Test: 58.3763     =="<<dot(word_block[2], word_block[3])<<std::endl;
+        // std::cerr <<"Test:  0.98579389 == "<< dot(param.u_score.span, phrase.span) << std::endl;
+        using word_t = decltype(param.bias);
+        std::vector<word_t> phrases;//WordBlock{word_block.word_dim};
+        std::vector<WordBlock::wordspan_t> current_words;
+        std::vector<rnn_t::float_t> scores;
+        std::vector<rnn_t::idx_t> idx;
+
+
+        auto n=word_block.size();
+        for(decltype(n) i=0; i<n; ++i)
+            current_words.push_back(word_block[i]);
+        for(decltype(n) i=0; i<n-1; ++i){
+            auto phrase = merge_to_phrase(word_block[i],word_block[i+1]);
+            phrases.push_back(std::move(phrase));
+            idx.push_back(i);
+        }
+        for(auto i:idx)
+            scores.push_back(dot(param.u_score.span, phrases[i].span));
+        while(idx.size()){
+            auto max_score=std::max_element(scores.cbegin(), scores.cend());
+
+            // for(auto const x: current_words)
+            //     std::cerr<<sum(x) << " ";
+            // std::cerr<<std::endl;
+
+            //i : score index
+            //idx : word_index
+            auto i_max = max_score-scores.cbegin();
+            auto idx_max = idx[i_max];
+            // std::cerr<<current_words.size()<<" Score: "<<*max_score<<", i_max : "<<i_max<<", idx_max : "<<idx_max<<"\n";
+            auto new_word=phrases[idx_max];
+            current_words[i_max]=new_word.span;
+            std::copy(current_words.cbegin()+i_max+2, current_words.cend(), current_words.begin()+i_max+1);
+            current_words.pop_back();
+            if(max_score!=scores.cbegin()){
+                auto i_left = i_max-1;
+                auto idx_left=idx[i_left];
+                auto new_phrase = merge_to_phrase(current_words[i_left], new_word.span);
+                phrases[idx_left]=std::move(new_phrase);
+                scores[i_left] =dot(param.u_score.span, new_phrase.span);
+                // std::cerr<<idx_left<<" New left score : " << scores[i_left]<<"\n";
+            }
+            if(max_score!=scores.cend()-1){
+                auto i_right = i_max+1;
+                auto idx_right=idx[i_right];
+                auto new_phrase = merge_to_phrase(new_word.span, current_words[i_right]);
+                phrases[idx_right]=std::move(new_phrase);
+                scores[i_right] =dot(param.u_score.span, new_phrase.span);
+                // std::cerr<<idx_right<<" New right score : " << scores[i_right]<<"\n";
+            }
+            std::copy(idx.cbegin()+i_max+1, idx.cend(), idx.begin()+i_max);
+            idx.pop_back();
+            std::copy(scores.cbegin()+i_max+1, scores.cend(), scores.begin()+i_max);
+            scores.pop_back();
+            // std::cerr<<"Phrase remains :";
+            // for(auto i : idx)
+            //     std::cerr<<i<< " ";
+            // std::cerr<<std::endl;
+            word_block.push_back(new_word.span);
+            std::cerr<<dot(param.u_score.span, word_block[word_block.size()-1])<<std::endl;
+        }
+        auto t_end = std::chrono::high_resolution_clock::now();
+        std::cout << "Wall time: "<< std::chrono::duration<double, std::milli>(t_load-t_start).count() << std::endl;
+        std::cout << "Wall time: "<< std::chrono::duration<double, std::milli>(t_end-t_load).count() << std::endl;
     } catch (H5::Exception ex) {
         std::cerr << ex.getCDetailMsg() << std::endl;
     } catch (...) {
