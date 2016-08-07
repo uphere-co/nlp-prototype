c626e7392620d9ce68dc758ff68ad008810051e3
Jihun Kim <jihun@uphere.co>
2016-08-07 18:45:17 +0000

    Change variable names to better reflect their definition.

--- rnn++/parser/parser.h
+++ rnn++/parser/parser.h
@@ -41,61 +41,62 @@ public:
         return new_node;
     }
 
+    //top_node  : a node which has no parent
+    //leaf_node : a node which has no children
     std::vector<node_type*> merge_leaf_nodes(std::vector<node_type> &leaves){
         auto n_leaf = leaves.size();
         auto last_leaf = leaves.cend()-1;
         for(auto it=leaves.cbegin(); it!=last_leaf;)
             leaves.push_back( merge_node(*it,*++it ));
 
-        std::vector<node_type*> phrase_pool;
+        std::vector<node_type*> top_node;
         for(auto it=leaves.data()+n_leaf; it!=leaves.data()+leaves.size(); ++it)
-            phrase_pool.push_back(it);
-        return phrase_pool;
+            top_node.push_back(it);
+        return top_node;
     }
-
-    auto foward_path(std::vector<node_type*> &top_leaves){
-        std::vector<decltype(top_leaves.size())> merge_history;
-        while(top_leaves.size()){
-            auto it_max=std::max_element(top_leaves.cbegin(), top_leaves.cend(),
+    auto foward_path(std::vector<node_type*> &top_nodes){
+        std::vector<decltype(top_nodes.size())> merge_history;
+        while(top_nodes.size()){
+            auto it_max=std::max_element(top_nodes.cbegin(), top_nodes.cend(),
                  [](auto const x, auto const y){
                      return x->score < y->score;
                  });
-            auto i_max = it_max - top_leaves.cbegin();
+            auto i_max = it_max - top_nodes.cbegin();
             merge_history.push_back(i_max);
-            if(it_max!=top_leaves.cbegin()){
+            if(it_max!=top_nodes.cbegin()){
                 auto it_left = *(it_max-1);
                 it_left->right=*it_max;
                 set_node_property(*it_left);
             }
-            if(it_max!=top_leaves.cend()-1){
+            if(it_max!=top_nodes.cend()-1){
                 auto it_right= *(it_max+1);
                 it_right->left=*it_max;
                 set_node_property(*it_right);
             }
-            std::copy(it_max+1,top_leaves.cend(),
-                      top_leaves.begin()+i_max);
-            top_leaves.pop_back();
+            std::copy(it_max+1,top_nodes.cend(),
+                      top_nodes.begin()+i_max);
+            top_nodes.pop_back();
         }
         return merge_history;
     }
 
-    void directed_merge(std::vector<node_type*> &top_leaves,
+    void directed_merge(std::vector<node_type*> &top_nodes,
                         std::vector<size_t> const &merge_history){
         for(auto idx_max : merge_history){
-            auto it_new  = top_leaves[idx_max];
+            auto it_new  = top_nodes[idx_max];
             if(idx_max!=0){
-                auto it_left = top_leaves[idx_max-1];
+                auto it_left = top_nodes[idx_max-1];
                 it_left->right=it_new;
                 set_node_property(*it_left);
             }
-            if(idx_max!=top_leaves.size()-1){
-                auto it_right= top_leaves[idx_max+1];
+            if(idx_max!=top_nodes.size()-1){
+                auto it_right= top_nodes[idx_max+1];
                 it_right->left=it_new;
                 set_node_property(*it_right);
             }
-            std::copy(top_leaves.cbegin()+idx_max+1,top_leaves.cend(),
-                      top_leaves.begin()+idx_max);
-            top_leaves.pop_back();
+            std::copy(top_nodes.cbegin()+idx_max+1,top_nodes.cend(),
+                      top_nodes.begin()+idx_max);
+            top_nodes.pop_back();
         }
     }
     Param param;
