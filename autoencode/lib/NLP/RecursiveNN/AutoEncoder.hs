db65dd4e76fbdbf26d4d518e554faab16e8f3b5b
Ian-Woo Kim <ianwookim@gmail.com>
2016-08-18 06:52:43 -0700

    implemented decode. but it produces a segfault error.

--- autoencode/lib/NLP/RecursiveNN/AutoEncoder.hs
+++ autoencode/lib/NLP/RecursiveNN/AutoEncoder.hs
@@ -5,6 +5,7 @@
 
 module NLP.RecursiveNN.AutoEncoder where
 
+import           Control.Applicative             ( (<$>), (<*>), pure )
 import           Control.Monad.IO.Class          ( liftIO )
 import           Data.Foldable
 import           Data.Hashable
@@ -38,26 +39,54 @@ data AutoEncoder = AutoEncoder { autoenc_dim :: Int
                                , autoenc_b   :: Vector Float
                                } 
 
-encodeAST :: (?expHash :: Exp Float :->: Hash) => AST.Module
-encodeAST =
+data ADNode = ADNode { adnode_autodec :: AutoDecoder
+                     , adnode_y  :: Vector Float
+                     }
+
+data AutoDecoder = AutoDecoder { autodec_dim :: Int
+                               , autodec_Wd  :: Matrix Float
+                               , autodec_b   :: Vector Float
+                               }
+
+encodeExp :: (?expHash :: Exp Float :->: Hash) => (MExp Float, [Variable]) -- AST.Module
+encodeExp =
   let idxi = ("i",1,100)
       idxj = ("j",1,100)
       idxk = ("k",1,100)
       idxI = ("I",1,200)
       c1 = ivar (mkSym "c1") [idxi]
       c2 = ivar (mkSym "c2") [idxj]
-      w = ivar (mkSym "w") [idxk, idxI]
-      b = ivar (mkSym "b") [idxk]
+      we = ivar (mkSym "we") [idxk, idxI]
+      be = ivar (mkSym "be") [idxk]
 
       c = concat_ idxI [c1,c2]
-      prd = sum_ [idxI] (mul [w, c])
-      result = tanh_ [ add [prd, b] ]
-      ext = external double "tanh" [(double, AST.Name "x")] 
-  in mkASTWithExt ext [("fun1",(result,[ V (mkSym "c1") [idxi]
-                                       , V (mkSym "c2") [idxj]
-                                       , V (mkSym "w") [idxk,idxI]
-                                       , V (mkSym "b") [idxk] ] ))]
+      prd = sum_ [idxI] (mul [we, c])
+      result = tanh_ [ add [prd, be] ]
+  in (result, [ V (mkSym "c1") [idxi]
+              , V (mkSym "c2") [idxj]
+              , V (mkSym "we")  [idxk,idxI]
+              , V (mkSym "be")  [idxk] ] )
 
+decodeExp :: (?expHash :: Exp Float :->: Hash) => (MExp Float, [Variable]) -- AST.Module
+decodeExp =
+  let idxi = ("i",1,100)
+      idxj = ("j",1,100)
+      idxk = ("k",1,100)
+      idxI = ("I",1,200)
+      y = ivar (mkSym "y") [idxk]
+      wd = ivar (mkSym "wd") [idxI, idxk]
+      bd = ivar (mkSym "bd") [idxI]
+      prd = sum_ [idxI] (mul [wd, y])
+      result = tanh_ [ add [prd, bd] ]
+  in (result, [ V (mkSym "y") [idxk]
+              , V (mkSym "wd") [idxI,idxk]
+              , V (mkSym "bd") [idxI] ] )
+
+
+fullAST :: (?expHash :: Exp Float :->: Hash) => AST.Module
+fullAST = mkASTWithExt ext [("encode",encodeExp), ("decode",decodeExp)]
+  where ext = external double "tanh" [(double, AST.Name "x")] 
+ 
                    
 encodeP :: AENode -> LLVMRunT IO (Vector Float)
 encodeP AENode {..} = do
@@ -67,7 +96,7 @@ encodeP AENode {..} = do
       vb  = autoenc_b aenode_autoenc
       vr = VS.replicate 100 0    :: VS.Vector Float
   mv@(VS.MVector _ fpr) <- liftIO $ VS.thaw vr
-  callFn "fun1Wrapper" [vc1,vc2,vwe,vb] fpr
+  callFn "encodeWrapper" [vc1,vc2,vwe,vb] fpr
   vr' <- liftIO $ VS.freeze mv
   return vr'
   
@@ -95,42 +124,43 @@ encode autoenc btr = go btr
         go (BinLeaf x) = return (BNTLeaf x)
 
 
-{- 
+decodeP :: ADNode -> LLVMRunT IO (Vector Float, Vector Float)
+decodeP ADNode {..} = do
+  let dim = 100 --  autodec_dim adnode_autodec
+      vy  = adnode_y
+      vwd = mat_content (autodec_Wd adnode_autodec)
+      vbd = autodec_b adnode_autodec
+      vr = VS.replicate 200 0 :: VS.Vector Float
+  mv@(VS.MVector _ fpr) <- liftIO $ VS.thaw vr
+  callFn "decodeWrapper" [vy,vwd,vbd] fpr
+  vr' <- liftIO $ VS.freeze mv
+  let c1 = VS.slice 0 dim vr'
+      c2 = VS.slice dim dim vr'
+  return (c1,c2)
 
-
-data ADNode = ADNode { adnode_autodec :: AutoDecoder
-                     , adnode_y  :: Vector Float
-                     }
-
-data AutoDecoder = AutoDecoder { autodec_dim :: Int
-                               , autodec_Wd  :: Matrix Float
-                               , autodec_b   :: Vector Float
-                               }
-
-
-decodeP :: ADNode -> (Vector Float, Vector Float)
-decodeP ADNode {..} = (c1,c2)
-  where
-    dim = autodec_dim adnode_autodec
-    wd = autodec_Wd adnode_autodec 
-    b = autodec_b adnode_autodec
-    r = mulMV wd adnode_y
-    rc = V.map tanh $ V.zipWith (+) r b 
-    c1 = V.slice 0 dim rc
-    c2 = V.slice dim dim rc
+    -- r = mulMV wd adnode_y
+    -- rc = V.map tanh $ V.zipWith (+) r b 
+    -- c1 = V.slice 0 dim rc
+    -- c2 = V.slice dim dim rc
    
 
-decode :: AutoDecoder -> BNTree (Vector Float) ()-> BNTree (Vector Float) (Vector Float)
+
+ 
+decode :: AutoDecoder
+       -> BNTree (Vector Float) ()
+       -> LLVMRunT IO (BNTree (Vector Float) (Vector Float))
 decode autodec bntr@(BNTNode v _ _) = go v bntr
   where 
-    go v1 (BNTNode _ x y) = let ad     = ADNode autodec v1
-                                (c1,_) = decodeP ad
-                            in BNTNode v1 (go c1 x) (go c1 y)
-    go v1 (BNTLeaf ()) = BNTLeaf v1
+    go v1 (BNTNode _ x y) = do (c1,c2) <- decodeP (ADNode autodec v1)
+                               BNTNode v1 <$> go c1 x <*> go c2 y --  (go c1 x) (go c1 y)
+    go v1 (BNTLeaf ()) = pure (BNTLeaf v1)
 decode _ (BNTLeaf _) = error "shouldn't happen"
 
 
-recDecode :: AutoDecoder -> BNTree (Vector Float) () -> BNTree (BNTree (Vector Float) (Vector Float)) ()
-recDecode _       (BNTLeaf ())      = BNTLeaf ()
-recDecode autodec n@(BNTNode _ x y) = BNTNode (decode autodec n) (recDecode autodec x) (recDecode autodec y) 
--}
+recDecode :: AutoDecoder
+          -> BNTree (Vector Float) ()
+          -> LLVMRunT IO (BNTree (BNTree (Vector Float) (Vector Float)) ())
+recDecode _       (BNTLeaf ())      = pure (BNTLeaf ())
+recDecode autodec n@(BNTNode _ x y) = 
+  BNTNode <$> decode autodec n <*> recDecode autodec x <*> recDecode autodec y
+
