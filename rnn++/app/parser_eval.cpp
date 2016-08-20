7995dd6f8a6d2cb320d88f98fe931f62caa7e3dd
Jihun Kim <jihun@uphere.co>
2016-08-20 14:22:52 +0000

    Get difference of two spans.

--- rnn++/app/parser_eval.cpp
+++ rnn++/app/parser_eval.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_set>    
 #include <string>
 #include <algorithm>
 #include <cassert>
@@ -21,6 +22,7 @@ auto fill_child_node=[](auto parent, auto child){
     else if(parent->right ==nullptr) parent->right=child;
     else {assert(0);}
 };
+
 std::vector<Node> deserialize_binary_tree(std::string tree_str){
     auto n_composites = std::count_if(tree_str.cbegin(), tree_str.cend(), node_begin);
     std::vector<Node> nodes(n_composites*2+1);
@@ -46,7 +48,8 @@ std::vector<Node> deserialize_binary_tree(std::string tree_str){
     assert(current_node == nullptr);
     return nodes;
 }
-using tree_span_t = std::pair<std::size_t,std::size_t>;
+auto is_composite = [](auto const &node){return node.left && node.right;};
+
 auto get_left_span = [](auto const &node){
     auto span=node.left; 
     while(span->left) span=span->left;
@@ -59,6 +62,26 @@ auto get_right_span = [](auto const &node){
 };
 auto get_span = [](auto const &node){
     return std::make_pair(get_left_span(node), get_right_span(node));
+};
+
+auto get_span_hashes = [](auto const &nodes){
+    const auto n_nodes = nodes.size();
+    std::vector<std::size_t> span_hashes;
+    for(auto const &node: nodes){
+        if(!is_composite(node)) continue;
+        auto left = get_left_span(node)-nodes.data();
+        auto right = get_right_span(node)-nodes.data();
+        auto hash = left*n_nodes + right;
+        span_hashes.push_back(hash);
+    }
+    return span_hashes;
+};
+
+auto span_diffs= [](auto const &spans1, auto const &spans2){
+    std::vector<std::size_t> diff;
+    std::set_difference(spans1.cbegin(),spans1.cend(), 
+                        spans2.cbegin(),spans2.cend(),std::back_inserter(diff));
+    return diff.size();
 };
 
 auto assert_node=[](auto const &parent, auto const &left, auto const &right){
@@ -89,6 +112,8 @@ void test_simple(){
     {
         auto nodes = deserialize_binary_tree("(((c d) b) ((c (c d)) d))");
         assert(nodes.size()==13);
+        for(int i=0; i<7; ++i) assert(!is_composite(nodes[i]));
+        for(int i=7; i<13; ++i) assert(is_composite(nodes[i]));
         assert_node(nodes[7], nodes[8], nodes[10]);
         assert_node(nodes[8], nodes[9], nodes[2]);
         assert_node(nodes[9], nodes[0], nodes[1]);
@@ -101,6 +126,27 @@ void test_simple(){
         assert_nodes_span(nodes, 10, 3, 6);
         assert_nodes_span(nodes, 11, 3, 5);
         assert_nodes_span(nodes, 12, 4, 5);
+        auto spans=get_span_hashes(nodes);
+        assert(spans.size()==6);        
+        assert(spans[0]==6);
+        assert(spans[1]==2);
+        assert(spans[2]==1);
+        assert(spans[3]==45);
+        assert(spans[4]==44);
+        assert(spans[5]==57);
+        // assert(spans.find(6)!=spans.cend());
+        // assert(spans.find(2)!=spans.cend());
+        // assert(spans.find(1)!=spans.cend());
+        // assert(spans.find(45)!=spans.cend());
+        // assert(spans.find(44)!=spans.cend());
+        // assert(spans.find(57)!=spans.cend());
+    }
+    {
+        auto tree1 = deserialize_binary_tree("(((c d) b) ((c (c d)) d))");
+        auto tree2 = deserialize_binary_tree("((c (d b)) ((c (c d)) d))");
+        auto spans1=get_span_hashes(tree1);
+        auto spans2=get_span_hashes(tree2);
+        assert(span_diffs(spans1, spans2)==1);
     }
     
 }
