37477d9a2d8f2c1fd66530091caf7cb1d16eb984
Ian-Woo Kim <ianwookim@gmail.com>
2016-08-31 15:48:35 +0000

    upgrade llvm-general for ghc 8 and also compatible with llvm-3.8. Now llvm 3.8 is used.

--- autoencode/shell.nix
+++ autoencode/shell.nix
@@ -1,10 +1,14 @@
 { pkgs ? (import <nixpkgs>{}) }:
 
 with pkgs;
-#with import <nixpkgs/pkgs/development/haskell-modules/lib.nix> { inherit pkgs; };
 
+let
+    llvmGeneralSrc = fetchgit {
+      url = "git://github.com/wavewave/llvm-general.git";
+      rev = "646bfa6168430b56035f9858c9676ac22fba4976";
+      sha256 = "1g97vi8jmp7n783s0kbk5vhrh4kjqf619nhw3qxjjlpz08vhjr9m";
+    };
 
-let 
     hsconfig = self: super: {
       "lbfgs" = self.callPackage
         ({ mkDerivation, array, base, stdenv, vector }:
@@ -41,145 +45,18 @@ let
            license = stdenv.lib.licenses.bsd3;
            hydraPlatforms = stdenv.lib.platforms.none;
          }) { inherit (pkgs) liblapack;};
- 
 
-      cuda = self.callPackage
-        ({ mkDerivation, base, bytestring, c2hs, pretty, stdenv
-         , template-haskell
-         , cudatoolkit, autoconf
-         }:
-         mkDerivation {
-           pname = "cuda";
-           version = "0.7.1.0";
-           src = fetchgit {
-             url = "git://github.com/wavewave/cuda.git";
-             rev = "05145a79dc652a1a9f72bc0d99a6db59a9dd55df";
-             sha256 = "16hsf2qwzai04bf0gyn6wpw37fk0kldbm6gcx1zzlmhkvb5g94wm";
-           };
-           isLibrary = true;
-           isExecutable = true;
-           libraryHaskellDepends = [ base bytestring template-haskell ];
-           libraryToolDepends = [ c2hs ];
-           executableHaskellDepends = [ base pretty ];
-           librarySystemDepends = [ cudatoolkit ];
-           buildDepends = [ autoconf ];
-           extraLibraries = (super.extraLibraries or []) ++ [pkgs.linuxPackages.nvidia_x11];
-           configureFlags = (super.configureFlags or []) ++
-             pkgs.lib.optional pkgs.stdenv.is64bit "--extra-lib-dirs=${cudatoolkit}/lib64" ++ [
-               "--extra-lib-dirs=${cudatoolkit}/lib"
-               "--extra-include-dirs=${cudatoolkit}/include"
-             ];
-           preConfigure = ''
-             unset CC
-           '';
-           homepage = "https://github.com/tmcdonell/cuda";
-           description = "FFI binding to the CUDA interface for programming NVIDIA GPUs";
-           license = stdenv.lib.licenses.bsd3;
-         }) {};
+        llvm-general-pure =
+	  let p1 = haskell.lib.overrideCabal super.llvm-general-pure (drv: {
+                     src = "${llvmGeneralSrc}/llvm-general-pure"; 
+                   });
+          in haskell.lib.addBuildDepend p1 self.transformers-compat; 
 
-      cublas = self.callPackage
-        ({ mkDerivation, base, cuda, filepath, language-c
-         , stdenv, storable-complex, template-haskell, autoconf
-         , cudatoolkit
-         }:
-         mkDerivation {
-           pname = "cublas";
-           version = "0.3.0.0";
-           src = fetchgit {
-             url = "git://github.com/wavewave/cublas.git";
-             rev = "0ad62bcfcbee369a03ddbf292e49514fb56a2886";
-             sha256 = "05rspmdl3q2ppgkfzhwviyphi82262r21h9142m1fg3dxiz22k1b";
-           };
-           libraryHaskellDepends = [
-             base cuda filepath language-c storable-complex template-haskell
-           ];
-           buildDepends = [ autoconf ];
-           librarySystemDepends = [ cudatoolkit ];
-           preConfigure = ''
-             unset CC
-             sed -i -e 's|/usr/bin/gcc|gcc|g' Foreign/CUDA/Cublas/THBase.hs 
-           '';
-           jailbreak = true;
-           homepage = "https://github.com/bmsherman/cublas";
-           description = "FFI bindings to the CUDA CUBLAS and CUSPARSE libraries";
-           license = stdenv.lib.licenses.bsd3;
-         }) {};
-    
-      accelerate = self.callPackage
-        ({ mkDerivation, array, base, base-orphans, containers, directory
-        , exceptions, fclabels, filepath, ghc-prim, hashable, hashtables
-        , mtl, pretty, stdenv, template-haskell, transformers, unique, unix
-        , unordered-containers
-        }:
-        mkDerivation {
-          pname = "accelerate";
-          version = "2.0.0.0";
-          #src = /home/wavewave/repo/srcc/accelerate;
-          src = fetchgit {
-            url = "git://github.com/AccelerateHS/accelerate.git";
-            rev = "9d0118f8ef3cd47d64a737b9d7d1d902a5ee72b7";
-            sha256 = "0gvqv6vx4izwhlgzs13vlmsp07lnfzpy2nzw5ahla8fkxmayllq0";
-          };
-          libraryHaskellDepends = [
-            array base base-orphans containers directory exceptions fclabels
-            filepath ghc-prim hashable hashtables mtl pretty template-haskell
-            transformers unique unix unordered-containers
-          ];
-          doHaddock = false;
-          doCheck = false;
-          
-          homepage = "https://github.com/AccelerateHS/accelerate/";
-          description = "An embedded language for accelerated array processing";
-          license = stdenv.lib.licenses.bsd3;
-        }) {};
-      accelerate-io = self.callPackage 
-        ({ mkDerivation, accelerate, array, base, bmp, bytestring, repa
-        , stdenv, vector
-        }:
-        mkDerivation {
-          pname = "accelerate-io";
-          version = "2.0.0.0";
-          src = fetchgit {
-            url = "git://github.com/AccelerateHS/accelerate-io.git";
-            rev = "a5e73442fd987243de12bfb0c1ff7a87d701ea94";
-            sha256 = "0rsr76y54r3dsrk39lbps660z4lvn3lrxp38ls13nr5xlf11aw7p";
-          };
-          libraryHaskellDepends = [
-            accelerate array base bmp bytestring repa vector
-          ];
-          homepage = "https://github.com/AccelerateHS/accelerate-io";
-          description = "Read and write Accelerate arrays in various formats";
-          license = stdenv.lib.licenses.bsd3;
-        }) {};
-      accelerate-cuda = self.callPackage
-        ({ mkDerivation, accelerate, array, base, binary, bytestring
-        , containers, cryptohash, cuda, directory, fclabels, filepath
-        , hashable, hashtables, language-c-quote, mainland-pretty, mtl
-        , old-time, pretty, process, SafeSemaphore, srcloc, stdenv
-        , template-haskell, text, transformers, unix, unordered-containers
-        }:
-        mkDerivation {
-          pname = "accelerate-cuda";
-          version = "2.0.0.0";
-          #src = /home/wavewave/repo/srcc/accelerate-cuda;
-          src = fetchgit {
-            url = "https://github.com/AccelerateHS/accelerate-cuda.git";
-            rev = "b33150316aa8b8dd611cfc59f08429bd64fdb747";
-            sha256 = "1b8784bn9gqidrhc9z8bnvxmvp7cp5sq45jp6907gpa1c4jnq3l8";
-          };
-          configureFlags = "-fdebug";
-          doCheck = false;
-          doHaddock = false;
-          libraryHaskellDepends = [
-            accelerate array base binary bytestring containers cryptohash cuda
-            directory fclabels filepath hashable hashtables language-c-quote
-            mainland-pretty mtl old-time pretty process SafeSemaphore srcloc
-            template-haskell text transformers unix unordered-containers
-          ];
-          homepage = "https://github.com/AccelerateHS/accelerate-cuda/";
-          description = "Accelerate backend for NVIDIA GPUs";
-          license = stdenv.lib.licenses.bsd3;
-        }) {};
+        llvm-general = haskell.lib.overrideCabal super.llvm-general (drv: {
+          src = "${llvmGeneralSrc}/llvm-general";
+	  libraryToolDepends = [ llvm_38 ];
+        });
+
     };
     
     newhaskellPackages = haskellPackages.override { overrides = hsconfig; };
@@ -188,13 +65,8 @@ let
               cabal-install
               xml-conduit split unordered-containers vector-algorithms storable-tuple
               tagged either
-              #accelerate
-              #accelerate-io
-              #accelerate-cuda
               mersenne-random
               math-functions
-              #cuda
-              #cublas
               hblas
               lbfgs
               MemoTrie lens
@@ -209,14 +81,13 @@ let
               zenc              
             ]);
 
-    llvm_35_debug = llvm_35.override { debugVersion = true; }; 
+    #llvm_35_debug = llvm_35.override { debugVersion = true; }; 
 
 in stdenv.mkDerivation {
      name = "ghc-shell";
-     buildInputs = [ hsenv cudatoolkit graphviz llvm_35
-                     #llvmPackages_35.lldb
+     buildInputs = [ hsenv graphviz #llvm_38
 		   ];
      shellHook = ''
-       export CUDA_PATH=${pkgs.cudatoolkit}
      '';
    }
+
