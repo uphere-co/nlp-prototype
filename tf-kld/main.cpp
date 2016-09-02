8f8d82a1ccff82e96ad2f90408ff838b8e268d20
Doh Young Mo <modohyoung@gmail.com>
2016-09-02 07:24:36 +0000

    Reading a data file and making a sparse matrix

--- tf-kld/main.cpp
+++ tf-kld/main.cpp
@@ -1,8 +1,11 @@
+#include <armadillo>
 #include <fstream>
+#include <set>
 #include <sstream>
 #include <string>
 #include <algorithm>
 #include <map>
+#include <iterator>
 
 #include "utils/hdf5.h"
 #include "utils/string.h"
@@ -12,10 +15,17 @@ namespace tfkld{
 namespace type{
   
 using int_t = int;
+//using int64_t = int64_t;
 using float_t = float;
 using char_t = char;
 }//namespace tfkld::type
 }//namespace tfkld
+
+using namespace tfkld::type;
+
+using hashmap_t = std::map<std::string, int_t>;
+using vocab_t = std::set<std::string>;
+using doc_t = std::vector<hashmap_t>;
 
 namespace tfkld{
 
@@ -28,37 +38,69 @@ struct TokenizedFile{
             exit(1);
         }
     }
+
+    void setBegin() {
+        val.clear();
+        val.seekg(0);
+    }
     std::ifstream val; 
 };
 
-//CAUTION!! Do not include a following line in a header.
-using namespace tfkld::type;
+struct docs{
+    doc_t docs;
+};
 
-using hashmap_t = std::map<std::string, int_t>;
-using vocab_t = std::vector<std::string>;
-
-vocab_t VocabLearn(TokenizedFile &file) {
+vocab_t LearnVocab(TokenizedFile &file) {
   std::string line;
   vocab_t vocab;
   while(std::getline(file.val, line)){
     std::istringstream iss{line};
     auto words = util::string::split(line);
-    for(auto x : words){
-      auto isin = std::find(vocab.begin(), vocab.end(), x);
-      if(isin != vocab.end()) {
-	vocab.push_back(x);
-      } else{
-	continue;
-      }
-    }
+    for(auto x : words) vocab.insert(x);
   }
-  
+  // return sorted vocabulary with ascending order
   return vocab;
 }
-  
-void PrintVocab(hashmap_t const &word_count){
-    for(auto x : word_count){
-        std::cout << x.first << " " <<x.second<< std::endl;
+
+doc_t LearnDoc(TokenizedFile &file) {
+    std::string line;
+    doc_t docs;
+    hashmap_t doc;
+    while (std::getline(file.val, line)) {
+        std::istringstream iss{line};
+        auto words = util::string::split(line);
+        for(auto x : words) {
+            auto isin = doc.find(x);
+            if(isin != doc.end()) {
+                doc[x] += 1;
+            } else {
+                doc[x] = 1;
+            }
+        }
+        docs.push_back(doc);
+        doc.clear();
+    }
+
+    return docs;
+}
+
+
+void fillMat(vocab_t const &vocab, doc_t const &docs, arma::sp_fmat &mat) {
+    int64_t row, col;
+    for(auto it = docs.begin(); it != docs.end(); ++it) {
+        for(auto itt = (*it).begin(); itt != (*it).end(); ++itt) {
+            row = std::distance(vocab.begin(),vocab.find((*itt).first));
+            col = std::distance(docs.begin(),it);
+            mat(row,col) = (*itt).second;
+        }
+    }
+}
+
+
+        
+void PrintVocab(vocab_t &vocab){
+    for(auto x : vocab){
+        std::cout << x << std::endl;
     }
 }
 
@@ -84,11 +126,74 @@ auto ToStrings(std::vector<char> const &concat_words){
 }
 }//namespace tfkld
 
+void testKLD() {
+    
+}
+
+
+
 int main(){
     using namespace tfkld;
-    std::string train_file = "data.txt";
-    TokenizedFile infile{train_file};
+    using namespace arma;
     
+    std::string train_file = "1M.training";
+    TokenizedFile infile{train_file};
+    auto vocab = LearnVocab(infile);
+    infile.setBegin();
+    auto docs = LearnDoc(infile);
+
+    int64_t n_rows, n_cols;
+    n_rows = vocab.size();
+    n_cols = docs.size();
+    //(n_rows, n_cols)
+    sp_fmat inMat(n_rows,n_cols);
+
+    //int64_t pos = std::distance(vocab.begin(), vocab.find("that"));
+    //std::cout << pos << std::endl;
+    fillMat(vocab, docs, inMat);
+    
+    //inMat.print("inMat = ");
+    std::cout << "Finished!" << std::endl;
+    
+    //arma::mat inMat = arma::randu<arma::mat>(5,5);
+
+
+
+    /*
+    sp_mat X = sprandu<sp_mat>(50000, 200000, 0.001);
+
+    mat U;
+    vec s;
+    mat V;
+
+    svds(U, s, V, X, 200);
+
+
+
+
+    //X.print("X = ");
+    //U.print("U = ");
+    s.print("S = ");
+    //V.print("V = ");
+    
+
+
+    */
+    /*
+    arma::mat inMat = arma::randu<arma::mat>(5,5);
+
+    arma::mat U;
+    arma::vec s;
+    arma::mat V;
+
+    arma::svd(U,s,V,inMat);
+
+
+    inMat.print("inMat = ");                                                                                                                                                  U.print("U = ");                                                                                                                                                          s.print("S = ");                                                                                                                                                          V.print("V = ");
+    */
+
+
+    //PrintVocab(vocab);
     //std::vector<char> concat_words = Concat(word_count_keys);
     
     //H5file file{H5name{"data.h5"}, hdf5::FileMode::replace};
@@ -98,5 +203,52 @@ int main(){
     //concat_read = file.readRawData(H5name{"bar.word_key"},concat_words); 
     //auto words = ToStrings(concat_read);
     //assert(words=word_count_keys);
+    /*
+
+  //fvec column vector                                                                                                                                                     
+
+  arma::fmat inMat;
+  arma::fvec v1;
+  arma::fvec v2;
+  arma::fvec v3;
+
+  v1 = {1, 2, 3};
+  v2 = {2, 3, 4};
+  v3 = {3, 4, 9};
+
+  std::vector<arma::fvec> v;
+  std::vector<arma::fvec> rv;
+  v.push_back(v1);
+  v.push_back(v2);
+  v.push_back(v3);
+
+  rv = makeBasis(v);
+  makeNormal(rv);
+  for(int i = 0; i < rv.size(); i++) {
+    rv[i].print();
+  }
+
+  inMat = {{1,2,3},{2,3,4},{3,4,9}};
+  std::cout << rank(inMat) << std::endl;
+
+  //arma::fmat U;                                                                                                                                                          
+  //arma::fvec S;                                                                                                                                                          
+  //arma::fmat V;                                                                                                                                                          
+
+
+  //arma::svd(U, S, V, inMat);                                                                                                                                             
+
+  //inMat.print("inMat = ");                                                                                                                                               
+  //U.print("U = ");                                                                                                                                                       
+  //S.print("S = ");                                                                                                                                                       
+  //V.print("V = ");                    
+
+
+
+
+
+     */
+
+    
     return 0;
 }
