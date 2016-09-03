0f30169c071defe5ca3a581d19e926a039604252
Ian-Woo Kim <ianwookim@gmail.com>
2016-09-03 07:24:41 +0000

    rnn++ on hydra!

--- nix/release.nix
+++ nix/release.nix
@@ -4,6 +4,6 @@ let pkgs = import nixpkgs {};
 
 in
 {
-  symbolic = import ../symbolic/default.nix {};
-  #hello = pkgs.hello;
+  symbolic = import ../symbolic { inherit pkgs; };
+  "rnn++"  = import ../rnn++ { inherit pkgs; };
 }
---
+++ rnn++/default.nix
@@ -0,0 +1,46 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let toolz     = callPackage ../nix/default-python.nix {
+                  inherit pkgs;
+                  buildPythonPackage = pkgs.python27Packages.buildPythonPackage;
+                };
+    toolz_cpp = callPackage ../nix/default-cpp.nix { };
+in
+stdenv.mkDerivation {
+  version = "0.0";
+  name = "rnn++-${version}";
+  src = ./.;
+  buildInputs = (with python27Packages;
+                 [ ipython jupyter ipyparallel
+                   line_profiler
+                   matplotlib seaborn
+                   numpy scipy pandas scikitlearn
+                   pyzmq
+                   cython
+                   numba
+                   toolz.gensim toolz.untangle
+                   #Theano Keras
+		   h5py
+                   pytest toolz.pytest-mock
+                   toolz.guppy
+                   toolz.nltk toolz.bllipparser
+                   psycopg2
+                 ]) 
+                   ++ 
+                 [
+                   wget zip unzip
+                   cmake pkgconfig clang_38 clang-analyzer
+                   boost
+                   hdf5 hdf5-cpp liblbfgs zeromq
+                   tbb openblas  
+                   linuxPackages_4_6.perf
+                   toolz_cpp.msgsl
+                   toolz_cpp.spdlog
+                 ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp hdf5io model1 parser_similarity train_model0 train_model1 $out/bin 
+  '';
+}
