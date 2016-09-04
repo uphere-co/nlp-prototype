0cffddacfd99b60eb205ac7fc1cff2c7e0d275ef
Ian-Woo Kim <ianwookim@gmail.com>
2016-09-04 19:26:53 +0000

    record word list in table (without double counting)

--- hmatrixtest/test.hs
+++ hmatrixtest/test.hs
@@ -3,6 +3,7 @@
 
 import Numeric.LinearAlgebra hiding ((!))
 import           Control.Monad
+import           Control.Monad.Primitive
 import           Control.Monad.IO.Class     (MonadIO(liftIO))
 import           Control.Monad.Loops          (whileJust_)
 import           Control.Monad.Trans          (lift) 
@@ -10,6 +11,7 @@ import           Control.Monad.Trans.Resource
 import           Control.Monad.Trans.State.Strict
 import           Data.Conduit
 import           Data.Conduit.Binary        (sourceFile)
+import           Data.Conduit.List          (isolate)
 import           Data.Conduit.Text   as CT
 import           Data.Foldable              (forM_)
 import           Data.HashMap.Strict        (HashMap)
@@ -17,7 +19,7 @@ import qualified Data.HashMap.Strict as HM
 import           Data.IORef                 
 import           Data.List                  (foldl')
 import           Data.IntMap                (IntMap)
-import qualified Data.IntMap         as M
+import qualified Data.IntMap         as IM
 import qualified Data.Set            as Set
 import           Data.Text                  (Text)
 -- import qualified Data.Text.Lazy      as TL
@@ -58,14 +60,21 @@ main' = do
   print (singularValues a')
 
 
-count :: (MonadIO m) => Int -> Sink Text m ()
-count !n = do 
-  mt <- await
-  forM_ mt $ \t -> do
+count :: (MonadIO m) => Int -> Sink a m ()
+count !n =
+  (await >>=) $ mapM_ $ \_ -> do
     when (n `mod` 10000 == 0) $ do
       liftIO $ print n
       -- liftIO $ print t
     count (n+1)
+
+enumerate :: (Monad m) => Int -> Conduit a m (Int,a)
+enumerate !n = do
+  mx <- await
+  forM_ mx $ \x -> do  
+    yield (n,x)
+    enumerate (n+1)
+  
 
 
 wordCount !n = 
@@ -81,12 +90,13 @@ wordHashMap !n !m = do
                           Nothing -> (n+1,HM.insert w n m)
                           Just _ -> (n,m)
 
-buildMap mref mv md = whileJust_ await $ \t -> do
-                        let ws = T.words t
-                        liftIO $ do
-                          is <-mapM (voca mref mv) ws
-                          let is' = Set.toList (Set.fromList is)
-                          mapM_ (doc md) is'
+buildMap (mref,mv) (dref,md) =
+  whileJust_ await $ \(docid,t) -> do
+    let ws = T.words t
+    liftIO $ do
+      is <-mapM (voca mref mv) ws
+      let is' = Set.toList (Set.fromList is)
+      doc dref md docid is'
 
 voca mref mv w = do
   BoundedWordMap n m <- readIORef mref
@@ -96,31 +106,39 @@ voca mref mv w = do
       writeIORef mref (BoundedWordMap (n+1) (HM.insert w n m))
       return n
     Just i -> MV.modify mv (+1) i >> return i
-      
 
-doc md j = MV.modify md (+1) j  
-                  
+doc :: IORef (IntMap [Int]) -> MVector (PrimState IO) Int -> Int -> [Int] -> IO ()
+doc dref md docid js = do
+  modifyIORef' dref (IM.insert docid js)
+  mapM_ (MV.modify md (+1)) js  
+
+
+  
 data BoundedWordMap = BoundedWordMap { newid :: !Int, wordmap :: !(HM.HashMap Text Int) }
 
 emptyBWM = BoundedWordMap 0 HM.empty
                       
 main = do
   mref <- newIORef emptyBWM
+  dref <- newIORef IM.empty
   mv <- V.thaw (V.replicate 1000000 (0 :: Int))
-  md <- V.thaw (V.replicate 1000000 (0 :: Int))  
+  md <- V.thaw (V.replicate 1000000 (0 :: Int))
   
   runResourceT $ 
-    sourceFile "1M.training" =$= CT.decode CT.utf8 =$= CT.lines $$ getZipSink $
-      (,) <$> ZipSink (count 0) <*> ZipSink (buildMap mref mv md)
+    sourceFile "1M.training" =$= CT.decode CT.utf8 =$= CT.lines =$= isolate 100000 =$= enumerate 0 $$ getZipSink $
+      (,) <$> ZipSink (count 0) <*> ZipSink (buildMap (mref,mv) (dref,md))
   BoundedWordMap n m <- readIORef mref
   v <- V.freeze mv :: IO (Vector Int)
   d <- V.freeze md :: IO (Vector Int)
-
+  docs <- readIORef dref
+  
   case HM.lookup "the" m of
     Nothing -> print "no such word"
     Just i -> do
       -- print (v!i)
       putStrLn ("(v,d)" ++ show (v!i, d!i))
+
+  print (IM.lookup 1000 docs) 
 
   {- 
   txt <- TLIO.readFile "1M.training" -- "/home/wavewave/repo/srcp/nlp-data/word2vec-dataset/1b.training"
