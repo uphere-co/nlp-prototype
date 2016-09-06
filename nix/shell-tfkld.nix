0b9681f2c9e54bc9e1b830a89069a2a7ba75b208
Doh Young Mo <mo@uphere.co>
2016-09-06 15:55:55 +0900

    Nix expression for TF-KLD is separated out from nix expression for cpp

---
+++ nix/shell-tfkld.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let toolz = callPackage ./default-cpp.nix { };
+in
+stdenv.mkDerivation {
+  name = "cpp-env";
+  buildInputs = [ boost
+                  toolz.openblas_static
+                  #liblapack
+                  toolz.armadillo toolz.libpca
+		  toolz.msgsl
+		  toolz.spdlog
+          toolz.libsvm
+          toolz.liblinear
+          #libsvm
+                  gdb
+		  pkgconfig
+		  gfortran #stdenv.cc.libc
+		  gnuplot
+          python27Full
+
+                  cmake clang_38 clang-analyzer
+                  hdf5 hdf5-cpp zeromq
+                  tbb 
+                  linuxPackages_4_6.perf
+                  #openblas
+                ];
+  shellHook = ''
+    export MS_GSL=${toolz.msgsl}
+    export SPDLOG=${toolz.spdlog}
+    export LIBSVM=${toolz.libsvm}
+    export LIBLINEAR=${toolz.liblinear}
+    export PATH=$LIBLINEAR/liblinear:$LIBSVM/libsvm:$PATH
+  '';
+}
