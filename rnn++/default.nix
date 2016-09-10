16b7730021697d23519e204c73b500d56bdbd2fe
Ian-Woo Kim <ianwookim@gmail.com>
2016-09-10 00:30:43 +0000

    make rnn++ build successful again: add fmt in buildInputs in default.nix and adjust CMakeLists.txt a little bit.

--- rnn++/default.nix
+++ rnn++/default.nix
@@ -2,44 +2,19 @@
 
 with pkgs;
 
-let toolz     = callPackage ../nix/default-python.nix {
-                  inherit pkgs;
-                  buildPythonPackage = pkgs.python27Packages.buildPythonPackage;
-                };
-    toolz_cpp = callPackage ../nix/default-cpp.nix { };
+let toolz_cpp = callPackage ../nix/default-cpp.nix { };
 in
 stdenv.mkDerivation rec {
   version = "0.0";
   name = "rnn++-${version}";
   src = ./.;
-  buildInputs = (with python27Packages;
-                 [ ipython jupyter ipyparallel
-                   line_profiler
-                   matplotlib seaborn
-                   numpy scipy pandas scikitlearn
-                   pyzmq
-                   cython
-                   numba
-                   toolz.gensim toolz.untangle
-                   #Theano Keras
-		   h5py
-                   pytest toolz.pytest-mock
-                   toolz.guppy
-                   toolz.nltk toolz.bllipparser
-                   psycopg2
-                 ]) 
-                   ++ 
-                 [
-                   wget zip unzip
-                   cmake pkgconfig clang_38 
-                   boost
-                   hdf5 hdf5-cpp liblbfgs 
-                   tbb openblas  
-                   toolz_cpp.msgsl
-                   toolz_cpp.spdlog
-                 ];
-  #installPhase = ''
-  #  mkdir -p $out/bin
-  #  cp hdf5io model1 parser_similarity train_model0 train_model1 $out/bin 
-  #'';
+  buildInputs = [ cmake clang_38 
+                  boost
+                  hdf5 hdf5-cpp liblbfgs 
+                  tbb openblas  
+                  toolz_cpp.msgsl
+                  toolz_cpp.spdlog
+		  toolz_cpp.fmt
+                ];
+  enableParallelBuilding = true;
 }
