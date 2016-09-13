24ae783d76c147391d44b03db3dcb8f1c590828b
Doh Young Mo <modohyoung@gmail.com>
2016-09-13 07:14:05 +0000

    Refactoring is done. Main body is separated out.

--- tf-kld/app/main.cpp
+++ tf-kld/app/main.cpp
@@ -9,355 +9,179 @@
 #include <unordered_set>
 #include <utility>
 
+#include "src/TFKLD.h"
+
 #include "utils/hdf5.h"
 #include "utils/string.h"
 #include "utils/profiling.h"
 
+#include <boost/algorithm/string.hpp>
+
 using namespace util;
 using namespace util::io;
 
-namespace tfkld{
-namespace type{
-  
-using int_t = int;
-//using int64_t = int64_t;
-using float_t = float;
-using char_t = char;
-}//namespace tfkld::type
-}//namespace tfkld
-
-using namespace tfkld::type;
-
-using hashmap_t = std::map<int64_t, int_t>;
-using vocab_t = std::map<std::string, int64_t>;
-//using vocab_t = std::unordered_set<std::string>;
-using doc_t = std::vector<hashmap_t>;
-
-namespace tfkld{
-
-struct TokenizedFile{
-    TokenizedFile(std::string train_file) {
-        val.open(train_file, std::ifstream::in);
-
-        if(val.fail()) {
-            std::cout << "ERROR: training data file not found!\n";
-            exit(1);
-        }
-    }
-
-    void setBegin() {
-        val.clear();
-        val.seekg(0);
-    }
-    std::ifstream val; 
-};
-
-struct SpValue{
-    SpValue() {
-        row = 0;
-        col = 0;
-        val = 0.0;
-    }
-
-    void reset() {
-        row = 0;
-        col = 0;
-        val = 0.0;
-    }
-    
-    int64_t row;
-    int64_t col;
-    float_t val;
-};
-
-vocab_t LearnVocab(TokenizedFile &file) {
-  std::string line;
-  vocab_t vocab;
-  int64_t word_idx = 0;
-  int64_t count = 0;
-  while(std::getline(file.val, line)){
-    count++;
-    if(count % 1000 == 0) std::cout << "\r" << count << " lines.";
-
-    std::istringstream iss{line};
-    auto words = util::string::split(line);
-    for(auto x : words) {
-        auto isin = vocab.find(x);
-        if(isin == vocab.end()) {
-            vocab[x] = word_idx;
-            word_idx++;
-        }
-    }
-  }
-  // return sorted vocabulary with ascending order
-  return vocab;
-}
-
-doc_t LearnDoc(vocab_t &vocab, TokenizedFile &file) {
-    std::string line;
-    doc_t docs;
-    hashmap_t doc;
-    int64_t count = 0;
-    while (std::getline(file.val, line)) {
-        count++;
-        if(count % 1000 == 0) std::cout << "\r" << count << " lines.";
-        
-        std::istringstream iss{line};
-        auto words = util::string::split(line);
-        for(auto x : words) {
-            auto word_idx = vocab.find(x) -> second;
-            auto isin = doc.find(word_idx);
-            if(isin != doc.end()) {
-                doc[word_idx] += 1;
-            } else {
-                doc[word_idx] = 1;
-            }
-        }
-        docs.push_back(doc);
-        doc.clear();
-    }
-
-    return docs;
-}
-
-void fillValue(std::vector<SpValue> &values, int64_t &count, vocab_t const &vocab, doc_t const &docs) {    
-    SpValue value;
-    for(auto it = docs.begin(); it != docs.end(); ++it) {
-        value.reset();
-        for(auto itt = it -> begin(); itt != it -> end(); ++itt) {
-            value.row = (*itt).first;
-            value.col = std::distance(docs.begin(),it);
-            value.val = (*itt).second;
-            values.push_back(value);
-            count++;
-        }
-    }   
-}
-
-float_t val_idf(int64_t D, int_t Dt) {
-    return log(D/(float_t)Dt);
-}
-    
-void MakeTFIDF(std::vector<SpValue> &values, int64_t &count, vocab_t const &vocab, doc_t const &docs) {
-    hashmap_t df;
-    int64_t D = docs.size();
-    
-    for(auto x : values) {
-        auto isin = df.find(x.row);
-        if(isin != df.end()) {
-            df[x.row] += 1;
-        } else {
-            df[x.row] = 1;
-        }
-    }
-
-    std::vector<float_t> idf;
-    for(auto x : df) {
-        idf.push_back(val_idf(D,x.second));
-    }
-
-    for(auto &x : values) {
-        x.val *= idf[x.row];
-    }
-
-}
-
-void fillMat(std::vector<SpValue> &values, int64_t &count, vocab_t const &vocab, doc_t const &docs, arma::sp_mat &mat) {
-
-    
-    arma::umat location(2,count);
-    arma::vec value(count);
-
-    for(int64_t a = 0; a < count; a++) {
-        location(0,a) = values[a].row;
-        location(1,a) = values[a].col;
-        value(a) = values[a].val;
-    }
-
-    mat = std::move( arma::sp_mat(location, value) );
-}
-    
-void PrintVocab(vocab_t &vocab){
-    for(auto x : vocab){
-        std::cout << x.first << std::endl;
-    }
-}
-
-auto Concat(std::vector<std::string> const &words){
-    std::vector<char> vec;
-    for(auto const &x:words){
-        std::copy(x.cbegin(),x.cend(),std::back_inserter(vec));
-        vec.push_back('\0');
-    }
-    return vec;
-}
-auto ToStrings(std::vector<char> const &concat_words){
-    std::vector<std::string> words;
-    auto it =concat_words.cbegin();
-    auto end=concat_words.cend();
-    while(it!=end){
-        words.push_back(std::string{&(*it)});
-        //std::cout<<std::string{&(*it)}<<std::endl;
-        it=std::find(it, end, '\0');
-        ++it;
-    }
-    return words;
-}
-}//namespace tfkld
-
-
-
-
 
 int main(){
+
     using namespace tfkld;
     using namespace arma;
 
     auto timer = Timer{};
-    
-    std::string train_file = "1K.training";
-    TokenizedFile infile{train_file};
 
-    auto vocab = LearnVocab(infile);
+    int K_dim = 100;
+    
+    std::string fin_name = "Tk_msr_paraphrase_train.txt";
+    MSParaFile fin{fin_name};
+
+    auto vocab = LearnVocab(fin);
     timer.here_then_reset("\nConstructed Vocabulary.\n");
-    infile.setBegin();
-    auto docs = LearnDoc(vocab, infile);
-    timer.here_then_reset("\nConstructed Documents.\n");
+    fin.setBegin();
+    auto docs = LearnPara(vocab,fin);
+    timer.here_then_reset("\nConstructed Paragraphs.\n");
+    fin.setBegin();
+    auto tag = LearnTag(fin);
+    timer.here_then_reset("\nConstructed Tag.\n");
     
     int64_t n_rows, n_cols;
     n_rows = vocab.size();
     n_cols = docs.size();
-    //(n_rows, n_cols)
-    sp_mat inMat(n_rows,n_cols);
 
-    //int64_t pos = std::distance(vocab.begin(), vocab.find("that"));
-    //std::cout << pos << std::endl;
-    
+    sp_mat inMat(n_rows, n_cols);
+
     std::vector<SpValue> values;
-    
-    int64_t count = 0;    
+    std::vector<float_t> idf;
+    std::vector<float_t> kld;
+    int64_t count = 0;
+
     fillValue(values, count, vocab, docs);
-    MakeTFIDF(values, count, vocab, docs);
+    //MakeTFIDF(idf, values, count, vocab, docs);
+    MakeTFKLD(kld, tag, values, count, vocab, docs);
     fillMat(values, count, vocab, docs, inMat);
 
     timer.here_then_reset("Filled the Matrix.\n");
     
-    //inMat.print("inMat = ");
-
     mat U;
     vec s;
     mat V;
 
-    svds(U, s, V, inMat, 100);
+    svds(U,s,V,inMat,K_dim);
 
-    timer.here_then_reset("Completed SVD calculation!\n");
+    timer.here_then_reset("SVD is complete.\n");
+
+    auto svec = makeSimMat(V);
+
+    timer.here_then_reset("Made Similarity Vectors.\n");
     
-    std::cout << "Finished!" << std::endl;
+    std::ofstream fout{"train_KLD.dat"};
 
-    //sp_mat tfidf = MakeTFIDF(inMat);
-    //_mat A = sprandu<sp_mat>(5,5,0.1);
-    //col(0) *= 10000;
-    //A.print("A = ");
-    //auto a = nonzeros(A.col(0)).size();
-    //std::cout << a << std::endl;
-    //for(auto x : v ) std::cout << x << std::endl;
+    count = 0;
+    int lcount = 1;
+    for(auto x : svec) {
+        fout << tag[count] << " ";
+        for(auto y : x) {
+            fout << lcount << ":" << y << " ";
+            lcount++;
+        }
+        fout << "\n";
+        lcount = 1;
+        count++;
+    }
+
+    timer.here_then_reset("Writing Similarity Vectors is Done.\n");
+
+    std::string vocab_filename = "vocab.dat";
+    std::ofstream vocab_out{vocab_filename};
+
+    std::string kld_filename = "kld.dat";
+    std::ofstream kld_out{kld_filename};
     
-    //timer.here_then_reset("Testing vectorising.\n");
+    for(auto x : vocab) {
+        vocab_out << x.first << " " << x.second << std::endl;
+    }
 
-    //arma::mat inMat = arma::randu<arma::mat>(5,5);
+    for(auto x : kld) {
+        kld_out << x << std::endl;
+    }
 
 
-
+    
     /*
-    sp_mat X = sprandu<sp_mat>(50000, 200000, 0.001);
-
-    mat U;
-    vec s;
-    mat V;
-
-    svds(U, s, V, X, 200);
-
-
-
-
-    //X.print("X = ");
-    //U.print("U = ");
-    s.print("S = ");
-    //V.print("V = ");
-    
-
-
-    */
-    /*
-    arma::mat inMat = arma::randu<arma::mat>(5,5);
-
-    arma::mat U;
-    arma::vec s;
-    arma::mat V;
-
-    arma::svd(U,s,V,inMat);
-
-
-    inMat.print("inMat = ");                                                                                                                                                  U.print("U = ");                                                                                                                                                          s.print("S = ");                                                                                                                                                          V.print("V = ");
+    H5file file{H5name{"data.h5"}, hdf5::FileMode::create};
+    auto vocab_word = getVocabWord(vocab);
+    auto vocab_index = getVocabIndex(vocab);
+    std::vector<char> concat_words = Concat(vocab_word);
+    file.writeRawData(H5name{"MSR.training.vocab.word"},concat_words);
+    file.writeRawData(H5name{"MSR.training.vocab.index"},vocab_index);
     */
 
-
-    //PrintVocab(vocab);
-    //std::vector<char> concat_words = Concat(word_count_keys);
     
-    //H5file file{H5name{"data.h5"}, hdf5::FileMode::replace};
-    //file.writeRawData(H5name{"1b.training.1M.count"},word_count_values);
-    //file.writeRawData(H5name{"1b.training.1M.word"},concat_words);
+    vocab_out.close();
+    kld_out.close();
+
+    std::string fin_name2 = "Tk_msr_paraphrase_test.txt";
+    MSParaFile fin2{fin_name2};
+
+    std::string vocabread_filename = "vocab.dat";
+    std::ifstream vocabread_in{vocabread_filename};
+
+    //auto vocab2 = ReadVocab(vocabread_in);
+    auto vocab2 = vocab;
     
-    //concat_read = file.readRawData(H5name{"bar.word_key"},concat_words); 
-    //auto words = ToStrings(concat_read);
-    //assert(words=word_count_keys);
-    /*
+    auto docs2 = LearnPara(vocab2,fin2);
+    timer.here_then_reset("\nConstructed Paragraphs.\n");
+    fin2.setBegin();
+    auto tag2 = LearnTag(fin2);
+    timer.here_then_reset("\nConstructed Tag.\n");
 
-  //fvec column vector                                                                                                                                                     
+    int64_t n_rows2, n_cols2;
+    n_rows2 = vocab2.size();
+    n_cols2 = docs2.size();
 
-  arma::fmat inMat;
-  arma::fvec v1;
-  arma::fvec v2;
-  arma::fvec v3;
+    sp_mat inMat2(n_rows2, n_cols2);
 
-  v1 = {1, 2, 3};
-  v2 = {2, 3, 4};
-  v3 = {3, 4, 9};
+    std::vector<SpValue> values2;
+    int64_t count2 = 0;
 
-  std::vector<arma::fvec> v;
-  std::vector<arma::fvec> rv;
-  v.push_back(v1);
-  v.push_back(v2);
-  v.push_back(v3);
+    fillValue(values2, count2, vocab2, docs2);
+    //MakeTFIDF(idf, values2);
+    MakeTFKLD(kld, values2);
+    fillMat(values2, count2, vocab2, docs2, inMat2);
+    
+    timer.here_then_reset("Filled the Matrix.\n");
 
-  rv = makeBasis(v);
-  makeNormal(rv);
-  for(int i = 0; i < rv.size(); i++) {
-    rv[i].print();
-  }
+    mat U2;
+    vec s2;
+    mat V2;
 
-  inMat = {{1,2,3},{2,3,4},{3,4,9}};
-  std::cout << rank(inMat) << std::endl;
+    svds(U2,s2,V2,inMat2,K_dim);
 
-  //arma::fmat U;                                                                                                                                                          
-  //arma::fvec S;                                                                                                                                                          
-  //arma::fmat V;                                                                                                                                                          
+    timer.here_then_reset("SVD is complete.\n");
+
+    auto svec2 = makeSimMat(V2);
+    normalizeSimMat(svec2);
+    
+    timer.here_then_reset("Made Similarity Vectors.\n");
+
+    std::ofstream fout2{"test_KLD.dat"};
+
+    count = 0;
+    lcount = 1;
+
+    for(auto x : svec2) {
+        fout2 << tag2[count] << " ";
+        for(auto y : x) {
+            fout2 << lcount << ":" << y << " ";
+            lcount++;
+        }
+        fout2 << "\n";
+        lcount = 1;
+        count++;
+    }
+
+    timer.here_then_reset("Writing Similarity Vectors is Done.\n");
 
 
-  //arma::svd(U, S, V, inMat);                                                                                                                                             
-
-  //inMat.print("inMat = ");                                                                                                                                               
-  //U.print("U = ");                                                                                                                                                       
-  //S.print("S = ");                                                                                                                                                       
-  //V.print("V = ");                    
-
-
-
-
-
-     */
-
+    fout2.close();
     
     return 0;
 }
