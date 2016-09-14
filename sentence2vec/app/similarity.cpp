1e8e4c9cd0456c66ea4b4cfefbccd67aa170a9df
Jihun Kim <jihun@uphere.co>
2016-09-14 06:48:36 +0000

    Simple reqeust/reply server of the sentence2vec similarity app.

--- sentence2vec/app/similarity.cpp
+++ sentence2vec/app/similarity.cpp
@@ -1,5 +1,9 @@
 #include <fstream>
 
+#include <zmq.hpp>
+#include "json/json.hpp"
+#include "tbb/task_group.h"
+
 #include "wordrep/sentence2vec.h"
 
 #include "parser/parser.h"
@@ -10,9 +14,6 @@
 #include "utils/print.h"
 #include "utils/loop_gen.h"
 
-#include "json/json.hpp"
-
-#include "tbb/task_group.h"
 
 using namespace sent2vec;
 using namespace rnn::wordrep;
@@ -203,12 +204,37 @@ int main(){
     SimilaritySearch engine{config};
     timer.here_then_reset("Search engine loaded.");
     auto input = load_json("/data/groups/uphere/similarity_test/queries.json");
+
+    const char * protocol = "tcp://*:5555";
+    zmq::context_t context (1);
+    zmq::socket_t socket (context, ZMQ_REP);
+    socket.bind(protocol);
+    while(1){
+        zmq::message_t request;
+        socket.recv (&request);
+        auto query = json::parse((const char*)request.data());
+        std::cerr << query.dump(4) << std::endl;
+
+        auto task=[&,query]() {
+            auto answer = engine.process_queries(query);
+            timer.here_then_reset("Query is answered.");
+            std::string aa{answer.dump(4)};
+            zmq::message_t reply(aa.size());
+            std::cerr<<aa.size()<<std::endl;
+            std::cerr<<answer.size()<<std::endl;
+            std::memcpy ((void *) reply.data (), (void*)aa.data(), aa.size());
+            socket.send (reply);
+        };
+        g.run(task);
+        g.wait();
+    }
+
     auto task=[&]() {
         auto answer = engine.process_queries(input);
-        //timer.here_then_reset("Queries answered.");
+        timer.here_then_reset("Query is answered.");
         std::cout << answer.dump(4) << std::endl;
     };
-    for(int i=0; i<100; ++i) {
+    for(int i=0; i<1; ++i) {
         g.run(task);
         std::cerr << i << std::endl;
     }
