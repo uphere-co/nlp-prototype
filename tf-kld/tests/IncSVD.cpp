336b8b387a8d9773dc2b72744394c02e1149abb6
Doh Young Mo <modohyoung@gmail.com>
2016-10-03 02:34:10 +0000

    Matrix bound check is added for debug.

--- tf-kld/tests/IncSVD.cpp
+++ tf-kld/tests/IncSVD.cpp
@@ -9,15 +9,30 @@
 
 #include <armadillo>
 
-int main() {
-    using namespace arma;
+using namespace arma;
 
-    int r_dim = 3; // Reduced rank r
+bool checkMat(int row, int col, mat const &mat) {
+    if(mat.n_rows == row && mat.n_cols == col) return true;
+    return false;
+}
+bool checkVec(int dim, vec const &vec) {
+    if(vec.size() == dim) return true;
+    return false;
+}
+
+void exit_with_error(std::string error_message) {
+    std::cout << error_message << std::endl;
+    exit(1);
+}
+
+int main() {
+
+    int r_dim = 5; // Reduced rank r
 
     // TF matrix for the test.
     // n_dim by m_dim .
-    int n_dim = 4;
-    int m_dim = 4;
+    int n_dim = 100;
+    int m_dim = 100;
     
     sp_mat inM(n_dim,m_dim);
     for(int i = 0; i < n_dim; i++) {
@@ -32,6 +47,10 @@ int main() {
 
     svds(inU,ins,inV,inM,r_dim);
 
+    if(checkMat(n_dim,r_dim,inU) == false) exit_with_error("U matrix is incorrect.");
+    if(checkVec(r_dim,ins) == false) exit_with_error("s diagonal matrix is incorrect.");
+    if(checkMat(m_dim,r_dim,inV) == false) exit_with_error("V matrix is incorrect.");
+    
     // incremental n_dim by c_dim matrix
     int c_dim = 1;
     mat inC(n_dim,c_dim);
@@ -41,16 +60,23 @@ int main() {
         }
     }
 
+    if(checkMat(n_dim,c_dim,inC) == false) exit_with_error("C matrix is incorrect.");
+    
     mat inL = trans(inU)*inC;
-
+    if(checkMat(r_dim,c_dim,inL) == false) exit_with_error("L matrix is incorrect.");
+    
     mat inH;
     inH = inC - inU*inL;
+    if(checkMat(n_dim,c_dim,inH) == false) exit_with_error("H matrix is incorrect.");
 
     mat inJ;
     mat inK;
 
     qr(inJ,inK,inH);
 
+    if(checkMat(n_dim,n_dim,inJ) == false) exit_with_error("J matrix is incorrect.");
+    if(checkMat(n_dim,c_dim,inK) == false) exit_with_error("K matrix is incorrect.");
+    
     mat inQ(r_dim+n_dim,r_dim+c_dim,fill::zeros);
     
     for(int r=0;r<r_dim;r++)
@@ -67,6 +93,8 @@ int main() {
             inQ(r_dim+i,r_dim+j)=inK(i,j);
         }
     }
+
+    if(checkMat(r_dim+n_dim,r_dim+c_dim,inQ) == false) exit_with_error("Q matrix is incorrect.");
     
     mat inUp;
     vec insp;
@@ -74,12 +102,20 @@ int main() {
     
     svd(inUp,insp,inVp,inQ);
 
+    if(checkMat(r_dim+n_dim,r_dim+n_dim,inUp) == false) exit_with_error("Up matrix is incorrect.");
+    if(checkVec(r_dim+c_dim,insp) == false) exit_with_error("sp diagonal matrix is incorrect.");
+    // Assuming r_dim+c_dim <= r_dim+n_dim
+    if(checkMat(r_dim+c_dim,r_dim+c_dim,inVp) == false) exit_with_error("Vp matrix is incorrect.");
+
+    
     mat inUpp;
     vec inspp;
     mat inVpp;
 
-    mat mapU = join_rows(inU,inJ);
-
+    mat mapU;
+    mapU = join_rows(inU,inJ);
+    if(checkMat(n_dim,r_dim+n_dim,mapU) == false) exit_with_error("mapU matrix is incorrect.");
+    
     mat mapV(m_dim+c_dim,r_dim+c_dim,fill::zeros);
     for(int i=0;i<m_dim;i++) {
         for(int j=0;j<r_dim;j++) {
@@ -90,7 +126,8 @@ int main() {
     for(int i=0;i<c_dim;i++) {
         mapV(m_dim+i,r_dim+i) = 1; 
     }
-
+    if(checkMat(m_dim+c_dim,r_dim+c_dim,mapV) == false) exit_with_error("mapV matrix is incorrect.");    
+    
     inUpp = mapU * inUp;
     inspp = insp;
     inVpp = mapV * inVp;
@@ -101,12 +138,17 @@ int main() {
         inspp_diag(i,i) = insp(i);
     }
 
+    if(checkMat(n_dim,n_dim+r_dim,inUpp) == false) exit_with_error("Upp matrix is incorrect.");
+    if(checkMat(n_dim+r_dim,r_dim+c_dim,inspp_diag) == false) exit_with_error("inspp_diag matrix is incorrect.");
+    if(checkMat(m_dim+c_dim,r_dim+c_dim,inVpp) == false) exit_with_error("Vpp matrix is incorrect.");
+
+
     
     mat resMat = inUpp*inspp_diag*trans(inVpp);
 
-    inM.print("inM = ");
-    inC.print("inC = ");
     resMat.print("resMat = ");
+    // End of updating SVD (slow way)
 
+    
     return 0;
 }
