7085e9b3868424c880ed069d8ae60e489a707d59
Ian-Woo Kim <ianwookim@gmail.com>
2016-10-04 09:53:02 +0000

    separate out template.hh and enable unique_ptr_wrapper generally useful.

--- query/csrc/invoke.cpp
+++ query/csrc/invoke.cpp
@@ -8,34 +8,21 @@
 // 
 #include "similarity/similarity.h"
 #include "utils/json.h"
+#include "template.hh"
 
 using json_t = nlohmann::json;
 using namespace std;
 
-template<class T> 
-class unique_ptr_wrapper {
-private:
-    unique_ptr<T> p_uniq; 
-public:
-    unique_ptr_wrapper( unique_ptr<T>& p ) { p_uniq = std::move(p); }
-    T* get() { return p_uniq.get(); }
-};
-
-//typedef unique_ptr_wrapper<json_t>* json_p;
-
-typedef unique_ptr_wrapper<json_t> unique_ptr_wrapper_json_t;
-// opaque pointer
-typedef unique_ptr_wrapper_json_t* json_p;
-
+unique_ptr_wrapper_type(json_t)
 
 extern "C" {
     
-    json_p      json_create    ( char*  str    );
-    void        json_finalize  ( json_p p      );
-    const char* json_serialize ( json_p output );
+    json_t_p    json_create    ( char*  str      );
+    void        json_finalize  ( json_t_p p      );
+    const char* json_serialize ( json_t_p output );
     
     void        query_init     ( char*   configfile );
-    json_p      query          ( json_p input      );
+    json_t_p    query          ( json_t_p input     );
     void        query_finalize ( void               );
 }
 
@@ -46,18 +33,18 @@ SimilaritySearch* engine;
 Timer timer{};
 
     
-json_p json_create( char* str )
+json_t_p json_create( char* str )
 {
     auto input = make_unique<json_t>( json::parse(str) ) ;
     return new unique_ptr_wrapper<json_t>(input);
 }
 
-void json_finalize( json_p p )
+void json_finalize( json_t_p p )
 {
     delete p;
 }
 
-const char* json_serialize( json_p p )
+const char* json_serialize( json_t_p p )
 {
     json_t* output = p->get();
     stringstream ss;
@@ -76,7 +63,7 @@ void query_init( char* configfile )
     timer.here_then_reset("Search engine loaded."); 
 }
 
-json_p query( json_p input )
+json_t_p query( json_t_p input )
 {
     auto answer = make_unique<json_t>( engine->process_queries(*(input->get()) )) ;
     timer.here_then_reset("Query is answered.");
---
+++ query/csrc/template.hh
@@ -0,0 +1,17 @@
+#include <memory>
+
+using namespace std;
+
+template<class T> 
+class unique_ptr_wrapper {
+private:
+    unique_ptr<T> p_uniq; 
+public:
+    unique_ptr_wrapper( unique_ptr<T>& p ) { p_uniq = std::move(p); }
+    T* get() { return p_uniq.get(); }
+};
+
+
+#define unique_ptr_wrapper_type(T)                              \
+  typedef unique_ptr_wrapper<T> unique_ptr_wrapper_ ## T ;      \
+  typedef unique_ptr_wrapper_ ## T * T ## _p;
