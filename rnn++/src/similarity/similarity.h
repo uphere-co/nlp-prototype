88074639883cdbd6ed72abdcff558b5c301167b4
Jihun Kim <jihun@uphere.co>
2016-10-09 15:49:44 +0000

    Separated type-related parameters in config.json in similarity engines

--- rnn++/src/similarity/similarity.h
+++ rnn++/src/similarity/similarity.h
@@ -13,10 +13,13 @@ struct SimilaritySearch{
     using voca_info_t = rnn::simple_model::VocaInfo;
     using param_t     = rnn::simple_model::Param;
     SimilaritySearch(json_t const &config)
-    :   sent_vecs{rnn::wordrep::load_voca_vecs<param_t::dim>(config["phrase_store"], config["phrase_vec"], util::datatype_from_string(config["float_t"]))},
+    :   sent_vecs{rnn::wordrep::load_voca_vecs<param_t::dim>(config["phrase_store"], config["phrase_vec"],
+                                                             util::datatype_from_string(config["w2v_float_t"]))},
         phrase_voca{rnn::wordrep::load_voca(config["phrase_store"], config["phrase_word"])},
-        param{rnn::simple_model::load_param(config["rnn_param_store"], config["rnn_param_uid"], util::datatype_from_string(config["float_t"]))},
-        rnn{config["wordvec_store"], config["voca_name"], config["w2vmodel_name"], util::datatype_from_string(config["float_t"])}
+        param{rnn::simple_model::load_param(config["rnn_param_store"], config["rnn_param_uid"],
+                                            util::datatype_from_string(config["param_float_t"]))},
+        rnn{config["wordvec_store"], config["voca_name"], config["w2vmodel_name"],
+            util::datatype_from_string(config["w2v_float_t"])}
     {}
 
     json_t static parse(const char *query) {return json_t::parse(query);}
@@ -55,7 +58,8 @@ struct BoWVSimilaritySearch{
     using json_t = nlohmann::json;
     using voca_info_t = rnn::simple_model::VocaInfo;
     BoWVSimilaritySearch(json_t const &config)
-    : rnn{config["wordvec_store"], config["voca_name"], config["w2vmodel_name"], util::datatype_from_string(config["float_t"])},
+    : rnn{config["wordvec_store"], config["voca_name"], config["w2vmodel_name"],
+          util::datatype_from_string(config["w2v_float_t"])},
       text{util::get_string_val(config,"textset")}, lines{util::get_string_val(config,"textset")}
     {}
     json_t process_queries(json_t ask) const;
