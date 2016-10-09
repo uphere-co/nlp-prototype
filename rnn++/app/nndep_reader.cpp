1194f7149ad0ea55f69818c82f8237a639cd4294
Jihun Kim <jihun@uphere.co>
2016-10-09 18:51:29 +0000

    Serialize/deserialize to disk for ParsedWordIdx.

--- rnn++/app/nndep_reader.cpp
+++ rnn++/app/nndep_reader.cpp
@@ -48,14 +48,33 @@ std::vector<int64_t> get_voca_idxs(rnn::wordrep::VocaIndexMap const &word2idx,
 
 struct ParsedWordIdx{
     ParsedWordIdx(ParsedWord const &words, rnn::wordrep::VocaIndexMap const &word2idx)
-            : sent_idx{words.sent_idx},
-              word{get_voca_idxs(word2idx, words.word)},
-              idx_word{words.idx_word},
-              head_word{get_voca_idxs(word2idx, words.head_word)},
-              idx_head{words.idx_head},
-              pos_raw{words.pos_raw},
-              pos{util::string::unpack_word_views(pos_raw)}
+    : sent_idx{words.sent_idx},
+      word{get_voca_idxs(word2idx, words.word)},
+      idx_word{words.idx_word},
+      head_word{get_voca_idxs(word2idx, words.head_word)},
+      idx_head{words.idx_head},
+      pos_raw{util::string::pack_words(words.pos)},
+      pos{util::string::unpack_word_views(pos_raw)}
     {}
+    ParsedWordIdx(util::io::H5file const &file, std::string prefix)
+    : sent_idx{file.getRawData<int64_t>(H5name{prefix+".sent_idx"})},
+      word{file.getRawData<int64_t>(H5name{prefix+".word"})},
+      idx_word{file.getRawData<int64_t>(H5name{prefix+".idx_word"})},
+      head_word{file.getRawData<int64_t>(H5name{prefix+".head_word"})},
+      idx_head{file.getRawData<int64_t>(H5name{prefix+".idx_head"})},
+      pos_raw{file.getRawData<char>(H5name{prefix+".POS"})},
+      pos{util::string::unpack_word_views(pos_raw)}
+    {}
+
+    void write_to_disk(std::string filename, std::string prefix) const {
+        H5file outfile{H5name{filename}, hdf5::FileMode::replace};
+        outfile.writeRawData(H5name{prefix+".sent_idx"}, sent_idx);
+        outfile.writeRawData(H5name{prefix+".word"},     word);
+        outfile.writeRawData(H5name{prefix+".idx_word"}, idx_word);
+        outfile.writeRawData(H5name{prefix+".head_word"},head_word);
+        outfile.writeRawData(H5name{prefix+".idx_head"}, idx_head);
+        outfile.writeRawData(H5name{prefix+".POS"},      pos_raw);
+    }
 
     std::vector<int64_t>     sent_idx;
     std::vector<int64_t>     word;
@@ -66,12 +85,16 @@ struct ParsedWordIdx{
     std::vector<const char*> pos;
 };
 
+
 int main(){
-    H5file infile{H5name{"news.Google.h5"}, hdf5::FileMode::read_exist};
-    ParsedWord news{infile, "test"};
+//    H5file infile{H5name{"news.Google.h5"}, hdf5::FileMode::read_exist};
+//    ParsedWord news{infile, "test"};
+//    ParsedWordIdx news_indexed{news, word2idx};
+//    news_indexed.write_to_disk("news.dep.h5", "test");
     auto voca = rnn::wordrep::load_voca("news.h5", "news.en.words");
     auto word2idx = voca.indexing();
-    ParsedWordIdx news_indexed{news, word2idx};
+    H5file infile{H5name{"news.dep.h5"}, hdf5::FileMode::read_exist};
+    ParsedWordIdx news_indexed{infile, "test"};
 
     auto beg=news_indexed.sent_idx.cbegin();
     auto end=news_indexed.sent_idx.cend();
--- rnn++/utils/string.h
+++ rnn++/utils/string.h
@@ -11,6 +11,8 @@ std::vector<std::string> split(std::string words, const char *delim=" \t");
 std::vector<std::string> readlines(std::string file);
 
 std::vector<char> pack_words(std::vector<std::string> const &words);
+std::vector<char> pack_words(std::vector<const char*> const &words);
+
 std::vector<std::string> unpack_words(std::vector<char> const &concat_words);
 std::vector<const char *> unpack_word_views(std::vector<char> const &concat_words);
 
