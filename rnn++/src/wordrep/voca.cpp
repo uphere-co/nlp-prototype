abc5329a67aa93fc6c36cf30f8c879b6819b16a2
Jihun Kim <jihun@uphere.co>
2016-10-14 19:07:22 +0000

    Simplify header dependency of VocaIndexMap

--- rnn++/src/wordrep/voca.cpp
+++ rnn++/src/wordrep/voca.cpp
@@ -3,10 +3,45 @@
 #include "wordrep/voca.h"
 
 #include "utils/hdf5.h"
+#include "utils/string.h"
 
 using namespace util::io;
 
 namespace wordrep{
+
+
+VocaIndexMap::VocaIndexMap(std::vector<idx_t::val_t> uids_val) {
+    auto n = uids_val.size();
+    for(decltype(n)i=0; i!=n; ++i){
+        idx_t uid{uids_val[i]};
+        VocaIndex idx{i};
+        uid2idx[uid]=idx;
+        idx2uid[idx]=uid;
+    }
+}
+VocaIndex VocaIndexMap::operator[](idx_t uid) const {
+    //return uid2idx[uid];
+    auto it=uid2idx.find(uid);
+    if(it==uid2idx.cend()) return VocaIndex{};
+    return it->second;
+}
+VocaIndexMap::idx_t VocaIndexMap::operator[](VocaIndex idx) const {
+    //return idx2uid[idx];
+    auto it=idx2uid.find(idx);
+    if(it==idx2uid.cend()) return idx_t{};
+    return it->second;
+}
+std::vector<VocaIndex> VocaIndexMap::getIndex(std::string sentence) const {
+    auto tokens = util::string::split(sentence);
+    WordUIDindex wordUIDs{"/home/jihuni/word2vec/ygp/words.uid"};
+    std::vector<VocaIndex> idxs;
+    for(auto const &word : tokens){
+        idxs.push_back((*this)[wordUIDs[word]]);
+    }
+    return idxs;
+}
+
+
 
 std::vector<WordUID::val_t> load_voca(std::string h5name, std::string voca_name){
     H5file file{H5name{h5name}, hdf5::FileMode::read_exist};
--- rnn++/src/wordrep/voca.h
+++ rnn++/src/wordrep/voca.h
@@ -4,8 +4,6 @@
 #include "utils/base_types.h"
 #include "wordrep/word_uid.h"
 
-#include "utils/string.h"
-
 namespace wordrep{
 struct VocaIndexDummy{};
 using VocaIndex = util::IntegerLike<VocaIndexDummy,-1>; //UID -1 for unknown words.
@@ -13,36 +11,10 @@ using VocaIndex = util::IntegerLike<VocaIndexDummy,-1>; //UID -1 for unknown wor
 class VocaIndexMap{
 public:
     using idx_t = WordUID;
-    VocaIndexMap(std::vector<idx_t::val_t> uids_val) {
-        auto n = uids_val.size();
-        for(decltype(n)i=0; i!=n; ++i){
-            idx_t uid{uids_val[i]};
-            VocaIndex idx{i};
-            uid2idx[uid]=idx;
-            idx2uid[idx]=uid;
-        }
-    }
-    VocaIndex operator[](idx_t uid) const {
-        //return uid2idx[uid];
-        auto it=uid2idx.find(uid);
-        if(it==uid2idx.cend()) return VocaIndex{};
-        return it->second;
-    }
-    idx_t operator[](VocaIndex idx) const {
-        //return idx2uid[idx];
-        auto it=idx2uid.find(idx);
-        if(it==idx2uid.cend()) return idx_t{};
-        return it->second;
-    }
-    std::vector<VocaIndex> getIndex(std::string sentence) const {
-        auto tokens = util::string::split(sentence);
-        WordUIDindex wordUIDs{"/home/jihuni/word2vec/ygp/words.uid"};
-        std::vector<VocaIndex> idxs;
-        for(auto const &word : tokens){
-            idxs.push_back((*this)[wordUIDs[word]]);
-        }
-        return idxs;
-    }
+    VocaIndexMap(std::vector<idx_t::val_t> uids_val);
+    VocaIndex operator[](idx_t uid) const;
+    idx_t operator[](VocaIndex idx) const;
+    std::vector<VocaIndex> getIndex(std::string sentence) const;
 
     bool isin(idx_t uid) {return uid2idx[uid]!=VocaIndex{};}
 //private:
