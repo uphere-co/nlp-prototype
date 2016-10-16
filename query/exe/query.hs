5eaef1eabfe1c62e2f306158307b4c4d41f378f6
Ian-Woo Kim <ianwookim@gmail.com>
2016-10-16 14:01:11 +0000

    first successful dependent parsing query from the web interface.

--- query/exe/query.hs
+++ query/exe/query.hs
@@ -15,21 +15,27 @@ import           Control.Distributed.Process.Closure
 import           Control.Distributed.Process.Node          (initRemoteTable,newLocalNode,runProcess)
 import           Control.Distributed.Process.Serializable
 import           Data.Aeson
+import           Data.Aeson.Encode                         (encodeToBuilder)
+import qualified Data.Attoparsec                     as A
 import qualified Data.Binary                         as Bi (encode,decode)
+import qualified Data.Binary.Builder                 as B  (toLazyByteString)
 import qualified Data.ByteString.Base64.Lazy         as B64
 import           Data.ByteString.Char8                     (ByteString)
 import qualified Data.ByteString.Char8               as B
 
 import qualified Data.ByteString.Lazy.Char8          as BL
 import           Data.ByteString.Unsafe                    (unsafeUseAsCStringLen,unsafePackCString)
+import qualified Data.HashMap.Strict                 as HM
 import qualified Data.Map                         as M
 
 import           Data.Text                                 (Text)
 import qualified Data.Text                           as T
 import qualified Data.Text.Encoding                  as TE
+import qualified Data.Text.Lazy.Encoding             as TLE
 import qualified Data.Text.IO                        as TIO
 import           Data.UUID                                 (toString)
 import           Data.UUID.V4                              (nextRandom)
+import qualified Data.Vector                         as V
 import           Foreign.C.String
 import           Foreign.C.Types                           (CInt(..))
 import           Foreign.ForeignPtr
@@ -73,17 +79,25 @@ json_serialize p = withForeignPtr p c_json_serialize
 runCoreNLP body = do
   -- let body = "establishing \rthe ecological criteria for awarding the EU ecolabel"
   -- body <- TE.encodeUtf8 <$> TIO.getContents 
-  lbstr <- simpleHttpClient methodPost "http://mark:9000/?properties={%22annotators%22%3A%22depparse%2Cpos%22%2C%22outputFormat%22%3A%22json%22}" (Just body)
+  lbstr <- simpleHttpClient methodPost "http://192.168.1.104:9000/?properties={%22annotators%22%3A%22depparse%2Cpos%22%2C%22outputFormat%22%3A%22json%22}" (Just body)
   -- BL.toStrict lbstr
   let txt' = TE.decodeUtf8 (BL.toStrict lbstr)
       txt = T.filter (>=' ') txt'
-  TIO.putStrLn txt
+      r_bstr = TE.encodeUtf8 txt
+  let Right (Object c) = A.parseOnly json r_bstr
+      c' = Object (HM.insert "queries" (Array (V.fromList [String (TE.decodeUtf8 body)])) c)
+  print c'
+  -- TIO.putStrLn txt
   -- print txt
+
+  
+  (return . BL.toStrict . B.toLazyByteString . encodeToBuilder) c' -- r_bstr
   
 queryWorker :: SendPort BL.ByteString -> Query -> Process ()
 queryWorker sc q = do
-  let r = encode (makeJson q)
-      bstr = BL.toStrict r 
+  -- let r = encode (makeJson q)
+  --     bstr = BL.toStrict r
+  bstr <- (liftIO . runCoreNLP . B.pack . head . querySentences) q
   bstr' <- liftIO $ B.useAsCString bstr $ 
     json_create >=> query >=> json_serialize >=> unsafePackCString
   liftIO $ B.putStrLn bstr'
