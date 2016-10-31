b462d50f177406adfe19222770bacdc2953f1a6b
Jihun Kim <jihun@uphere.co>
2016-10-31 08:44:58 +0000

    Cache word cutoffs from WordImportance.

--- rnn++/src/wordrep/word_prob.cpp
+++ rnn++/src/wordrep/word_prob.cpp
@@ -7,7 +7,14 @@ using namespace util::io;
 namespace wordrep{
 
 WordImportance::WordImportance(H5file const& stats)
-        : ratios{stats.getRawData<double>(H5name{"prob.ratio"})}
-{}
+        : ratios{stats.getRawData<double>(H5name{"prob.ratio"})},
+          cutoffs{}
+{
+    for(auto ratio : ratios){
+        auto factor = ratio+0.001;
+        factor = factor<1.0? 1.0: factor;
+        cutoffs.push_back(0.9*(1- 1/(factor)));
+    }
+}
 
 }//namespace wordrep
