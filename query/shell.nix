9e04f02a37d6321780a54669c3a3aa32bcc8f546
Ian-Woo Kim <ianwookim@gmail.com>
2017-01-05 23:01:08 +0000

    query/shell.nix update: query-binding autobuild when entering into the shell.

--- query/shell.nix
+++ query/shell.nix
@@ -6,8 +6,32 @@ with pkgs;
 
 let toolz_cpp = callPackage ../nix/default-cpp.nix {};
     hsconfig = import ../nix/haskell-modules/configuration-ghc-8.0.x.nix { inherit pkgs; };
+
+    queryBindingSrc = import ../query-binding {
+      inherit pkgs;
+      haskellPackages = haskellPackages.override { overrides = hsconfig; };
+    }; 
     hsconfig2= self: super: {
       query-common = self.callPackage ../query-common {};
+      "query-binding" = self.callPackage
+        ({ mkDerivation, base, fficxx, fficxx-runtime, stdenv
+         , template-haskell, rnnpp, json, hdf5-cpp, msgsl, tbb
+         }:
+         mkDerivation {
+           pname = "query-binding";
+           version = "0.0";
+           src = queryBindingSrc;
+           libraryHaskellDepends = [
+	     base fficxx fficxx-runtime template-haskell
+	   ];
+	   librarySystemDepends = [
+	     rnnpp json hdf5-cpp msgsl tbb
+	   ];
+	   license = stdenv.lib.licenses.bsd3;
+	 }) { inherit rnnpp tbb hdf5-cpp;
+              json  = toolz_cpp.json;
+              msgsl = toolz_cpp.msgsl;
+            };
     };
     
     newHaskellPackages = haskellPackages.override { overrides = self: super: hsconfig self super // hsconfig2 self super; };
@@ -23,6 +47,7 @@ let toolz_cpp = callPackage ../nix/default-cpp.nix {};
 	      network-simple
 	      fficxx-runtime
 	      fficxx
+              query-binding
             ]);
 in stdenv.mkDerivation {
   name = "query-dev";
