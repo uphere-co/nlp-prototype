42c3633f8ab2022540b8d6f343d2f8f0b00b7e95
Ian-Woo Kim <ianwookim@gmail.com>
2017-01-11 12:45:36 +0000

    confine_ygp_table_columns for RegisteredSentence. QueryText api is now called with optional table parameter.

--- query-common/lib/QueryServer/Type.hs
+++ query-common/lib/QueryServer/Type.hs
@@ -11,14 +11,14 @@ import qualified Data.Binary      as Bi
 import           Data.Hashable
 import           Data.Text              (Text)
 
-data Query = QueryText { query_text :: Text }
+data Query = QueryText { query_text :: Text, query_tables :: [Text] }
            | QueryRegister { query_register :: Text }
            --  | QueryById { query_ids :: [Int] }
            | QuerySuggest { query_ideas :: [Text] }
   deriving (Show,Eq,Ord)
 
 instance Bi.Binary Query where
-  put (QueryText     txt) = Bi.put (0 :: Int) >> Bi.put txt
+  put (QueryText txt tables) = Bi.put (0 :: Int) >> Bi.put txt >> Bi.put tables  
   put (QueryRegister txt) = Bi.put (1 :: Int) >> Bi.put txt
   -- put (QueryById     ids) = Bi.put (2 :: Int) >> Bi.put ids
   put (QuerySuggest txts) = Bi.put (3 :: Int) >> Bi.put txts
@@ -26,7 +26,7 @@ instance Bi.Binary Query where
   get = do
     t :: Int <- Bi.get
     case t of
-      0 -> QueryText     <$> Bi.get
+      0 -> QueryText <$> Bi.get <*> Bi.get
       1 -> QueryRegister <$> Bi.get
       -- 2 -> QueryById     <$> Bi.get
       3 -> QuerySuggest <$> Bi.get
@@ -35,20 +35,25 @@ instance Bi.Binary Query where
 
 data RegisteredSentences = RS { rs_sent_uids :: [ Int ]
                               , rs_Countries :: [ Text ]
+                              , rs_confine_ygp_table_columns :: [ Text ]
                               , rs_max_clip_len :: Maybe Int
                               }
   deriving (Show,Eq,Ord)
 
 instance FromJSON RegisteredSentences where
-  parseJSON (Object o) = do suids <- o .: "sent_uids"
-                            countries <- (o .: "Countries" <|> return [])
-                            ((o .: "max_clip_len" >>= \n -> return (RS suids countries (Just n)))
-                             <|> return (RS suids countries Nothing))  
+  parseJSON (Object o) = do
+    suids <- o .: "sent_uids"
+    countries <- (o .: "Countries" <|> return [])
+    tablecolumns <- (o .: "confine_ygp_table_columns" <|> return [])
+    ((o .: "max_clip_len" >>= \n -> return (RS suids countries tablecolumns (Just n)))
+     <|> return (RS suids countries tablecolumns Nothing))  
   parseJSON invalid    = typeMismatch "RegisteredSentences" invalid
 
 instance ToJSON RegisteredSentences where
   toJSON RS {..} = object $ [ "sent_uids" .= rs_sent_uids
-                            , "Countries" .= rs_Countries ]
+                            , "Countries" .= rs_Countries
+                            , "confine_ygp_table_columns" .= rs_confine_ygp_table_columns
+                            ]
                             ++ maybe [] (\x -> ["max_clip_len" .= x]) rs_max_clip_len
 
 
