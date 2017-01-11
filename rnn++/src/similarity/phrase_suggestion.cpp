9547e4de4919cdaf468a5fa18e3054d3ac3f2674
Jihun Kim <jihun@uphere.co>
2017-01-11 18:25:56 +0000

    Fix a loop in query suggestion.

--- rnn++/src/similarity/phrase_suggestion.cpp
+++ rnn++/src/similarity/phrase_suggestion.cpp
@@ -16,8 +16,11 @@ WordUsageInPhrase::usages(wordrep::WordUID word) const {
     auto filter_noisy_word = [this](auto uids){
         return util::filter(uids, [this](auto uid){return importance.score(uid)>0.5;});
     };
-    for(auto sent : sents){
-        if(!sent.isin(word)) continue;
+    auto n=sents.size();
+    tbb::parallel_for(decltype(n){0}, n, [&](auto i) {
+//    for(auto sent : sents){
+        auto sent = sents[i];
+        if(!sent.isin(word)) return;    
         auto phrases = phrase_segmenter.broke_into_phrases(sent, 5.0);
         for(auto phrase : phrases){
             if(phrase.size()>10 || phrase.size()==1) continue;
@@ -25,7 +28,7 @@ WordUsageInPhrase::usages(wordrep::WordUID word) const {
                 phrase_usages.push_back(Words{phrase.to_word_uids()});
             }
         }
-    }
+    });
     std::map<Words,int64_t> phrase_count;
     std::map<Words,std::map<Words,int64_t>> phrase_reprs;
     for(auto& words_in_phrase : phrase_usages){
