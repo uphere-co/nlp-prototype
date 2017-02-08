3fbc8eaefe078760d49a0ff1704037306a5bf2a3
Jihun Kim <jihun@uphere.co>
2017-02-08 05:08:14 +0000

    Extracts ID, type, label, and its aliases per Wikidata item.

--- rnn++/app/wikidata_etl.cpp
+++ rnn++/app/wikidata_etl.cpp
@@ -6,6 +6,9 @@
 #include "utils/string.h"
 #include "utils/json.h"
 
+using util::get_str;
+using util::find;
+
 void count(std::istream&& is){
     tbb::task_group g;
     while (auto buffer=util::string::read_chunk(is, 2000000)) {
@@ -18,7 +21,14 @@ void count(std::istream&& is){
                 if(line.back()!=',') continue;
                 line.back() = ' ';
                 auto elm = util::json_t::parse(line);
-                fmt::print("{}\n", util::get_str(elm,"id"));
+                std::stringstream ss;
+                fmt::print(ss, "{}\t{}", get_str(elm,"id"), get_str(elm,"type"));
+                auto maybe_label = util::find<std::string>(elm["labels"]["en"], "value");
+                if(maybe_label) {
+                    fmt::print(ss, "\t{}", maybe_label.value());
+                    for(auto& x : elm["aliases"]["en"]) fmt::print(ss, "\t{}", get_str(x,"value"));
+                }
+                fmt::print("{}\n", ss.str());
             }
         });
     }
