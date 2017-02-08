7d7a3c14ddb734de98f9bd0b5d1799b1195e2052
Ian-Woo Kim <ianwookim@gmail.com>
2017-02-08 15:40:22 +0000

    separate out WikiData.Type

--- wikidata/parser.hs
+++ wikidata/parser.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import           Control.Applicative              ((<|>))
 import           Control.Monad                    (guard,join,replicateM)
 import           Control.Monad.Trans.Either
 import           Control.Monad.Trans.State        (State,runState,evalState,execState)
@@ -19,79 +18,8 @@ import           Data.Text                        (Text)
 import qualified Data.Text                  as T
 import qualified Data.Text.Format           as TF
 import qualified Data.Text.Lazy.IO          as TLIO
-
-data LangValue = LV { lv_language :: Text
-                    , lv_value :: Text
-                    } deriving (Show, Eq)
-
-instance FromJSON LangValue where
-  parseJSON (Object o) = LV <$> (o .: "language")
-                            <*> (o .: "value")
-  parseJSON invalid = AT.typeMismatch "LangValue" invalid
-
-data Snak = Snak { snak_snaktype :: Text
-                 , snak_property :: Text
-                 , snak_datatype :: Maybe Text
-                 , snak_datavalue :: Maybe Value
-                 } deriving (Show, Eq)
-
-instance FromJSON Snak where
-  parseJSON (Object o) = Snak <$> (o .: "snaktype")
-                              <*> (o .: "property")
-                              <*> ((Just <$> (o .: "datatype")) <|> return Nothing)
-                              <*> ((Just <$> (o .: "datavalue")) <|> return Nothing)
-  parseJSON invalid = AT.typeMismatch "Snak" invalid
-
-data Reference = Ref { ref_hash :: Text
-                     , ref_snaks :: HM.HashMap Text [Snak]
-                     } deriving (Show, Eq)
-
-instance FromJSON Reference where
-  parseJSON (Object o) = Ref <$> (o .: "hash")
-                              <*> (o .: "snaks")
-  parseJSON invalid = AT.typeMismatch "Reference" invalid
-
-data Claim = Claim { claim_id :: Text
-                   , claim_mainsnak :: Snak
-                   , claim_type :: Text
-                   , claim_rank :: Text
-                   , claim_qualifiers :: Maybe [Snak]
-                   , claim_references :: Maybe [Reference]
-                   } deriving (Show, Eq)
-
-instance FromJSON Claim where
-  parseJSON (Object o) = Claim <$> (o .: "id")
-                               <*> (o .: "mainsnak")
-                               <*> (o .: "type")
-                               <*> (o .: "rank")
-                               <*> ((Just <$> (o .: "qualifiers")) <|> return Nothing)
-                               <*> ((Just <$> (o .: "references")) <|> return Nothing)
-  parseJSON invalid = AT.typeMismatch "Claim" invalid
-
-data TopLevel = TopLevel { toplevel_id :: Text
-                         , toplevel_type :: Text
-                         , toplevel_labels :: HM.HashMap Text LangValue
-                         , toplevel_descriptions :: HM.HashMap Text LangValue
-                         , toplevel_aliases :: HM.HashMap Text [LangValue]
-                         , toplevel_claims :: HM.HashMap Text [Claim]
-                         , toplevel_sitelinks :: Value
-                         , toplevel_lastrevid :: Maybe Int
-                         , toplevel_modified :: Maybe Text
-                         } deriving (Show,Eq)
-
-instance FromJSON TopLevel where
-  parseJSON (Object o) =
-    TopLevel <$> (o .: "id")
-             <*> (o .: "type")
-             <*> (o .: "labels") 
-             <*> (o .: "descriptions") 
-             <*> (o .: "aliases") 
-             <*> (o .: "claims")
-             <*> (o .: "sitelinks")
-             <*> ((Just <$> (o .: "lastrevid")) <|> return Nothing )
-             <*> ((Just <$> (o .: "modified")) <|> return Nothing)
-  parseJSON invalid = AT.typeMismatch "TopLevel" invalid
-   
+--
+import           WikiData.Type
 
 extractTopN :: Int -> EitherT String (State BL.ByteString) [TopLevel]
 extractTopN n = do
