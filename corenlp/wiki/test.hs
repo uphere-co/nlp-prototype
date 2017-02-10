2d60d9335f34edc7e41206e15e1f77e8624a1238
Jihun Kim <jihun@uphere.co>
2017-02-10 02:44:47 +0000

    Change string type to Data.Text for dealing UTF-8 characters.

--- corenlp/wiki/test.hs
+++ corenlp/wiki/test.hs
@@ -3,32 +3,37 @@ import           System.IO
 import qualified Data.HashMap.Strict as HM
 import           Text.Printf
 import           Data.Maybe (fromMaybe)
-import qualified Data.ByteString.Lazy.Char8 as BL
-import           Data.ByteString.Lex.Fractional 
+import           Data.Text                         (Text)
+import qualified Data.Text                  as T
+import           Data.Text.Read                    (rational)
+import qualified Data.Text.Encoding         as T.E
+import qualified Data.Text.IO               as T.IO
 import           Data.Monoid
+
 
 -- scoreFile = "/data/groups/uphere/ontology/wikidata/word_importance"
 scoreFile = "single"
 entityFile = "/data/groups/uphere/ontology/wikidata/wikidata.labels.single_word"
 
-parseScore :: [BL.ByteString] -> (BL.ByteString, Double)
-parseScore [x,y] = (x, fromMaybe 0.0 (fmap fst (readDecimal (BL.toStrict y)) ))
+extractDouble (Right (d, _)) = d
+
+parseScore :: [Text] -> (Text, Double)
+parseScore [x,y] = (x, extractDouble (rational y))
 parseScore line = error (show line)
 
 -- parseEntity (x:xs) = (x, head xs )
 parseEntity [x,y] = (x,y )
 parseEntity line = error (show line)
 
-readScores :: FilePath -> IO( HM.HashMap BL.ByteString Double )
+readScores :: FilePath -> IO( HM.HashMap Text Double )
 readScores scoreFile = do                  
-  scoreStr <- BL.readFile scoreFile
-  return $ HM.fromList $ (map parseScore . map BL.words . BL.lines) scoreStr
+  scoreStr <- T.IO.readFile scoreFile
+  return $ HM.fromList $ (map parseScore . map T.words . T.lines) scoreStr
 
 main = do
-  entityStr <- BL.readFile entityFile
+  entityStr <- T.IO.readFile entityFile
   scoring <- readScores scoreFile
-  -- let entities = (map parseEntity . map BL.words . BL.lines) entityStr
-  let entities = (map parseEntity . map (BL.split ' ') . BL.lines) entityStr
+  let entities = (map parseEntity . map T.words . T.lines) entityStr
       outputs =  map (\(uid, word) ->(uid, word, fromMaybe 0.0 (HM.lookup word scoring) ))  entities
       knownEntities = filter (\(_,_,score) -> score>0.0) outputs
-  mapM_ (\(uid,word,score) -> BL.putStrLn (uid <>" "<> word <>" "<> BL.pack (show score))) $ knownEntities
+  mapM_ (\(uid,word,score) -> T.IO.putStrLn (uid <>" "<> word <>" "<> T.pack (show score))) $ knownEntities
