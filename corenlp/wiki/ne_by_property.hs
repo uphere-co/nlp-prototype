b2a738e2a55778934bbd386a14f942505b20653a
Jihun Kim <jihun@uphere.co>
2017-02-14 07:54:49 +0000

    Algorithm to determine if some families of Wikidata entities are named entities.

---
+++ corenlp/wiki/ne_by_property.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+import           System.IO
+import qualified Data.HashMap.Strict as HM  (fromList, toList, lookup, map)
+import qualified Data.Map            as M   (Map, delete, alter, empty, fromList, toList, lookup, map)
+import           Text.Printf
+import           Data.Maybe (fromMaybe)
+import           Data.Text                         (Text)
+import qualified Data.Text                  as T
+import           Data.Text.Read                    (rational)
+import qualified Data.Text.Encoding         as T.E
+import qualified Data.Text.IO               as T.IO
+import           Data.Monoid
+import           Data.List                         (foldl')
+
+
+{--
+uidFile = "items.by_p31.test"
+neFile   = "is_ne.test"
+uidFile = "items.by_p31"
+neFile   = "uid.is_ne"
+--}
+uidFile = "items.by_p31"
+neFile   = "uid.is_ne"
+
+data IsNE = IsNE { unIsNE :: M.Map Text Text}
+          deriving (Show)
+
+parseIsNE [x,y] = (x, y)
+
+
+readIsNE :: FilePath -> IO IsNE 
+readIsNE isNEFile = do                  
+  neStr <- T.IO.readFile isNEFile
+  return $ IsNE (M.fromList $ map (parseIsNE . T.split (=='\t')) (T.lines neStr))
+
+isNE (IsNE neDict) uid = fromMaybe "Unknown" flag
+                       where flag = M.lookup uid neDict
+
+
+parseGroupedItems line = (tag, items)
+                       where 
+                         [tag, itemsStr] = T.split (=='\t') line
+                         items = T.words itemsStr
+
+
+flagCount flags = foldl' update M.empty flags
+                where 
+                  update acc flag = let f Nothing  = Just 1
+                                        f (Just n) = Just (n+1)
+                                    in M.alter f flag acc
+
+ratioCutoff :: M.Map Text Int -> Text
+ratioCutoff counts = f flag
+                   where 
+                     mTrue  = fromMaybe 0 $ M.lookup "True" counts
+                     mFalse = fromMaybe 0 $ M.lookup "False" counts
+                     flag = mTrue*5 > mFalse
+                     f True = "True"
+                     f False = "False"
+
+isNEProperty :: IsNE -> [Text] -> Text
+isNEProperty neDict items = f flag
+                          where
+                            flags = map (isNE neDict) items
+                            flag  = any (=="True") flags
+                            f True = "True"
+                            f False = "False"
+
+main = do
+  neDict <- readIsNE neFile
+  
+  {-
+  print (isNE neDict "Q180925798172598712") -- "Unknown"
+  print (isNE neDict "Q349") -- "False"
+  print (isNE neDict "Q354") -- "True"
+  -}
+
+  groupedItemsStr <- T.IO.readFile uidFile
+  let
+      groups = map parseGroupedItems (T.lines groupedItemsStr)
+      --ts = map (\(tag, items) -> tag <> "\t" <> isNEProperty neDict items) groups
+      tmps = map (\(tag, items) -> tag <> "\t" <> (ratioCutoff $ flagCount (map (isNE neDict) items))) groups
+  --mapM_ print tmps
+  mapM_ T.IO.putStrLn tmps
+
