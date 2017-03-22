deb6d4fbe13deeb41d399560bd838b550b3f8490
Ian-Woo Kim <ianwookim@gmail.com>
2017-03-22 18:15:35 +0000

    query: use optparse-applicative. query-daemon has now more descriptive command line message.

--- query/exe/query.hs
+++ query/exe/query.hs
@@ -15,6 +15,7 @@ import           Data.Text                                 (Text)
 import           Foreign.C.String
 import           Network.Transport.UpHere    (createTransport,defaultTCPParameters
                                              ,DualHostPortPair(..))
+import           Options.Applicative
 import           System.Environment
 import           System.IO                                 (hPutStrLn, stderr)
 --
@@ -24,18 +25,6 @@ import           QueryServer.Type
 import           CloudHaskell.Server
 import           Network.Util
 import           Worker
-
-withHeartBeat :: LogLock -> ProcessId -> Process ProcessId -> Process ()
-withHeartBeat lock them action = do
-  pid <- action                                            -- main process launch
-  whileJust_ (expectTimeout 10000000) $ \(HB n) -> do      -- heartbeating until it fails. 
-    atomicLog lock ("heartbeat: " ++ show n)
-    send them (HB n)
-      
-  atomicLog lock "heartbeat failed: reload"     -- when fail, it prints messages  
-  kill pid "connection closed"                             -- and start over the whole process.
-
-
 
 start :: EngineWrapper -> TMVar (HM.HashMap Text ([Int],[Text])) -> LogLock -> Process () 
 start engine resultref lock = do
@@ -49,15 +38,42 @@ start engine resultref lock = do
       (q,sc') <- receiveChan rc
       liftIO $ hPutStrLn stderr (show q)
       spawnLocal (queryWorker resultref sc' engine q)
- 
+
+
+data ServerOption = ServerOption { _port :: Int
+                                 , _hostg :: String
+                                 , _hostl :: String
+                                 , _config :: String
+                                 }
+
+pOptions :: Parser ServerOption
+pOptions = ServerOption <$> option auto (long "port" <> short 'p' <> help "Port number")
+                        <*> strOption (long "global-ip" <> short 'g' <> help "Global IP address")
+                        <*> strOption (long "local-ip"  <> short 'l' <> help "Local IP address")
+                        <*> strOption (long "config-file" <> short 'c' <> help "Config file")
+
+
+queryServerOption = info pOptions ( fullDesc <> progDesc "Query server daemon" <> header "options are port, global-ip, local-ip")
+
   
 main :: IO ()
 main = do
+  {- 
   port <- getEnv "PORT"
   let portnum :: Int = read port
       port' = show (portnum+1)
   [hostg,hostl,config] <- getArgs
-  let dhpp = DHPP (hostg,port') (hostl,port')
+  let dhpp = DHPP (hostg,port') (hostl,port') -}
+  opt <- execParser queryServerOption
+
+  let portnum = _port opt 
+      port = show portnum 
+      port' = show (portnum+1)
+      hostg = _hostg opt
+      hostl = _hostl opt
+      config = _config opt
+      dhpp = DHPP (hostg,port') (hostl,port')
+  
   etransport <- createTransport dhpp defaultTCPParameters
   case etransport of
     Left err -> hPutStrLn stderr (show err)
