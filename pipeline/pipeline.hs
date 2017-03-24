59aed1146c54ea0c7628238ae591b070830878e5
Doh Young Mo <modohyoung@gmail.com>
2017-03-24 07:12:08 +0000

    Log during processing is added.

--- pipeline/pipeline.hs
+++ pipeline/pipeline.hs
@@ -102,13 +102,13 @@ data ConfigRSS = ConfigRSS
 
 defYGP :: ConfigYGP
 defYGP = ConfigYGP { engine_type             = "ygp"
-                   , corenlp_client_script   = "../rnn++/scripts/corenlp.py"
-                   , word_uids_dump          = "/data/groups/uphere/similarity_test/all_words"
+                   , corenlp_client_script   = "../../rnn++/scripts/corenlp.py"
+                   , word_uids_dump          = "all_words" -- "/data/groups/uphere/similarity_test/all_words"
                    , pos_uids_dump           = "/data/groups/uphere/similarity_test/poss.uid"
                    , arclabel_uids_dump      = "/data/groups/uphere/similarity_test/dep.uid"
                    , column_uids_dump        = "/data/groups/uphere/similarity_test/column.uid"
                    , country_uids_dump       = "/data/groups/uphere/similarity_test/country.uid"
-                   , word_prob_dump          = "/data/groups/uphere/similarity_test/prob.h5"
+                   , word_prob_dump          = "prob.h5" -- "/data/groups/uphere/similarity_test/prob.h5"
                    , corenlp_dumps           = "ygp.corenlp" -- "/data/groups/uphere/similarity_test/ygp.corenlp"
                    , dep_parsed_store        = "ygp.h5" -- "/data/groups/uphere/similarity_test/ygp.h5"
                    , dep_parsed_prefix       = "ygp"
@@ -116,10 +116,10 @@ defYGP = ConfigYGP { engine_type             = "ygp"
                    , voca_name               = "news.en.uids"
                    , w2vmodel_name           = "news.en.vecs"
                    , w2v_float_t             = "float32"
-                   , wikidata_entities       = "/home/jihuni/word2vec/rss/wikidata.all_entities"
-                   , wikidata_uids           = "/home/jihuni/word2vec/rss/wikidata.uid"
-                   , wikidata_properties     = "/home/jihuni/word2vec/rss/wikidata.properties"
-                   , named_entity_uids       = "/home/jihuni/word2vec/rss/wikidata.uid.ne"
+                   , wikidata_entities       = "wikidata.all_entities" -- "/home/jihuni/word2vec/rss/wikidata.all_entities"
+                   , wikidata_uids           = "wikidata.uid" -- "/home/jihuni/word2vec/rss/wikidata.uid"
+                   , wikidata_properties     = "wikidata.properties" -- "/home/jihuni/word2vec/rss/wikidata.properties"
+                   , named_entity_uids       = "wikidata.uid.ne" -- "/home/jihuni/word2vec/rss/wikidata.uid.ne"
                    }
 
 defRSS :: ConfigRSS
@@ -163,29 +163,43 @@ instance FromJSON ConfigRSS where
 --
 
 main = do
+  -- Write JSON config files.
   BL.writeFile "config.ygp.json" (encode defYGP)
   BL.writeFile "config.rss.json" (encode defRSS)
-  
+
+  -- Build apps in rnn++.
   callProcess "cmake" ["../../rnn++"]
-  callProcess "make" ["-j20"]  
-  callCommand "./ygpdb_dump /data/groups/uphere/similarity_test/column.uid | java edu.stanford.nlp.process.PTBTokenizer -preserveLines > ygp.text.ptb"
-  callCommand "cat ygp.text.ptb | ./word_count | awk '{print $1}' >> all_words.duplicate"
-  callCommand "cat all_words.duplicate | ./word_count | awk '{print $1}' >> all_words"
-  callCommand "pigz -dc /opt/wikidata-20170206-all.json.gz | ./wikidata_etl config.ygp.json >wikidata.items"
-  callCommand "cat wikidata.items | awk -F $'\t' 'NF==5{print $3}' > wikidata.items.P31"
-  callCommand "cat wikidata.items | awk -F $'\t' 'NF==5{print $4}' > wikidata.items.P279"
-  callCommand "cat wikidata.items.P31 | tr ' ' '_' | ./word_count > wikidata.items.P31.count"
-  callCommand "cat wikidata.items.P279 | tr ' ' '_' | ./word_count > wikidata.items.P279.count"
-  callCommand "cat wikidata.items | awk 'BEGIN {FS=\"\t\"};NF==5{print $3 \"\t\" $1}'> items.uid"
-  callCommand "cat wikidata.items | awk 'BEGIN {FS=\"\t\"};{print \"WIKIDATAITEM_\" $1 \"\t\" $NF}' > wikidata.ner_input"
+  callProcess "make" ["-j20"]
+  putStrLn "rnn++ app build completed"
 
-  let corenlpEnv = "/data/groups/uphere/parsers/corenlp"
-  -- callCommand $ "java -mx48g edu.stanford.nlp.ie.NERClassifierCombiner -ner.model "++ corenlpEnv ++"/classifiers/english.all.3class.distsim.crf.ser.gz,"++ corenlpEnv ++"/classifiers/english.conll.4class.distsim.crf.ser.gz,"++ corenlpEnv ++"/classifiers/english.muc.7class.distsim.crf.ser.gz -textFile wikidata.ner_input > wikidata.ner" -- very time-consuming
-
+  -- Build apps in corenlp.
   callCommand "ghc -o count ../../corenlp/wiki/count.hs ../../corenlp/wiki/wikidata.hs"
   callCommand "ghc -o corenlp_ner ../../corenlp/wiki/corenlp_ner.hs ../../corenlp/wiki/corenlp.hs ../../corenlp/wiki/wikidata.hs"
   callCommand "ghc -o ne_by_property ../../corenlp/wiki/ne_by_property.hs ../../corenlp/wiki/wikidata.hs"
   callCommand "ghc -o wikidata_ner ../../corenlp/wiki/wikidata_ner.hs ../../corenlp/wiki/wikidata.hs"
+  putStrLn "corenlp app build completed"
+
+  -- Dump the YGP DB, and produce all_words
+  callCommand "./ygpdb_dump /data/groups/uphere/similarity_test/column.uid | java edu.stanford.nlp.process.PTBTokenizer -preserveLines > ygp.text.ptb"
+  callCommand "cat ygp.text.ptb | ./word_count | awk '{print $1}' >> all_words.duplicate"
+  callCommand "cat all_words.duplicate | ./word_count | awk '{print $1}' >> all_words"
+  putStrLn "all_words produced"
+
+  -- From Wikidata JSON dump, produces wikidata.items which is the first source of all wikidata data
+  callCommand "pigz -dc /opt/wikidata-20170206-all.json.gz | ./wikidata_etl config.ygp.json >wikidata.items"
+  putStrLn "wikidata.items produced"
+
+  callCommand "cat wikidata.items | awk -F $'\t' 'NF==5{print $3}' > wikidata.items.P31"
+  -- callCommand "cat wikidata.items | awk -F $'\t' 'NF==5{print $4}' > wikidata.items.P279" -- Not used for now
+  callCommand "cat wikidata.items.P31 | tr ' ' '_' | ./word_count > wikidata.items.P31.count"
+  -- callCommand "cat wikidata.items.P279 | tr ' ' '_' | ./word_count > wikidata.items.P279.count" -- Not used for now
+  callCommand "cat wikidata.items | awk 'BEGIN {FS=\"\t\"};NF==5{print $3 \"\t\" $1}'> items.uid"
+  callCommand "cat wikidata.items | awk 'BEGIN {FS=\"\t\"};{print \"WIKIDATAITEM_\" $1 \"\t\" $NF}' > wikidata.ner_input"
+  putStrLn "wikidata analysis completed"
+
+  let corenlpEnv = "/data/groups/uphere/parsers/corenlp"
+  callCommand $ "java -mx48g edu.stanford.nlp.ie.NERClassifierCombiner -ner.model "++ corenlpEnv ++"/classifiers/english.all.3class.distsim.crf.ser.gz,"++ corenlpEnv ++"/classifiers/english.conll.4class.distsim.crf.ser.gz,"++ corenlpEnv ++"/classifiers/english.muc.7class.distsim.crf.ser.gz -textFile wikidata.ner_input > wikidata.ner" -- very time-consuming
+  putStrLn "corenlp parsing for wikidata completed"
 
   callCommand "./corenlp_ner  > wikidata.is_sfne"
   callCommand "cat wikidata.is_sfne | awk 'BEGIN {FS=\"\t\"};{print $1 \"\t\" $NF}' > wikidata.uid.is_sfne"
@@ -194,22 +208,26 @@ main = do
   callCommand "./wikidata_ner > wikidata.nes"
   -- callCommand "./count > items.by_p31"
 
+  putStrLn "wikidata NER completed"
+
   callCommand "cat wikidata.nes | awk -F '\t' '$2==\"True\"{print $1}' > wikidata.uid.ne"
   callCommand "cat wikidata.items | awk -F '\t' 'NF==5{print $1}' > wikidata.uid"
   callCommand "cat wikidata.items | awk -F '\t' '{print $1 \"\t\" $NF}' > wikidata.all_entities"
   callCommand "cat wikidata.items | awk -F '\t' 'NF==5{print $1 \"\t\" $3}' > wikidata.properties"
 
-  -- callCommand "find /opt/YGP.text/ -type f | cat -n | xargs -P 20 -i'{}' python ../../rss_crawler/parse_article.py YGP {} /opt/RSS.text/"
+  putStrLn "wikidata items produced"
+
+
   createDirectoryIfMissing True "YGP.json"
   -- callCommand "find /opt/YGP.text -type f | xargs -P20 -I {} python ../../rnn++/scripts/corenlp.py {} YGP.json/" -- time-consuming
-  callCommand "find YGP.json/  -name '*.*.*.*' > ygp.corenlp"
+  putStrLn "corenlp parsing for YGP completed"
+
+  -- callCommand "find YGP.json/  -name '*.*.*.*' > ygp.corenlp" -- This is original command
+  callCommand "find /opt/YGP.json/  -name '*.*.*.*' > ygp.corenlp"
   let minorVersion = ("0" :: String)
   callCommand $ "make -j20 && time ./ygpdb_etl config.ygp.json ygp.corenlp " ++ minorVersion
+  putStrLn "indexing YGP completed"
 
-
-  -- callCommand "./word_importance_build"
-  
-  -- callCommand "find YGP.json/ -type f > ygp_jsons" -- for RSS
-  -- callCommand "./ygp_dump config.ygp.json ygp_jsons 1" -- for RSS
+  callCommand "./word_importance_build"
 
   putStrLn "Pipeline finished!"
