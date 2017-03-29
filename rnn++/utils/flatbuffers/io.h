c5f75a3eeae6068b7de1d08fbc84ed3330335548
Jihun Kim <jihun@uphere.co>
2017-03-29 08:43:02 +0000

    Refactoring on binary file I/O module of Pairs.

--- rnn++/utils/flatbuffers/io.h
+++ rnn++/utils/flatbuffers/io.h
@@ -24,19 +24,24 @@ void to_file(std::vector<Pair> const& vals, PairsBinary file);
 std::unique_ptr<char[]> load_binary_file(PairsBinary file);
 
 template<typename T>
-tbb::concurrent_vector<T> deserialize_pairs(std::unique_ptr<char[]> data){
+void deserialize_pairs(std::unique_ptr<char[]> data, tbb::concurrent_vector<T>& pairs){
     namespace fb = util::io::fb;
     auto rbuf = fb::GetPairs(data.get());
     auto& properties_buf = *rbuf->vals();
     auto n = properties_buf.size();
-    tbb::concurrent_vector<T> properties;
-    properties.resize(n,{-1,-1});
 
-    tbb::parallel_for(decltype(n){0},n, [&properties_buf,&properties](auto i) {
+    pairs.resize(n,{-1,-1});
+    tbb::parallel_for(decltype(n){0},n, [&properties_buf,&pairs](auto i) {
         auto it=properties_buf[i];
-        properties[i]={it->key(), it->value()};
+        pairs[i]={it->key(), it->value()};
     });
-    return properties;
+}
+
+template<typename T>
+auto deserialize_pairs(std::unique_ptr<char[]> data){
+    auto pairs = std::make_unique<tbb::concurrent_vector<T>>();
+    deserialize_pairs(std::move(data), *pairs);
+    return pairs;
 }
 
 }//namespace util::io::fb
