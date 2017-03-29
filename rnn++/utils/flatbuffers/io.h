4463719a56bb2cc61d4d0f56cf0b9c809a266a54
Jihun Kim <jihun@uphere.co>
2017-03-29 06:00:35 +0000

    FlatBuffer scheme for pair values.

---
+++ rnn++/utils/flatbuffers/io.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "utils/flatbuffers/pairs_generated.h"
+
+namespace util {
+namespace io {
+namespace fb {
+
+inline bool operator<(Pair const& x, Pair const& y) {
+    return x.key() < y.key();
+}
+inline bool operator==(Pair const& x, Pair const& y) {
+    return x.key() == y.key();
+}
+
+}//namespace util::io::fb
+}//namespace util::io
+}//namesapce util
