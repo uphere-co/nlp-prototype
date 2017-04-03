aea249d1d57b84d475fea78582b819738c98b1db
Doh Young Mo <modohyoung@gmail.com>
2017-04-03 02:23:04 +0000

    optparse-applicative is used to parse options.

--- pipeline/Pipeline.hs
+++ pipeline/Pipeline.hs
@@ -6,9 +6,23 @@ module Main where
 --
 import           Type
 import           Run
+import           System.Directory       (createDirectoryIfMissing)
+import           Options.Applicative
+
+data PipelineOption = PipelineOption { _minorVersion :: Int
+                                     , _output       :: String
+                                     }
+
+pOptions :: Parser PipelineOption
+pOptions = PipelineOption <$> option auto (long "minor version" <> short 'v' <> help "Minor version")
+           <*> strOption (long "output path" <> short 'o' <> help "Output path")
+
+pipelineOption = info pOptions ( fullDesc <> progDesc "Pipeline App" <> header "options are minor-version, output path")
 
 main :: IO ()
 main = do
+  opt <- execParser pipelineOption    
+  
   writeJSON YGP
   writeJSON RSS
   mkRnnApp
