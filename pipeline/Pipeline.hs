e3a386a7b0b51c8340d8cb540662e7cf433ad9d1
Doh Young Mo <modohyoung@gmail.com>
2017-04-03 05:18:12 +0000

    Working directory can be set.

--- pipeline/Pipeline.hs
+++ pipeline/Pipeline.hs
@@ -6,28 +6,35 @@ module Main where
 --
 import           Type
 import           Run
-import           System.Directory       (createDirectoryIfMissing)
+import           System.Directory       (createDirectoryIfMissing,setCurrentDirectory)
 import           Options.Applicative
 
-data PipelineOption = PipelineOption { _minorVersion :: Int
-                                     , _output       :: String
+data PipelineOption = PipelineOption { _workingDirectory :: String
+                                     , _minorYGPVersion  :: Int
+                                     , _minorRSSVersion  :: Int
                                      }
 
 pOptions :: Parser PipelineOption
-pOptions = PipelineOption <$> option auto (long "minor version" <> short 'v' <> help "Minor version")
-           <*> strOption (long "output path" <> short 'o' <> help "Output path")
+pOptions = PipelineOption <$> strOption (long "working-directory" <> short 'w' <> help "Working directory")
+           <*> option auto (long "minor-YGP-version" <> short 'y' <> help "YGP minor version")
+           <*> option auto (long "minor-RSS-version" <> short 'r' <> help "RSS minor version")
 
-pipelineOption = info pOptions ( fullDesc <> progDesc "Pipeline App" <> header "options are minor-version, output path")
+pipelineOption = info pOptions ( fullDesc <> progDesc "Pipeline App" <> header "options are working-directory, minor-YGP-version, minor-RSS-version.")
 
 main :: IO ()
 main = do
-  opt <- execParser pipelineOption    
-  
+  opt <- execParser pipelineOption
+  let wd = _workingDirectory opt
+      yv = _minorYGPVersion opt
+      rv = _minorRSSVersion opt
+
+  setCurrentDirectory wd
+
   writeJSON YGP
   writeJSON RSS
   mkRnnApp
   mkCoreNLPApp
   doWikidataAnalysis
-  runYGP
-  runRSS
+  runYGP yv
+  runRSS rv
   calculateWI
