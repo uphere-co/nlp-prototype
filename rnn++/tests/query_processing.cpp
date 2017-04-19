8ad19844bc77193eddd23c24bcdff99dab2406ed
Jihun Kim <jihun@uphere.co>
2017-04-19 05:48:14 +0000

    Rename variables.

--- rnn++/tests/query_processing.cpp
+++ rnn++/tests/query_processing.cpp
@@ -144,14 +144,14 @@ private:
     tbb::concurrent_vector<value_type> sorted_words;
 };
 
-struct MatchedIndexPerSent{
+struct MatchedEntityPerSent{
     using Key = wordrep::SentUID;
     using Value = LookupEntityCandidate::Index;
 
-    friend bool operator==(MatchedIndexPerSent const& x, MatchedIndexPerSent const& y){
+    friend bool operator==(MatchedEntityPerSent const& x, MatchedEntityPerSent const& y){
         return x.key==y.key;
     }
-    friend bool operator<(MatchedIndexPerSent const& x, MatchedIndexPerSent const& y){
+    friend bool operator<(MatchedEntityPerSent const& x, MatchedEntityPerSent const& y){
         return x.key<y.key;
     }
 
@@ -245,7 +245,7 @@ private:
 };
 
 
-auto scoring_dep_word(LookupEntityCandidate const& candidates, MatchedIndexPerSent i){
+auto scoring_dep_word(LookupEntityCandidate const& candidates, MatchedEntityPerSent i){
     return candidates.score(i.val);
 }
 
@@ -370,7 +370,7 @@ int query_sent_processing(int argc, char** argv) {
 
     auto pre_results_per_entity = map(preprocessed_sent.entities, [&](auto& e) {
         return concat_mapmap(candidates.find(e.uid), [&](auto i) {
-            return MatchedIndexPerSent{texts->sent_uid(candidates.token_index(i)), i};
+            return MatchedEntityPerSent{texts->sent_uid(candidates.token_index(i)), i};
         });
     });
     assert(pre_results_per_entity.size()==preprocessed_sent.entities.size());
@@ -378,10 +378,9 @@ int query_sent_processing(int argc, char** argv) {
     auto keys_per_ambiguous_entity=util::mapmap(pre_results_per_entity, [](auto x){return x.key;});
     timer.here_then_reset("Map phase for Wiki entities : get keys.");
     auto common_keys = util::intersection(keys_per_ambiguous_entity);
-    util::sort(common_keys);
     timer.here_then_reset("Map phase for Wiki entities : get intersection of keys.");
 
-
+    util::sort(common_keys);
     for(auto& matched_indexes : pre_results_per_entity) {
         auto last = std::partition(matched_indexes.begin(), matched_indexes.end(), [&](auto &token) {
             if (util::binary_find(common_keys, token.key)) return true;
@@ -397,22 +396,15 @@ int query_sent_processing(int argc, char** argv) {
     std::vector<std::vector<MatchedTokenPerSent>> matched_tokens_per_entity;
     auto n = pre_results_per_entity.size();
     for(decltype(n)i=0; i!=n; ++i){
-        auto& e = preprocessed_sent.entities.at(i);
-        auto& matched_indexes = pre_results_per_entity.at(i);
-        auto last = std::partition(matched_indexes.begin(), matched_indexes.end(), [&](auto& token) {
-            if(util::binary_find(common_keys, token.key)) return true;
-            return false;
-        });
-        matched_indexes.erase(last, matched_indexes.end());
-        timer.here_then_reset("Map phase for Wiki entities : collect relevant tokens.");
+        auto& entity = preprocessed_sent.entities.at(i);
+        auto& entity_name = entity.idxs;
+        auto op_gov_similarity = get_op_word_sim(entity.word_gov);
 
-        auto op_gov_similarity = get_op_word_sim(e.word_gov);
-
-        auto matched_tokens = map(matched_indexes, [&](auto i){
+        auto& entity_occurences = pre_results_per_entity.at(i);
+        auto matched_tokens = map(entity_occurences, [&](auto i){
             auto idx = candidates.token_index(i.val);
             //texts, e, op_gov_word_similarity, i
             auto key = texts->sent_uid(idx);
-            auto& query_words = e.idxs;
             auto m_words = ner_tagged_tokens.find(idx);
             assert(m_words);
             auto entity_words = m_words.value();
@@ -420,7 +412,7 @@ int query_sent_processing(int argc, char** argv) {
             auto score_gov = op_gov_similarity.gov_scoring(data_word_gov);
             auto score_dep = candidates.score(i.val);
             auto match_score = score_dep * score_gov;
-            return MatchedTokenPerSent{key, {query_words, entity_words, match_score}};
+            return MatchedTokenPerSent{key, {entity_name, entity_words, match_score}};
         });
         timer.here_then_reset("Map phase for Wiki entities : get matched_tokens.");
         util::drop_duplicates(matched_tokens);
@@ -443,8 +435,8 @@ int query_sent_processing(int argc, char** argv) {
         for(auto simword_idx : similar_words){
             auto similar_word    = word_sim->sim_word(simword_idx);
             auto score_dep = op_dep_similarity.dep_scoring(similar_word);
-            auto matched_idxs   = words->find(similar_word);
-            for(auto idx : matched_idxs){
+            auto word_occurences   = words->find(similar_word);
+            for(auto idx : word_occurences){
                 auto token_idx = words->token_index(idx);
                 auto key = texts->sent_uid(token_idx);
                 if(!util::binary_find(common_keys, key)) continue;
