9a94e8cf04893cd3c99adfe4df96a148908de669
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-25 19:03:24 +0000

    create WikidataUIDindex and EntityReprs to finally show AnnotatedSentence. yet segfault.

--- rnn++/app/token_tagger_app.cpp
+++ rnn++/app/token_tagger_app.cpp
@@ -15,18 +15,43 @@ wordrep::wiki::SortedEntities create_entities(wordrep::WordUIDindex const& wordU
     return {std::move(items)};
 }
 
+ 
+// wordrep::wiki::wikidataUIDindex
+wordrep::WikidataUIDindex create_WikidataUIDindex(std::string const& filename) {
+    tbb::concurrent_vector<std::pair<wordrep::WikidataUID,std::string>> items;
+    
+    auto lines = util::string::readlines(filename);
+    for(auto& line : lines) {
+        auto tokens = util::string::split(line, "\t");
+        auto uid = wordrep::WikidataUIDindex::get_uid(tokens[0]);
+        items.push_back(std::make_pair(uid,tokens[1]));
+    }
+    return {std::move(items)};
+
+    
+    //return std::move(wikidataUIDs);   
+}
+
 
 int main( int argc, char** argv )
 {
     std::cout << "start token_tagger_app" << std::endl;
     util::json_t p = util::load_json("../tests/data/sentence.1.corenlp");
-    wordrep::WordUIDindex wordUIDs("/data/groups/uphere/engine.rss/words.uid" ) ;
+    wordrep::WordUIDindex wordUIDs{"/data/groups/uphere/engine.rss/words.uid"};
     wordrep::wiki::SortedEntities entities = create_entities(wordUIDs, "F7745.all_entities");
+    //wordrep::wiki::UIDSortedEntities uid_sorted_ett { entitie} ;
+    wordrep::wiki::EntityReprs entity_reprs{entities.to_uid_sorted()};
+    wordrep::WikidataUIDindex wikidataUIDs = create_WikidataUIDindex("F7745.all_entities");   
     wikidata::GreedyAnnotator ann(entities);
 
+    // int s = 0; 
+     for(auto e = entities.cbegin(); e != entities.cend() ; e++ ) {
+        std::cout << e->repr(wikidataUIDs,wordUIDs) << std::endl;
+    }
     
     std::cout << p.dump(4) << std::endl;
-    std::cout << wordrep::WordUIDindex::get_uid("Earlier") << std::endl;
+    // std::cout << wordrep::WordUIDindex::get_uid("Earlier") << std::endl;
     wordrep::AnnotatedSentence asent = tagger::test_program(ann,p);
+    std::cout << asent.repr(entity_reprs,wikidataUIDs,wordUIDs) << std::endl;
     // asent( entity_reprs, , widx)
 }
