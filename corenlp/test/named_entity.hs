a95e9d482d9b76e7e8794548007a3b330f213899
Jihun Kim <jihun@uphere.co>
2017-05-10 05:35:05 +0000

    Cleanup logic to filter out non-named entity tokens.

--- corenlp/test/named_entity.hs
+++ corenlp/test/named_entity.hs
@@ -11,7 +11,7 @@ parseStanfordNE (C.EntityToken (C.WordToken word) (C.NETag tag)) =  N.parseStr w
 
 main = do
   let 
-    test_str = "United/ORGANIZATION Airlines/ORGANIZATION -LRB-/O UAL.N/O -RRB-/O and/O its/O chief/O executive/O faced/O mounting/O pressure/O on/O Tuesday/DATE from/O a/O worldwide/O backlash/O over/O its/O treatment/O of/O a/O passenger/O who/O was/O dragged/O from/O his/O seat/O on/O a/O plane/O on/O Sunday/DATE to/O make/O room/O for/O four/O employees/O on/O the/O overbooked/O flight/O ./O \n\
+    corenlp_output = "United/ORGANIZATION Airlines/ORGANIZATION -LRB-/O UAL.N/O -RRB-/O and/O its/O chief/O executive/O faced/O mounting/O pressure/O on/O Tuesday/DATE from/O a/O worldwide/O backlash/O over/O its/O treatment/O of/O a/O passenger/O who/O was/O dragged/O from/O his/O seat/O on/O a/O plane/O on/O Sunday/DATE to/O make/O room/O for/O four/O employees/O on/O the/O overbooked/O flight/O ./O \n\
 \Lawyers/O for/O the/O passenger/O ,/O Dr./O David/PERSON Dao/PERSON ,/O issued/O a/O statement/O late/O on/O Tuesday/DATE confirming/O his/O identity/O and/O saying/O that/O he/O and/O his/O family/O were/O ``/O focused/O only/O on/O Dr./O Dao/PERSON 's/O medical/O care/O and/O treatment/O ''/O in/O a/O Chicago/LOCATION hospital/O ./O \n\
 \The/O U.S./ORGANIZATION Department/ORGANIZATION of/ORGANIZATION Transportation/ORGANIZATION launched/O an/O inquiry/O into/O the/O incident/O ,/O and/O New/LOCATION Jersey/LOCATION Governor/O Chris/PERSON Christie/PERSON called/O for/O new/O rules/O to/O curb/O the/O airline/O practice/O of/O overbooking/O flights/O ./O \n\
 \United/ORGANIZATION CEO/O Oscar/PERSON Munoz/PERSON issued/O a/O statement/O on/O Tuesday/DATE apologizing/O to/O Dao/PERSON without/O naming/O him/O ./O \n\
@@ -22,12 +22,12 @@ main = do
 \On/O Monday/DATE ,/O Munoz/PERSON issued/O a/O memo/O to/O employees/O defending/O the/O company/O but/O not/O apologizing/O to/O the/O passenger/O ./O \n\
 \Munoz/PERSON ,/O a/O former/O railroad/O executive/O who/O took/O over/O the/O helm/O at/O United/ORGANIZATION in/O 2015/DATE ,/O had/O already/O been/O under/O pressure/O from/O activist/O investors/O to/O improve/O the/O airline/O 's/O performance/O ,/O including/O its/O customer/O relations/O ./O \n\
 \"
-    tokens   = map parseStanfordNE (C.parseNEROutputStr test_str)
+    tokens   = map parseStanfordNE (C.parseNEROutputStr corenlp_output)
     oscar    = parseStanfordNE (C.parseNERToken "Oscar/PERSON")
     munoz    = N.parseStr "Munoz" "PERSON"
     united   = N.parseStr "United" "ORGANIZATION"
     airlines = N.parseStr "Airlines" "ORGANIZATION"
-  T.IO.putStrLn test_str
+  T.IO.putStrLn corenlp_output
   print tokens
   print oscar
   print munoz
@@ -37,5 +37,5 @@ main = do
   let entities = [united, airlines, oscar, munoz, united, munoz]
   print entities
   print (N.mergeTokens entities)
-  print (filter (\x -> N._type x /= N.Other) (N.mergeTokens tokens))
+  print (N.mergeTokens tokens)
   --print (N.collapseToken united munoz)
--- corenlp/wiki/NamedEntity.hs
+++ corenlp/wiki/NamedEntity.hs
@@ -13,28 +13,40 @@ data NamedEntity = NamedEntity { _str  :: Text
                                , _type :: NamedEntityClass}
                  deriving(Show, Eq)
 
-parseStr :: Text -> Text -> NamedEntity
-parseStr str t | t== "PERSON"   = NamedEntity str Person
-               | t== "ORGANIZATION"      = NamedEntity str Org
-               | t== "LOCATION" = NamedEntity str Loc
-               | t== "TIME"     = NamedEntity str Time
-               | t== "DATE"     = NamedEntity str Date
-               | t== "O"        = NamedEntity str Other
+data NamedEntityFrag = NamedEntityFrag { _fstr  :: Text
+                                       , _ftype :: NamedEntityClass}
+                     | None
+                     deriving(Show, Eq)
+
+isSameTYpe :: NamedEntityFrag -> NamedEntityFrag -> Bool
+isSameTYpe None _ = False
+isSameTYpe _ None = False
+isSameTYpe frag1 frag2 = _ftype frag1 == _ftype frag2
+
+parseStr :: Text -> Text -> NamedEntityFrag
+parseStr str t | t== "PERSON"      = NamedEntityFrag str Person
+               | t== "ORGANIZATION"= NamedEntityFrag str Org
+               | t== "LOCATION"    = NamedEntityFrag str Loc
+               | t== "TIME"        = NamedEntityFrag str Time
+               | t== "DATE"        = NamedEntityFrag str Date
+               | t== "O"           = None
 parseStr _ _  = error "Unknown named entity class"
 
-
-mergeToken :: NamedEntity -> NamedEntity -> NamedEntity
-mergeToken (NamedEntity entity1 tag1) (NamedEntity entity2 tag2) | tag1 == tag2 = NamedEntity (T.unwords [entity1, entity2]) tag1
+mergeToken :: NamedEntityFrag -> NamedEntityFrag -> NamedEntityFrag
+mergeToken (NamedEntityFrag entity1 tag1) (NamedEntityFrag entity2 tag2)
+  | tag1 == tag2 = NamedEntityFrag (T.unwords [entity1, entity2]) tag1
 mergeToken _ _ = error "Cannot collapse entities with different types"
 
-mergeTokensImpl :: NamedEntity -> [NamedEntity]
-                -> ([NamedEntity] -> [NamedEntity])
-                -> ([NamedEntity] -> [NamedEntity])
+mergeTokensImpl :: NamedEntityFrag -> [NamedEntityFrag]
+                -> ([NamedEntityFrag] -> [NamedEntityFrag])
+                -> ([NamedEntityFrag] -> [NamedEntityFrag])
+mergeTokensImpl None   [] outputBuilder = outputBuilder 
 mergeTokensImpl entity [] outputBuilder = outputBuilder <> (entity:)
+mergeTokensImpl None (next : unresolvedEntities) outputBuilder = mergeTokensImpl next unresolvedEntities outputBuilder
 mergeTokensImpl current (next : unresolvedEntities) outputBuilder
-    | _type current == _type next = mergeTokensImpl (mergeToken current next) unresolvedEntities outputBuilder
-    | otherwise                   = mergeTokensImpl next unresolvedEntities (outputBuilder <> (current:) )
+    | isSameTYpe current next = mergeTokensImpl (mergeToken current next) unresolvedEntities outputBuilder
+    | otherwise               = mergeTokensImpl next unresolvedEntities (outputBuilder <> (current:) )
 
-mergeTokens :: [NamedEntity] -> [NamedEntity]
+mergeTokens :: [NamedEntityFrag] -> [NamedEntityFrag]
 mergeTokens []     = []
 mergeTokens (e:es) = mergeTokensImpl e es id []
