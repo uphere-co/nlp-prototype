0aed42eb920473594c9e4841bf06f66f2eec143a
Jihun Kim <jihun@uphere.co>
2017-05-12 07:02:08 +0000

    Script for querying Wikidata to get CSV files.

---
+++ corenlp/scripts/wikidata_queries.sh
@@ -0,0 +1,33 @@
+#List persons work for company
+curl -H "Accept: text/csv" -G https://query.wikidata.org/sparql --data-urlencode query='
+SELECT DISTINCT ?person ?personLabel ?company ?companyLabel WHERE {
+   ?person  wdt:P108 ?company .
+   ?company wdt:P31/wdt:P279* wd:Q4830453 .
+   SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "en" .
+   }
+ }
+' > employees.csv
+
+#List persons related to public companies (e.g. founder, CEO, board members, and so on)
+curl -H "Accept: text/csv" -G https://query.wikidata.org/sparql --data-urlencode query='
+SELECT DISTINCT ?company ?companyLabel ?person ?personLabel WHERE {
+   ?company wdt:P414 ?excahge .
+   ?company p:P1789|p:P3320|p:P1037|p:P169|p:P127|p:P112|p:P488 ?person_statement .
+   ?person_statement ?property ?person .
+   ?person wdt:P31 wd:Q5 .
+   SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "en" .
+   }
+ }
+' > businesspersons.csv
+
+#List public companies
+curl -H "Accept: text/csv" -G https://query.wikidata.org/sparql --data-urlencode query='
+SELECT ?company ?companyLabel ?excahge ?excahgeLabel WHERE {
+   ?company wdt:P414 ?excahge .
+   SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "en" .
+   }
+ }
+' > public_companies.csv
