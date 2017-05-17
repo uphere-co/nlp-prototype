a6eb8aa3541e16deb7b33d7ae6fce1f95eb0cc63
Jihun Kim <jihun@uphere.co>
2017-05-17 11:57:53 +0000

    Simplify assert statements.

--- corenlp/test/Assert.hs
+++ corenlp/test/Assert.hs
@@ -1,5 +1,12 @@
 module Assert where
+
+import           Control.Monad.Primitive               (PrimMonad)
     
 assert :: Bool -> IO ()
 assert True = return ()
 assert False = error "Assertion error"
+
+--assertEqual :: (PrimMonad m) => m (a) -> a -> IO()
+assertEqual expr val = do
+  r <- expr
+  assert (r == val)
