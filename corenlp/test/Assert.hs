f6c911cfdedf24a7e8fb068cc930d4ede91272ab
Jihun Kim <jihun@uphere.co>
2017-05-17 12:10:42 +0000

    Improve type signatures of assert routines.

--- corenlp/test/Assert.hs
+++ corenlp/test/Assert.hs
@@ -1,12 +1,15 @@
 module Assert where
 
 import           Control.Monad.Primitive               (PrimMonad)
+
+foo :: IO Bool
+foo  = return True
     
-assert :: Bool -> IO ()
+assert :: (PrimMonad m) => Bool -> m ()
 assert True = return ()
 assert False = error "Assertion error"
 
---assertEqual :: (PrimMonad m) => m (a) -> a -> IO()
+assertEqual :: (PrimMonad m, Eq a) => m a -> a -> m ()
 assertEqual expr val = do
   r <- expr
   assert (r == val)
