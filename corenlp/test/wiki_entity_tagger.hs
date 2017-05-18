509c6a4aa5f681d0a555a3e145345f5f960b43f7
Jihun Kim <jihun@uphere.co>
2017-05-18 02:15:11 +0000

    Re-implement assertEqual, called massertEqual, using HUnit.

--- corenlp/test/wiki_entity_tagger.hs
+++ corenlp/test/wiki_entity_tagger.hs
@@ -8,7 +8,8 @@ import           Control.Monad.Primitive               (PrimMonad, PrimState)
 import           Data.Vector.Generic.Mutable.Base      (MVector)
 import           Data.Vector                           (Vector)
 import           Data.Ord                              (Ord)
-import           Assert                                (assertEqual)
+import           Assert                                (massertEqual)
+import           Test.HUnit                            (assertBool)
 import qualified Data.Text                    as T
 import qualified Data.Text.IO                 as T.IO
 import qualified Data.Vector.Generic.Mutable  as MV
@@ -19,7 +20,6 @@ import qualified Data.Vector.Unboxed           as V
 -}
 import qualified Data.Vector.Algorithms.Intro  as VA
 import qualified Data.Vector.Algorithms.Search as VS
-import           Test.HUnit          (assertBool)
 
 
 itemTuple :: [Text] -> (Text,[Text])
@@ -96,21 +96,21 @@ testBinarySearch = do
   
   tt <- V.thaw wordss
   VA.sort tt  
-  assertEqual (V.freeze tt) (V.fromList wordssSorted)
+  massertEqual (V.freeze tt) (V.fromList wordssSorted)
   
-  assertEqual (binarySearchLR tt ["B"]) (3,6)
-  assertEqual (binarySearchLR tt ["C"]) (9,11)  
-  assertEqual (binarySearchLR tt ["D"]) (12,12)
-  assertEqual (binarySearchLRBy (ithElementOrdering 0) tt ["D"]) (12,12)
+  massertEqual (binarySearchLR tt ["B"]) (13,6)
+  massertEqual (binarySearchLR tt ["C"]) (9,11)  
+  massertEqual (binarySearchLR tt ["D"]) (12,12)
+  massertEqual (binarySearchLRBy (ithElementOrdering 0) tt ["D"]) (12,12)
   
   (bidxBL0, bidxBR0) <- binarySearchLRBy (ithElementOrdering 0) tt ["B", "C"]
   assertBool "" ((bidxBL0, bidxBR0)==(3,9))
-  assertEqual (binarySearchLRByBounds (ithElementOrdering 1) tt ["B", "C"] bidxBL0 bidxBR0) (7,9)
+  massertEqual (binarySearchLRByBounds (ithElementOrdering 1) tt ["B", "C"] bidxBL0 bidxBR0) (7,9)
   (_, _) <- binarySearchLRByBounds (ithElementOrdering 1) tt ["B", "C"] 3 6  
 
   (tl0, tr0) <- binarySearchLRBy (ithElementOrdering 0) tt ["E", "B"]
   assertBool "" ((tl0, tr0)==(12,14))
-  assertEqual (binarySearchLRByBounds (ithElementOrdering 1) tt ["E", "B"] tl0 tr0) (14,14)
+  massertEqual (binarySearchLRByBounds (ithElementOrdering 1) tt ["E", "B"] tl0 tr0) (14,14)
 
 testVectorSlicing = do
   let 
@@ -125,14 +125,14 @@ testGreedyMatching = do
   let 
     entities = V.fromList ([["A"], ["B"], ["B","C"], ["B","D","E"],["B","D","F"],["C"],["C","D","E","F"]] :: [[Text]])
     words    = ["X", "A","B", "Z"] :: [Text]
-  assertEqual (greedyMatch entities ([]))    (0, IndexRange 0 7)
-  assertEqual (greedyMatch entities (["X"])) (0, IndexRange 0 7)
-  assertEqual (greedyMatch entities (["B"])) (1, IndexRange 1 5)
+  massertEqual (greedyMatch entities ([]))    (0, IndexRange 0 7)
+  massertEqual (greedyMatch entities (["X"])) (0, IndexRange 0 7)
+  massertEqual (greedyMatch entities (["B"])) (1, IndexRange 1 5)
   assertBool "" ((filter (\x -> length x == 2) (V.toList $ V.slice 1 6 entities)) == ([["B", "C"]]))
-  assertEqual (greedyMatch entities (["B","C","X","Y"])) (2, IndexRange 2 3)
-  assertEqual (greedyMatch entities (["B","D","X","Y"])) (2, IndexRange 3 5)
-  assertEqual (greedyMatch entities (["C","D","E","F"])) (4, IndexRange 6 7)
-  assertEqual (greedyMatch entities (["C","D","E","F"])) (4, IndexRange 6 7)
+  massertEqual (greedyMatch entities (["B","C","X","Y"])) (2, IndexRange 2 3)
+  massertEqual (greedyMatch entities (["B","D","X","Y"])) (2, IndexRange 3 5)
+  massertEqual (greedyMatch entities (["C","D","E","F"])) (4, IndexRange 6 7)
+  massertEqual (greedyMatch entities (["C","D","E","F"])) (4, IndexRange 6 7)
 
 main = do
   --assertBool "HUnit" False
