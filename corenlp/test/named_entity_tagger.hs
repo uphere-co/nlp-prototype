7a8cb7303e97358d0fb084db024a23a769132dc1
Jihun Kim <jihun@uphere.co>
2017-05-22 07:43:16 +0000

    Code cleanup and add more test cases.

--- corenlp/test/named_entity_tagger.hs
+++ corenlp/test/named_entity_tagger.hs
@@ -14,6 +14,10 @@ import           WikiEntity                            (parseEntityLine,loadEnti
 import           WikiEntityTagger                      (buildEntityTable,wikiAnnotator)
 import           WikiNamedEntityTagger                 (getStanfordNEs,parseStanfordNE,namedEntityAnnotator)
 import           CoreNLP                               (parseNEROutputStr)
+-- For testing:
+import           Misc                                  (IRange(..))
+import qualified NamedEntity                   as N
+import qualified WikiEntity                    as Wiki
 
 
 testNamedEntityTagging = testCaseSteps "Named entity tagging on CoreNLP NER output" $ \step -> do
@@ -24,10 +28,18 @@ testNamedEntityTagging = testCaseSteps "Named entity tagging on CoreNLP NER outp
     ner_text = "Google/ORGANIZATION and/O Facebook/ORGANIZATION Inc./ORGANIZATION are/O famous/O AI/O companies/O ./O NLP/ORGANIZATION stands/O for/O natural/O language/O processing/O ./O"
     stanford_nefs =  map parseStanfordNE (parseNEROutputStr ner_text)
     matchedItems  = namedEntityAnnotator entities stanford_nefs
+    uids = fromList . map Wiki.UID
+    expected_matches = [(IRange 12 15, uids ["Q30642"], N.Other),
+                        (IRange 9 10,  uids ["Q30642"], N.Other),
+                        (IRange 6 7,   uids ["Q42970", "Q11660"], N.Other),
+                        (IRange 2 4,   uids ["Q380"], N.Other),
+                        (IRange 0 1,   uids ["Q95", "Q9366"], N.Other)]
+
     tt = getStanfordNEs stanford_nefs
+    expected_tt = [(IRange 9 10, N.Org),(IRange 2 4,N.Org),(IRange 0 1, N.Org)]
   print ner_text
-  print matchedItems
-  print tt
+  eassertEqual tt expected_tt
+  eassertEqual matchedItems expected_matches
 
 unitTests =
   testGroup
--- corenlp/wiki/WikiNamedEntityTagger.hs
+++ corenlp/wiki/WikiNamedEntityTagger.hs
@@ -27,16 +27,19 @@ namedEntityAnnotator entities frags = map (\(range,uids)->(range,uids, N.Other))
     matchedItems = wikiAnnotator entities words
 
 
+
 partitonFrags:: [NamedEntityFrag] -> [(IRange, NamedEntityClass)]
 partitonFrags frags = ifoldl' f [] (fromList frags)
   where
-    g idx frag = (IRange idx (idx+1), N._ftype frag)
+    incR (IRange beg end) = IRange beg (end+1)
+    toRange idx = IRange idx (idx+1)
+    g idx frag = (toRange idx, N._ftype frag)
     f [] idx frag = [g idx frag]
-    f accum@((IRange beg end, tag):ss) idx frag | N._ftype frag == tag = (IRange beg (end+1), tag):ss
-                                                | otherwise            = g idx frag : accum
+    f accum@((range, tag):ss) idx frag | N._ftype frag == tag = (incR range, tag):ss
+                                       | otherwise            = g idx frag : accum
 
 dropNonNE:: [(IRange, NamedEntityClass)] -> [(IRange, NamedEntityClass)]
 dropNonNE = filter (\(_, tag)-> tag /= N.Other)
 
 getStanfordNEs :: [NamedEntityFrag] -> [(IRange, NamedEntityClass)]
-getStanfordNEs frags = dropNonNE (partitonFrags frags)
+getStanfordNEs = dropNonNE . partitonFrags
