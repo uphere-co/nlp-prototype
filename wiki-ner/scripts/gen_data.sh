2db836c517eab946ee4465ad6e78ebab91c8765d
Jihun Kim <jihun@uphere.co>
2017-05-24 08:05:07 +0000

    Bash commands to process raw data files.

---
+++ wiki-ner/scripts/gen_data.sh
@@ -0,0 +1,10 @@
+DIR=/data/groups/uphere/wikidata
+tail -n +2 $DIR/public_companies.csv| awk -F ',' '{print $1}' | awk -F "/" '{print $NF}' > ne.org
+tail -n +2 $DIR/businesspersons.csv | awk -F ',' '{print $1}' | awk -F "/" '{print $NF}' > ne.person   
+tail -n +2 $DIR/employees.csv       | awk -F ',' '{print $1}' | awk -F "/" '{print $NF}' >> ne.person
+
+QEDIR=/opt/develset.rss/
+grep -Fwf ne.person /opt/develset.rss/wikidata.all_entities > uid.person
+grep -Fwf ne.org /opt/develset.rss/wikidata.all_entities    > uid.org
+grep -Fwf <(cat ne.*) /opt/develset.rss/wikidata.properties > property
+
