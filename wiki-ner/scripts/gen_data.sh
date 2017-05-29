1518d310a3dc030d792d4e7e7ace17230f4038a9
Jihun Kim <jihun@uphere.co>
2017-05-29 11:52:45 +0000

    ETL logics to get wiki entity types and their hierarchy.

--- wiki-ner/scripts/gen_data.sh
+++ wiki-ner/scripts/gen_data.sh
@@ -26,3 +26,17 @@ grep -Fwf ne.person $DIR/wikidata.all_entities > uid.person
 #Note : it takes ~ 80 s
 grep -Fwf ne.org $DIR/wikidata.all_entities    > uid.org
 
+
+
+# Get type hierarchy for organization types
+tail -n +2 orgs.csv | awk -F ',' '{n=split($1,a,"/"); print a[n] "\t" $2}' > org_types
+tail -n +2 orgs.csv | awk -F ',' '{n=split($1,a,"/"); print a[n]}' > org_types.uid
+#takes ~22s
+awk -F"\t" 'NR == FNR { a[$1]; next } NF==5 && $1 in a {print}' org_types.uid wikidata.items > items.org
+awk -F"\t" 'NR == FNR { a[$1]=$2; next } NF==5 {split($4, p279s, " "); for(i in p279s) {p=p279s[i];if(p in a) print $1 "\t" $NF "\t" p "\t" a[p]}}' org_types items.org > types.org
+
+# Public companies with their P31 and P279 property values
+grep -Fwf ne.company wikidata.items | awk -F"\t" 'NF==5{print}' > items.company
+# Print company and their organization types
+awk -F"\t" 'NR == FNR { a[$1]=$2; next } NF==5 {split($3, p31s, " "); for(i in p31s) {p31=p31s[i];if(p31 in a) print $1 "\t" $NF "\t" p31 "\t" a[p31]}}' org_types items.company > class.company
+
